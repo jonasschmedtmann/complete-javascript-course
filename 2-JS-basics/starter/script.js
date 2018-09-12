7e3bdb9084afe2090bd1db2449b25f2fa7f539d6
Natan <nxu6202@rit.edu>
2018-09-12 23:11:34 +0200

    added code about variables

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,19 @@
 var firstName = 'John'
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+
+console.log(job);
+
+var _3years = 3;
+var johnMark = 'John and Mark';
+var function = 23;
