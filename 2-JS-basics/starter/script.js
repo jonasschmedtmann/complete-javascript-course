415e75fe6d3f5b50409fd72d127ea3a7a6253a1c
maxniceman <maximum666@ukr.net>
2019-03-10 20:28:04 +0100

    pig game

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -59,7 +59,7 @@ console.log('final array ' + final);*/
 /*************************
  CC #4
  *************************/
-var john = {
+/*var john = {
     name: "John",
     mass: 95,
     height: 1.95,
@@ -95,13 +95,83 @@ if (john.calcBmi() > mark.calcBmi()) {
 }else {
     console.log(mark.name + ' and ' + john.name + ' have an equal bmi-index' + '. And it\'s ' + mark.bmiIndex);
 }
+*/
+/*************************
+ CC #5
+ *************************/
+john = {
+    bills: [124, 48, 268, 180, 42],
+    tips: [],
+    final: [],
+    calcTips: function () {
+        for (var i=0; i<this.bills.length;i++) {
+            var billItem = this.bills[i];
+            var percentage;
+            if(billItem<50) {
+                percentage = .2;
+            }else if (billItem>50 && billItem<200) {
+                percentage = .15;
+            }else {
+                percentage = .1;
+            }
+            this.tips.push(billItem*percentage);
+            this.final.push(this.tips[i]+billItem);
+        }
+    }
+}
+
+mark = {
+    bills: [77, 375, 110, 45],
+    tips: [],
+    final: [],
+    calcTips: function () {
+        for (var i=0; i<this.bills.length;i++) {
+            var billItem = this.bills[i];
+            var percentage;
+            if(billItem<100) {
+                percentage = .2;
+            }else if (billItem>100 && billItem<300) {
+                percentage = .1;
+            }else {
+                percentage = .25;
+            }
+            this.tips.push(billItem*percentage);
+            this.final.push(this.tips[i]+billItem);
+        }
+    }
+}
+
+function avgTips (arr) {
+    var sum = 0;
+    for (var i=0; i<arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum/arr.length;
+}
 
 
-
-
-
-
-
+john.calcTips();
+console.log(john.bills);
+console.log(john.tips);
+console.log(john.final);
+console.log('---');
+mark.calcTips();
+console.log(mark.bills);
+console.log(mark.tips);
+console.log(mark.final);
+console.log('---');
+var avgJohnTips = avgTips(john.tips);
+var avgMarkTips = avgTips(mark.tips);
+console.log('---');
+console.log(avgJohnTips, avgMarkTips);
+console.log('---');
+if (avgJohnTips > avgMarkTips) {
+    console.log('John\'s family has a biggest average tips ' + avgJohnTips);
+}else if (avgMarkTips > avgJohnTips) {
+    console.log('Mark\'s family has a biggest average tips ' + avgMarkTips);
+}else {
+    console.log('Mark\'s and John\'s families have the same amount of tips ' + avgJohnTips);
+}
 
 
 
--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,24 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    var dice = Math.floor(Math.random() * 6) + 1;
+    var diceDOM = document.querySelector('.dice');
+
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    //document.querySelector('#current-' + activePlayer).textContent = dice;
+});
