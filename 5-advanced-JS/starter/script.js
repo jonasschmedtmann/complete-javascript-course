e24557ea2a7f85a63c31862148be1097e721842b
kubistaja <kubista.jakub@allrisk.cz>
2019-05-03 17:29:46 +0200

    Add Object.create and comparison

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -9,6 +9,7 @@ function repeatProtoypes() {
     };
     Person.prototype.job = 'developer';
 
+    // Inherits from constructor prototype property
     let jakub = new Person('Jakub', 1993);
     let martin = new Person('Martin', 1992);
 
@@ -20,4 +21,28 @@ function repeatProtoypes() {
     console.log(jakub instanceof Person);               // true
     console.info([1, 2, 3]);                            // (3) [1, 2, 3]
     console.dir([1, 2, 3]);                             // Array(3)
+};
+
+// Lecture: Object.create
+function repeatObjectCreate() {
+    let personProto = {
+        calculateAge: () => {
+            console.log(2019 - this.yearOfBirth);
+        }
+    }
+    // Inherits directly from first argument
+    let john = Object.create(personProto);
+    john.name = 'john';
+    john.yearOfBirth = 1990;
+
+    let jane = Object.create(personProto, {
+        name: { value: 'Jane' },
+        yearOfBirth: { value: 1980 }
+    });
+
+    console.log(john, jane)
 }
+/**
+ * Result: Object.create has better performance results for
+ * creating big data structures that function constructor pattern.
+ */
