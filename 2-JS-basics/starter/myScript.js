ae83ece3b06d737f81e1214778ab789d5928c89b
Jared Womack <womackjw@hotmail.com>
2019-05-08 16:05:14 -0700

    Completed lesson 14 and now working on if else statements

--- 2-JS-basics/starter/myScript.js
+++ 2-JS-basics/starter/myScript.js
@@ -1,19 +1,157 @@
 /*******************************************
-*Variables and Data Types
-*  */
+ *Variables and Data Types
+ *  */
+// var firstName = 'John';
+// console.log(firstName);
+// var lastName = 'smith';
+// var age = 28;
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// var job;
+// console.log(job);
+
+// job = 'Teacher';
+// console.log(job);
+
+// var _3years = 3;
+// var johnMark = 'John and Mark';
+
+/**************************************
+ * Variable mutation and type coercion
+ * 
+ */
+
+// var firstName = 'John';
+// var age = 28;
+// //Type Coercion below
+// // console.log(firstName + ' ' + age)
+
+// var job, isMarried;
+
+// job = 'teacher';
+// isMarried = false;
+
+// // console.log(firstName + ' is a ' + age + ' years old' + job + '. Is he married? ' + isMarried);
+
+// //variable mutation
+
+// age = 'twenty eight';
+// job = 'driver';
+// alert(firstName + ' is a ' + age + ' years old' + job + '. Is he married? ' + isMarried);
+
+// var lastName = prompt('What is his last name?');
+// console.log(firstName + ' ' + lastName);
+
+/****************************************
+ * Basic Operators +*-||%&& */
+// var year, yearJohn, yearMark
+
+// now = 2020;
+// ageJohn =  28;
+// ageMark =  33;
+
+// console.log(yearJohn);
+// console.log(now + 2);
+// console.log(now * 2);
+// console.log(now / 10);
+
+
+// // Logical Operator
+// var johnOlder = ageJohn < ageMark;
+// console.log(johnOlder)
+
+// //typeOf operator
+// console.log(typeof johnOlder);
+// console.log(typeof ageJohn);
+// console.log(typeof 'Mark is older than John');
+// var x;
+// console.log(typeof x);
+
+/********************************************************
+ * Operator precedence
+ */
+
+// var now = 2019;
+// var yearJohn = 1985;
+// var fullAge = 18;
+// //multiple operators
+// var isFullAge = now - yearJohn >= fullAge;
+// console.log(isFullAge);
+// //grouping
+// var ageJohn = now - yearJohn;
+// var ageMark = 35;
+// var average = (ageJohn + ageMark) / 2;
+// console.log(average);
+
+// //multiple assignments
+// var x, y;
+// x = y = (3 + 5) * 4-6;
+// console.log(x, y);
+
+// //more operators
+// x *= 2;
+// console.log(x);
+// x += 10;
+// console.log(x);
+// x++;
+// console.log(x);
+// x--;
+// console.log(x);
+
+// var markMass, johnMass, marlHeight, johnHeight, markBmi, JohnBmi, markHigherBmi;
+
+// markMass = 100;
+// johnMass = 110;
+// markHeight = 1.5;
+// johnHeight = 2;
+// markBmi = markMass / (markHeight*2);
+// johnBmi = johnMass / (johnHeight*2);
+// markHigherBmi = markBmi > johnBmi;
+// console.log(`Is Mark's BMI higher than John's? ${markHigherBmi}. `)
+
+/******************************************
+ * If / else statements
+ */
+
 var firstName = 'John';
-console.log(firstName);
-var lastName = 'smith';
-var age = 28;
+var civilStatus = 'single';
 
-var fullAge = true;
-console.log(fullAge);
+var isMarried = true;
 
-var job;
-console.log(job);
+if (isMarried) {
+    console.log(`${firstName} is married`)
+} else {
+    console.log(`${firstName} will hopefully marry soon :)`)
+}
 
-job = 'Teacher';
-console.log(job);
+var markMass, johnMass, marlHeight, johnHeight, markBmi, JohnBmi, markHigherBmi;
 
-var _3years = 3;
-var johnMark = 'John and Mark';
+markMass = 100;
+johnMass = 110;
+markHeight = 1.5;
+johnHeight = 2;
+markBmi = markMass / (markHeight * 2);
+johnBmi = johnMass / (johnHeight * 2);
+
+if (markBmi > johnBmi) {
+    console.log(`Marks bmi of ${markBmi} is higher than Johns bmi of ${johnBmi}.`)
+} else {
+    console.log(`Marks bmi is lower than Johns`)
+}
+
+/***************************************************
+ * Boolean Logic
+ */
+
+var firstName = 'John';
+var age = 16;
+
+if (age < 13) {
+    console.log(`${firstName} is a boy`);
+} else if (age >= 13 || age < 20) {
+    console.log(`${firstName} is a teenager.`)
+} else {
+    console.log(`${firstName} is a man.`);
+}
