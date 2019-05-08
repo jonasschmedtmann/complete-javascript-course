7e3bb344cf421ed36fe189e9d6e0713391942bfb
Jared Womack <womackjw@hotmail.com>
2019-05-08 09:39:43 -0700

    checked out a firstName-lastName branch and added a js file for practice. Completed variables and data types

---
+++ 2-JS-basics/starter/myScript.js
@@ -0,0 +1,19 @@
+/*******************************************
+*Variables and Data Types
+*  */
+var firstName = 'John';
+console.log(firstName);
+var lastName = 'smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+var _3years = 3;
+var johnMark = 'John and Mark';
