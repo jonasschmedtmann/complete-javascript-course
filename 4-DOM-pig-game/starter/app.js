7607b17e604d4fe58497932b99d9cce73f6edf81
Haemee Nabors <HaemeeNabors@gmail.com>
2019-06-14 09:58:36 -0500

    fixed typo

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -7,4 +7,5 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
+- This line is added with Eclipse
 */
