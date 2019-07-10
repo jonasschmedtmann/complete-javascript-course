cfa9fd6d9d00926031bebe0d882bf413da9157ac
Stephen Sampson <ssampson@metocean.com>
2019-07-10 19:49:26 -0300

    Completed Section 3

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,21 +1,36 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+/*
+
+//functions
+calculateAge(1990);
+
+function calculateAge(year){
+    console.log(2019-year);
+}
 
 
+// retirement(1956) // doesn't work!
+var retirement = function(year){
+    console.log(65 - (2019 - year));
+}
 
+//variables
 
+console.log(age);
+var age = 23; 
 
+function foo(){
+    console.log(age);
+    var age = 65;
+    console.log(age);
+}
 
+foo();
+console.log(age);
 
-
-
-
-
-
-
-
-
+*/
 
 ///////////////////////////////////////
 // Lecture: Scoping
@@ -62,7 +77,25 @@ function third() {
 }
 */
 
+/*
+var a = 'Hello!';
+first();
 
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        third()
+
+        function third() {
+            var d = 'John';
+            console.log(a + b + c + d);
+        }
+    }
+}
+*/
 
 ///////////////////////////////////////
 // Lecture: The this keyword
