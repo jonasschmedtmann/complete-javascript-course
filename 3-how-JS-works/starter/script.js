1b6b7c162c259efa8c94f2407eb1edc23a141976
Stephen Sampson <stephensampson@dal.ca>
2019-07-11 08:49:00 -0300

    Update script.js

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -100,11 +100,32 @@ function first() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+console.log(this);
+
+function calculateAge(year){
+    console.log(2019 - year);
+    console.log(this);
+}
+
+calculateAge(1985);
 
 
+var john = {
+    name : 'John',
+    yearOfBirth : 1990,
+    calculateAge : function(){
+        console.log(this);
+        console.log(2019 - this.yearOfBirth);
+    }
+};
 
+john.calculateAge();
 
+var mike = {
+    name : 'Mike',
+    yearOfBirth : 1984,
+};
 
+mike.calculateAge = john.calculateAge;
 
-
-
+mike.calculateAge();
