4c6c142a24d724b0e8a4ed6e75bc299139cb470a
Gaurav Sharma <gaurav.haxor@gmail.com>
2019-11-30 20:35:33 +0530

    Updated API
    Food2Forkify api has been shutdown hence implemeneted another API

--- 9-forkify/final/src/js/models/Search.js
+++ 9-forkify/final/src/js/models/Search.js
@@ -1,18 +1,17 @@
-import axios from 'axios';
-import { key, proxy } from '../config';
+import axios from "axios";
+import {key} from "../config";
 
 export default class Search {
     constructor(query) {
         this.query = query;
     }
-
     async getResults() {
         try {
-            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
-            this.result = res.data.recipes;
-            // console.log(this.result);
+            const res = await axios(`https://api.spoonacular.com/recipes/search?apiKey=${key}&number=30&query=${this.query}`);
+            this.result = res.data.results;
         } catch (error) {
-            alert(error);
+            alert(error)
         }
     }
 }
+
