6d0c1c2bbf6ce9d60cd20683ccd64bc6878ef19a
jcbhass <47954686+jcbhass@users.noreply.github.com>
2019-12-01 09:42:06 -0500

    Add files via upload

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,20 +1,20 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+calculateAge(1965);//available before code is executed bc of hoisting. 
+function calculateAge(year) {
+    console.log(2016 - year);
+}
+//only for funcition declarations, not function expressions. 
 
+//retirement(1956); // get error that retiremnet is not defined. 
 
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
 
-
-
-
-
-
-
-
-
-
-
-
+//variables are undefined when hoisted until they have the value put in. 
+//main thing is that can have function declarations used before they are defined bc of hoisting. 
 
 
 ///////////////////////////////////////
@@ -23,26 +23,26 @@
 
 // First scoping example
 
-/*
-var a = 'Hello!';
-first();
 
-function first() {
-    var b = 'Hi!';
-    second();
+// var a = 'Hello!';
+// first();
 
-    function second() {
-        var c = 'Hey!';
-        console.log(a + b + c);
-    }
-}
-*/
+// function first() {
+//     var b = 'Hi!';
+//     second();
 
+//     function second() {
+//         var c = 'Hey!';
+//         console.log(a + b + c);
+//     }
+// }
+
+//Each funcition creates a scope. 
 
 
 // Example to show the differece between execution stack and scope chain
 
-/*
+
 var a = 'Hello!';
 first();
 
@@ -58,19 +58,35 @@ function first() {
 
 function third() {
     var d = 'John';
-    console.log(a + b + c + d);
+    //console.log(c);
+    console.log(a+d);
 }
-*/
+
 
 
 
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+//this keyword is always attached to Window unless in a method of an object. Attached to Window bc global object. Needs another object. 
+//this has to be apart of method, never a regular function, even if the function is in an object.
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2016 - this.yearOfBirth);
+    }
+}
 
+john.calculateAge();
 
-
-
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+}
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
 
 
 
