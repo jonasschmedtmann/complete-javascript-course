22867f97f44b7d18e2bbc214cf4d9cc6652fab47
Sai Bala Krishna Allamsetty <sbk888.sbk@gmail.com>
2019-12-23 07:46:51 +0530

    Pig Game started

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,17 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, currentScore, activePlayer;
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.floor( Math.random()  * 6 ) + 1;
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+
+ var x = document.querySelector('#score-0').textContent;
+ console.log(x);
+
+ document.querySelector('.dice').style.display = 'none';
