81d931a31997c4930115765afb46ab40eac68fbc
Zack Bumpous <upwork.ztb@gmail.com>
2020-01-06 10:58:22 -0600

    created variable that is undefined and used it in my console.log and seen it return undefined

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,10 +1,10 @@
-
-
 var firstName = 'Zachary';
-
 var lastName = 'Bumpous';
 var age = 22;
 var aspiringDev = true;
+var fullAge = true;
+var firstDevJob = false;
+var job;
 
 console.log(firstName)
-console.log(`Hello, if you did not know, it is ${aspiringDev} that ${firstName} ${lastName} is an aspiring developer. He is ${age} years young.`)
+console.log(`Hello, if you did not know, it is ${aspiringDev} that ${firstName} ${lastName} is an aspiring developer. He is ${age} years young. Has he had a developer job? ${firstDevJob}. His current job is ${job} because he is unemployed.`)
