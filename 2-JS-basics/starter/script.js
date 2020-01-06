936657387d37ef20a80e32aa8578597dc00c115f
Zack Bumpous <upwork.ztb@gmail.com>
2020-01-06 10:33:07 -0600

    Added a boolean and number primitive data type into variables and added them into my dynamic console.log

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,10 @@
-console.log("Hello World from the script.js file!!!")
+
+
+var firstName = 'Zachary';
+
+var lastName = 'Bumpous';
+var age = 22;
+var aspiringDev = true;
+
+console.log(firstName)
+console.log(`Hello, if you did not know, it is ${aspiringDev} that ${firstName} ${lastName} is an aspiring developer. He is ${age} years young.`)
