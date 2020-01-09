069acf1821bda237ded6f382bb90f8eaae21127a
Zack Bumpous <upwork.ztb@gmail.com>
2020-01-09 11:57:58 -0600

    Lesson 16, Scoping and the Scope Chain complete, walks through a couple examples demonstrating what variables functions have access to depending on their scope, and an example showing the difference between execution stack and scope chain

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,5 +1,5 @@
 ///////////////////////////////////////
-// Lecture: Hoisting LESSON 15\
+// Lecture: Hoisting LESSON 15\ HOISTING IS FIRST STEP OF CREATION PHASE
 // 1. notice how calcAge works before and after the function is even declared, this is hoisting
 // 2. in the creation phase of the execution context, in this case the global execution context, the function declaration calcAge is stored in the variable object (VO) even before the code is executed, and this is why when we finally do execute the code, the calcAge function is available for us to use it
 // 3. this works for function declarations, but there is also something called function expressions, right?
@@ -7,43 +7,52 @@
 
 // functions
 
-calcAge(1997);
-function calcAge(year) {
-    console.log(2020 - year);
-}
-calcAge(1997);
+// calcAge(1997);
+// function calcAge(year) {
+//     console.log(2020 - year);
+// }
+// calcAge(1997);
 
-// 4. let us see how this works out w/ function expressions
+// // 4. let us see how this works out w/ function expressions
 
-// retirement(1997); 5. this wont work, because hoisting only works for function declarations, not function expressions!!!
-var retirement = function(year) {
-    console.log(65 - (2020 - year));
-}
-retirement(1997);
-
-
-// variables
-// console.log(age); 6. this wont work, undefined, this is how hoisting works with variables, because in the creation phase of the variable object (VO) the code is scanned for variable declarations and the variables are then set to undefined
-var age = 23;
+// // retirement(1997); 5. this wont work, because hoisting only works for function declarations, not function expressions!!!
+// var retirement = function(year) {
+//     console.log(65 - (2020 - year));
+// }
+// retirement(1997);
 
 
-function foo() {
-    var age = 65;
-    console.log(age); // this will console log 65, it has its own variable object (VO)
-}
-foo();
-console.log(age); // this will console log 23, it has its own variable object (VO)
+// // variables
+// // console.log(age); 6. this wont work, undefined, this is how hoisting works with variables, because in the creation phase of the variable object (VO) the code is scanned for variable declarations and the variables are then set to undefined
+// var age = 23;
+
+
+// function foo() {
+//     var age = 65;
+//     console.log(age); // this will console log 65, it has its own variable object (VO)
+// }
+// foo();
+// console.log(age); // this will console log 23, it has its own variable object (VO)
+
+
+
+
+
+
+
+
+
 
 
 
 
 ///////////////////////////////////////
-// Lecture: Scoping
+// Lecture: Scoping LESSON 16
 
 
-// First scoping example
+// First scoping example, this works because the second function has access to the variables in the first function and the global scope, and thats because the second function is written INSIDE of the first
 
-/*
+
 var a = 'Hello!';
 first();
 
@@ -56,31 +65,31 @@ function first() {
         console.log(a + b + c);
     }
 }
-*/
 
 
 
-// Example to show the differece between execution stack and scope chain
 
-/*
-var a = 'Hello!';
+// Example to show the difference between execution stack and scope chain
+
+
+var a = 'Hello!'; // have var a in our global scope
 first();
 
-function first() {
+function first() { // have the first() function in global scope
     var b = 'Hi!';
-    second();
+    second(); // inside first function, define var b, this will run function second because it is hoisted
 
     function second() {
         var c = 'Hey!';
-        third()
+        third() // inside second function, second function has access to global scope because of scope chain
     }
 }
 
-function third() {
+function third() { // have the third() function in global scope, but even tho the second function has access to function third, third function does not have that reverse action to function second. Function third is in a diff. scope.
     var d = 'John';
-    console.log(a + b + c + d);
+    console.log(c);
 }
-*/
+
 
 
 
