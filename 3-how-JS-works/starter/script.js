aeecfa9423f33c3f3c875b08840a0388479bf0c5
Zack Bumpous <upwork.ztb@gmail.com>
2020-01-08 19:27:29 -0600

    Lesson 15 Hoisting in Practice complete, created a function declaration and looked at hoisting w some console logs before and after the function declaration. Also did the same process to a function expression and noticed the difference. Lastly, loked at the hoisting of variables and the same variable name inside of a function

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,18 +1,38 @@
 ///////////////////////////////////////
-// Lecture: Hoisting
+// Lecture: Hoisting LESSON 15\
+// 1. notice how calcAge works before and after the function is even declared, this is hoisting
+// 2. in the creation phase of the execution context, in this case the global execution context, the function declaration calcAge is stored in the variable object (VO) even before the code is executed, and this is why when we finally do execute the code, the calcAge function is available for us to use it
+// 3. this works for function declarations, but there is also something called function expressions, right?
 
 
+// functions
+
+calcAge(1997);
+function calcAge(year) {
+    console.log(2020 - year);
+}
+calcAge(1997);
+
+// 4. let us see how this works out w/ function expressions
+
+// retirement(1997); 5. this wont work, because hoisting only works for function declarations, not function expressions!!!
+var retirement = function(year) {
+    console.log(65 - (2020 - year));
+}
+retirement(1997);
 
 
+// variables
+// console.log(age); 6. this wont work, undefined, this is how hoisting works with variables, because in the creation phase of the variable object (VO) the code is scanned for variable declarations and the variables are then set to undefined
+var age = 23;
 
 
-
-
-
-
-
-
-
+function foo() {
+    var age = 65;
+    console.log(age); // this will console log 65, it has its own variable object (VO)
+}
+foo();
+console.log(age); // this will console log 23, it has its own variable object (VO)
 
 
 
