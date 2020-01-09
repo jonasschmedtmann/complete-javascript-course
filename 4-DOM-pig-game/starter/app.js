19497e244d4e343dce97b1f96e3fa4e868167286
Zack Bumpous <upwork.ztb@gmail.com>
2020-01-09 16:36:37 -0600

    Finishes Lesson 18, made the query selector more dynamic, used innerHTML method to change html. Also used the .style .display methods and set display equal to none to hide some CSS

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -19,7 +19,7 @@ GAME RULES:
 
 // First DOM Access and Manipulation, LESSON 18
 // 1. declare variables
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, dice;
 
 scores = [0, 0];
 roundScore = 0;
@@ -36,5 +36,23 @@ console.log(dice); // 7. It randomizes in the console. Now it is time for some D
 // 8. The Object that will give us access to the DOM is the Document object
 // 9. querySelector allows us to grab a specific HTML element by the ID name we gave it, in this case it is score-0
 // 10. if we want to change the text in this element, then we need another method... which is the textContent method and set it defined to the dice 
-document.querySelector('#score-0').textContent = dice;
-document.querySelector('#score-1').textContent = dice;
+// document.querySelector('#score-' + activePlayer).textContent = dice;
+// 11. If we want to insert some HTML, we cant use textContent, instead we use .innerHTML and set it equal to a string '' and can write html inside
+document.querySelector('#score-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+// 12. we can use the querySelector to actually only read from our webpage as well... and then store it in a variable for example
+// 13. create a random variable and put querySelector inside (store)
+// 14. Dont set textContent equal to anything, then console.log the variable it was stored in (x)
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+// 15. Not only can we use querySelector to change the HTML of an element... we can also use it to change the CSS! We are selecting by class, not id, so instead of a # we need a .
+// 16. now we need to use the .style method, and then the CSS property that we want to change
+// 17. the CSS property that we want to change is the display property, we want to set it to none
+document.querySelector('.dice').style.display = 'none';
+
+
+
+
