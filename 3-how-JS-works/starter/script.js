9fd5591d687bab01a26435370d92d052ed876e39
Zack Bumpous <upwork.ztb@gmail.com>
2020-01-09 12:56:42 -0600

    The This KeyWord, LESSON 17 completed, creates a function expression inside an object and demonstrates console logging the this keyword, how it becomes what that object is, and then having an innerfunction inside the first function in the object, and console logging the this keyword and how it shows the window object which is global, and then this also demonstrates method borrowing in an object that wishes to achieve a similar outcome with different data

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -53,54 +53,103 @@
 // First scoping example, this works because the second function has access to the variables in the first function and the global scope, and thats because the second function is written INSIDE of the first
 
 
-var a = 'Hello!';
-first();
+// var a = 'Hello!';
+// first();
 
-function first() {
-    var b = 'Hi!';
-    second();
+// function first() {
+//     var b = 'Hi!';
+//     second();
 
-    function second() {
-        var c = 'Hey!';
-        console.log(a + b + c);
-    }
-}
-
-
+//     function second() {
+//         var c = 'Hey!';
+//         console.log(a + b + c);
+//     }
+// }
 
 
-// Example to show the difference between execution stack and scope chain
 
 
-var a = 'Hello!'; // have var a in our global scope
-first();
+// // Example to show the difference between execution stack and scope chain
 
-function first() { // have the first() function in global scope
-    var b = 'Hi!';
-    second(); // inside first function, define var b, this will run function second because it is hoisted
 
-    function second() {
-        var c = 'Hey!';
-        third() // inside second function, second function has access to global scope because of scope chain
-    }
-}
+// var a = 'Hello!'; // have var a in our global scope
+// first();
 
-function third() { // have the third() function in global scope, but even tho the second function has access to function third, third function does not have that reverse action to function second. Function third is in a diff. scope.
-    var d = 'John';
-    console.log(c);
-}
+// function first() { // have the first() function in global scope
+//     var b = 'Hi!';
+//     second(); // inside first function, define var b, this will run function second because it is hoisted
+
+//     function second() {
+//         var c = 'Hey!';
+//         third() // inside second function, second function has access to global scope because of scope chain
+//     }
+// }
+
+// function third() { // have the third() function in global scope, but even tho the second function has access to function third, third function does not have that reverse action to function second. Function third is in a diff. scope.
+//     var d = 'John';
+//     console.log(c);
+// }
+
+
+
+
+
+
+
+
+
 
 
 
 
 ///////////////////////////////////////
-// Lecture: The this keyword
+// Lecture: The this keyword LESSON 17
+//1. console log this keyword to see it is global
+// console.log(this);
 
+// calculateAge(1997);
+// //2. create function declaration calcAge and console.log this keyword
+// // function declaration
+// function calculateAge(year){
+//     console.log(2020 - year);
+//     console.log(this); // regular function call, not a method, so should be the same as the console log on line 108
+// }
 
+// function expression 
+var zack = {
+    name: 'Zack',
+    yearOfBirth: 1997,
+    calculateAge: function(){
+        console.log(2020 - this.yearOfBirth);
+        console.log(this); // 3. inside here we can also console log the this keyword, but we will have to perform a method call. See step 4
+    
+        // 6. We are going to add a new function inside of the calculateAge function/method and console.log this keyword
+    
+        function innerFunction() {
+            console.log(this); // 8. Notice how our object is back to being the window (global) object. This is up for debates in the JavaScript community on whether this is the way it should be or not
+            // 9. it follows the rules though... although it is written inside of a method pertaining to an object  (calcAge function => zack object) it is still just a regular function
+        }
+        // 7. perform our method call for innerFunction
+        innerFunction();
+    }
+}
 
+// 4. perform a method call
+zack.calculateAge();
 
+// 5. note how now the this keyword is the Zack object! The this keyword refers to the object that calls the method, in this case the zack object called it
 
+// 10. creating a new object, doing the same this as the one above.
+var brandon = {
+    name: 'Brandon',
+    yearOfBirth: 1998,
+    // 11. we could copy and paste the calcAge function, or we could be smart and do something really common in JavaScript, called METHOD BORROWING
+    // 12. we are going to BORROW zack objects method and use it on Brandon
+}
 
+// 13. this is how we allow brandon object to borrow method from zack object
+brandon.calculateAge = zack.calculateAge;
 
-
-
+// 14. call our function
+brandon.calculateAge();
+// 15. notice how the this variable we are borrowing becomes the brandon object. notice how the innerFunction console log of this that we borrowed is the window object still
