0d29f0ba18505f23e91b84a4a966cbbea097d334
Zack Bumpous <upwork.ztb@gmail.com>
2020-01-21 18:01:49 -0600

    Created a game initialization function

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -21,19 +21,7 @@ GAME RULES:
 // 1. declare variables
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
-
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     // Do something here
@@ -108,3 +96,28 @@ function nextPlayer() {
     document.querySelector('.dice').style.display = 'none';
 }
 
+
+// Creating a Game Initialization Function LESSON 21 
+
+document.querySelector('.btn-new').addEventListener('click', init)
+
+function init(){
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active'); // removing and adding so we don't add 2 active classes, because then if we did want to remove later, we would have to remove twice. its like it stacks
+    document.querySelector('.player-1-panel').classList.remove('active');
+}
