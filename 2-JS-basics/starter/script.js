290d45f7745b94e0b5eca46eec15ba49f1a46e7a
Stephanie <stephaniebpena@gmail.com>
2020-01-29 11:58:43 -0800

    js basics start

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,32 @@
+/******************************************
+* Variables and data types
+*/
+/*
+var firstName = 'Steph';
+console.log(firstName);
+
+var lastName = 'Peña';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// Variable naming rules
+var _3years = 3;
+var johnMark = 'John and Mark';
+*/
+
+/******************************************
+* Variable mutation and type coercion
+*/
+
+var firstName = 'Steph';
+var age = 28;
+
+console.log(firstName + ' ' + age)
