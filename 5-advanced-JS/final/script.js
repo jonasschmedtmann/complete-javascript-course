4d1b73fa7f81524d556b069f6cb67299fc5fa46f
Zack Bumpous <upwork.ztb@gmail.com>
2020-02-03 10:30:21 -0600

    Working on coding challenge

--- 5-advanced-JS/final/script.js
+++ 5-advanced-JS/final/script.js
@@ -329,7 +329,7 @@ c) correct answer (I would use a number for this)
 */
 
 
-/*
+///*
 (function() {
     function Question(question, answers, correct) {
         this.question = question;
@@ -376,7 +376,7 @@ c) correct answer (I would use a number for this)
 
     questions[n].checkAnswer(answer);
 })();
-*/
+//*/
 
 
 
--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -303,6 +303,7 @@ interviewQuestion('other')('Debbi');
 
 
 // 69. Bind, Call and Apply
+/*
 var zack = {
     name: 'Zack',
     age: 22,
@@ -342,7 +343,7 @@ emilyFormal('Winter')
 
 
 
-
+// 69 CONT'd.. using BIND method.. function is from a prior lesson
 
 
 var years = [1990, 1965, 1937, 2005, 1998];
@@ -373,3 +374,38 @@ console.log(ages);
 console.log(fullAgeJapan);
 console.log(fullAgeUSA);
 console.log(fullAgeAlcUSA);
+*/
+
+
+
+
+// Lesson 70. Coding Challenge 7
+/*
+--- Let's build a fun quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
+
+
+// 1. Build a function constructor to describe a question. question must include the ? itself, the answers a person can choose, and the correct answer
+
+(function QuestionConstructor(question, answers, correctAnswer){
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+})
