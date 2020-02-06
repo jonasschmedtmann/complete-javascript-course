e17b44efd5e903430110c595754dae329e746134
Zack Bumpous <upwork.ztb@gmail.com>
2020-02-06 14:03:21 -0600

    Wrote different modules, with both private and public methods and variables, and we also made them interact with one another. This is sort of the barebones structure of the Budgety app.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -11,6 +11,8 @@ function Person(pName){
 var me = new Person("Zachary");
 */
 
+
+
 // Practicing writing and demonstrating use of a simple IIFE (iffy) [immediately invoked function expression] the format looks like: (function () {})();
 // so it is an anonymous function, and the () at the ends mean we are immediately invoking it.
 
@@ -30,3 +32,47 @@ function SomeFunction(){
 }
 SomeFunction();
 */
+
+
+
+
+
+// 77. Implementing the Module Pattern
+// the x and add variables are private bc inside of IIFE, publicTest is public even though it is in IIFE bc it is in the closure, and publicTest has access to x and add because it is inside of that IIFE
+// module1
+var budgetController = (function () {
+    var x = 23;
+
+    var add = function(a){
+        return x + a;
+    }
+
+    // module pattern ex below
+    return {
+        publicTest: function(b){
+            return add(b);
+        }
+    }
+})();
+
+
+
+// module2
+var UIController = (function () {
+
+})();
+
+
+// module3 // we will pass the other two modules into this one so that we can connect, or that they can sort of know about each other
+// budgetCtrl = budgetController // UICtrl = UIController
+var controller = (function (budgetCtrl, UICtrl) {
+
+    var z = budgetCtrl.publicTest(5);
+
+    return {
+        anotherPublicMethod: function(){
+            console.log(z);
+        }
+    }
+
+})(budgetController, UIController);
