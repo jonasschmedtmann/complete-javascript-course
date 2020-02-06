291856d47873a07f5d415e6a40143a974448b414
Zack Bumpous <upwork.ztb@gmail.com>
2020-02-06 13:04:41 -0600

    Wrote a simple IIFE that demonstrates removing data from the global scope by using the same variable in both the IIFE and SomeFunction.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -10,3 +10,23 @@ function Person(pName){
 
 var me = new Person("Zachary");
 */
+
+// Practicing writing and demonstrating use of a simple IIFE (iffy) [immediately invoked function expression] the format looks like: (function () {})();
+// so it is an anonymous function, and the () at the ends mean we are immediately invoking it.
+
+// 1. global variable pllution
+// 2. init only once
+// init starts // once
+/*
+(function () {
+var counter = 15;
+counter++; // this counter is referencing the one assigned to 15
+// ends here
+})();
+
+function SomeFunction(){
+    var counter = 0;
+    counter = counter + 1; // this counter variable is referencing the one assigned a value of 0
+}
+SomeFunction();
+*/
