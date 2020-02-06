8a2b42e6b2b0c0073830863bfc058135450dbd12
Zack Bumpous <upwork.ztb@gmail.com>
2020-02-06 09:31:17 -0600

    Almost done with expert coding challenge 7, created a nextQuestion function, stored some code I had already written inside it, called or invoked the function inside of itself as well as outside so that it would loop repeatedly through my questions array, however it would never end so inside of that nextQuestion function I added an if statement that allows a user to type the word exit inside of a prompt and it will end the loop of questions

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -407,6 +407,7 @@ c) correct answer (I would use a number for this)
 
 // 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 
+/*
 // for step 7 we are going to create an immediately invoked function expression and put all our code inside it
 (function() {
     function QuestionConstructor(question, answers, correctAnswer){
@@ -471,4 +472,101 @@ c) correct answer (I would use a number for this)
     // 6. Call the method
     questions[randomQuestionLogger].checkAnswer(answer);
 })();
+*/
 
+
+
+
+
+/*
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
+
+// Copying code from the Challenge 7 Part 1
+
+// for step 7 we are going to create an immediately invoked function expression and put all our code inside it
+(function() {
+    function QuestionConstructor(question, answers, correctAnswer){
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+    }
+    // 4. Writing a method to log random question to console
+    // QuestionConstructor.prototype.displayQuestion() = function() {
+    //     console.log(this.question);
+    
+    //     // we need to loop through all the answers and display all of them
+    //     for (var i = 0; i < this.answers.length; i++){
+    //         console.log(i + '; ' + this.answers[i])
+    //     }
+    // }
+    
+    QuestionConstructor.prototype.displayQuestion = function() {
+        console.log(this.question);
+    
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }
+    
+    // 6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+    QuestionConstructor.prototype.checkAnswer = function(ans) {
+        if (ans === this.correctAnswer){
+            console.log('Correct answer!');
+        } else {
+            console.log('Wrong answer. Try again.')
+        }
+    }
+    
+    
+    // 2. Create a couple of questions using the costructor
+    var q1 = new QuestionConstructor('Is JavaScript the coolest programming language in the world?',
+    ['Yes', 'No'],
+    0);
+    
+    var q2 = new QuestionConstructor('What is the name of this course\'s teacher?',
+    ['John', 'Zack', 'Jonas'],
+    2);
+    
+    var q3 = new QuestionConstructor('Which of the following best describes coding?',['Hard', 'Boring', 'Fun', 'Lame'],
+    2);
+    
+
+      // 3. Store all the questions we created inside an array
+      var questions = [q1, q2, q3];
+        
+
+    // 8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+    function nextQuestion(){
+  
+        // 4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+        
+        var randomQuestionLogger = Math.floor(Math.random() * questions.length);
+        questions[randomQuestionLogger].displayQuestion();
+        
+        // 5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+        
+        // since our answers are numbers, but the console is turning them to strings, we need to parseInt our answer so that it makes it a number
+        
+        var answer = prompt('Please select the correct answer');
+        
+
+        // 9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+        if(answer !== 'exit'){
+            // 6. Call the method
+            questions[randomQuestionLogger].checkAnswer(parseInt(answer));
+
+            nextQuestion();
+        }
+    }
+    nextQuestion();
+})();
