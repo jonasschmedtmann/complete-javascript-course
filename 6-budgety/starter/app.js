e4636036b1be059c75076f01f9f83baa922db6e0
Zack Bumpous <upwork.ztb@gmail.com>
2020-02-06 12:40:44 -0600

    Practiced writing a simple closure, moving on to practicing simple iffy next

---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,12 @@
+// Practicing writing and demonstrating use of a simple closure
+/*
+function Person(pName){
+    var _personName = pName;
+
+    this.getPersonName = function(){
+        return _personName;
+    };
+}
+
+var me = new Person("Zachary");
+*/
