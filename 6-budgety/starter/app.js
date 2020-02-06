36afb48dca4f41b19f0cf4fb101144d741ffed84
Zack Bumpous <upwork.ztb@gmail.com>
2020-02-06 14:30:05 -0600

    Creates my event listeners, a click event on the add btn html class, and a keypress event that only will add an item if we keypress the ENTER key.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -34,45 +34,56 @@ SomeFunction();
 */
 
 
+// format for basic IIFE: (function () {})();
 
 
 
-// 77. Implementing the Module Pattern
+// BUDGETY APP
 // the x and add variables are private bc inside of IIFE, publicTest is public even though it is in IIFE bc it is in the closure, and publicTest has access to x and add because it is inside of that IIFE
-// module1
+
+// 1. BUDGET CONTROLLER // module1
 var budgetController = (function () {
-    var x = 23;
 
-    var add = function(a){
-        return x + a;
-    }
+    // Some code
 
-    // module pattern ex below
-    return {
-        publicTest: function(b){
-            return add(b);
-        }
-    }
 })();
 
 
 
-// module2
+// 2. UI CONTROLLER // module2
 var UIController = (function () {
 
+    // Some code
+
 })();
 
 
+// GLOBAL APP CONTROLLER
 // module3 // we will pass the other two modules into this one so that we can connect, or that they can sort of know about each other
 // budgetCtrl = budgetController // UICtrl = UIController
 var controller = (function (budgetCtrl, UICtrl) {
 
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function(){
+        // 1. Get the filled input data
 
-    return {
-        anotherPublicMethod: function(){
-            console.log(z);
-        }
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+        console.log('It works.') // tests for clicking button or key down on ENTER key
     }
+    
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem); // callback so auto called back when needed (someone pressing button)
+      
 
+    document.addEventListener('keypress', function(event) {
+
+        
+        if (event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+    });
 })(budgetController, UIController);
