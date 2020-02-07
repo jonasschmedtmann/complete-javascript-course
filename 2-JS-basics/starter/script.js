5aa7f6137d8b441bce810454297d53de357e0272
jkaddu <jkaddu@yahoo.com>
2020-02-06 21:27:11 -0800

    practice

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,15 @@
+var firstName = "Joshua";
+var lasName = "Kaddu";
+console.log(firstName, lasName);
+
+var age = 29;
+console.log(age);
+
+var ofAge = true;
+console.log(ofAge);
+
+var job;
+console.log(job);
+
+job = "philanthropist";
+console.log(job);
