0a83a4ba6eea17e0cae9d6552e698ac4976d82c4
Zack Bumpous <upwork.ztb@gmail.com>
2020-02-07 01:57:42 -0600

    Inside UIController, created private DOMstrings object with the inputs of our HTML class names that we need, just incase we end up changing them, they will be dynamic and won't break. In our return, we then created the getDOMstrings method to expose the DOMstrings object to the public so we could use invoke/call them in the controller. I then went ahead and made all of the querySelectors that I also created, which were inside a getInput method in our return statement inside the UIController. This getInput method actually returns an object with the 3 query selectors so it just runs all of them instantly. I then invoked this as var DOM inside of controller.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -45,27 +45,54 @@ SomeFunction();
 var budgetController = (function () {
 
     // Some code
+   
 
 })();
 
 
 
-// 2. UI CONTROLLER // module2
+// 2. UI CONTROLLER // module2 // write some methods to get some data for us or to calculate something
 var UIController = (function () {
 
-    // Some code
+    var DOMstrings = { // these are private
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
 
+
+
+    return {
+    
+        
+        getInput: function(){
+            // ! This function returns an object with three methods (query selector) bc it is more efficient since it will auto run all 3, instead of just writing/reading them in order. Call the method in the controller
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            }
+        },
+
+        getDOMstrings: function(){ // this exposes DOMstrings to the public so we can use them elsewhere
+            return DOMstrings;
+        }
+    };
 })();
 
 
-// GLOBAL APP CONTROLLER
+// GLOBAL APP CONTROLLER -- the place where we tell the other modules what to do. We call the methods in here, and then you can pass that methods data into the budgetController
 // module3 // we will pass the other two modules into this one so that we can connect, or that they can sort of know about each other
 // budgetCtrl = budgetController // UICtrl = UIController
 var controller = (function (budgetCtrl, UICtrl) {
 
-    var ctrlAddItem = function(){
-        // 1. Get the filled input data
+    var DOM = UICtrl.getDOMstrings();
 
+    var ctrlAddItem = function(){
+        // 1. Get the filled input data (calling the getInput method in UIController)
+        var input = UICtrl.getInput();
+        console.log(input);
         // 2. Add the item to the budget controller
 
         // 3. Add the item to the UI
@@ -73,10 +100,10 @@ var controller = (function (budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
-        console.log('It works.') // tests for clicking button or key down on ENTER key
+       // console.log('It works.') // tests for clicking button or key down on ENTER key
     }
     
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem); // callback so auto called back when needed (someone pressing button)
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem); // callback so auto called back when needed (someone pressing button)
       
 
     document.addEventListener('keypress', function(event) {
