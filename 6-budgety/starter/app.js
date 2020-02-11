c20ecd5f6c41d7c6371084cf9483d0b9a722978e
Zack Bumpous <upwork.ztb@gmail.com>
2020-02-11 14:47:45 -0600

    Just messed around and worked on console logging arrays and nested arrays inside of arrays while using the flat() method on them to understand it. It is really cool for bringing together a nested array into its, so to say, parent array.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -38,13 +38,43 @@ SomeFunction();
 
 
 
+
+//Practicing writing a function constructor
+//normal function//camel notation//
+/*
+function CreateCircle(radius){
+    return {
+        radius,
+        draw(){
+            console.log('draw');
+        }
+    };
+}
+const myCircle = createCircle(1);
+
+//constructor function//pascal notation//
+function Circle(radius){
+    this.radius = radius;
+    this.draw = function(){
+        console.log('draw');
+    }
+}
+const circle = new Circle(1);
+*/
+
+
+
+
+
+
 // BUDGETY APP
 // the x and add variables are private bc inside of IIFE, publicTest is public even though it is in IIFE bc it is in the closure, and publicTest has access to x and add because it is inside of that IIFE
+/*
 
 // 1. BUDGET CONTROLLER // module1
 var budgetController = (function () {
 
-    // Some code
+    
    
 
 })();
@@ -127,3 +157,22 @@ var controller = (function (budgetCtrl, UICtrl) {
 })(budgetController, UIController);
 
 controller.init();
+*/
+
+
+/*
+console.log({});
+console.log([]);
+
+var arr1 = [1, 2, [3, 4]];
+console.log(arr1) 
+console.log(arr1.flat())
+
+var arr2 = [[1, 2, 3], [4, 5, 6]]
+console.log(arr2) 
+console.log(arr2.flat())
+
+var arr3 = [[[1, 2], 3], [4, 5, 6]]
+console.log(arr3) 
+console.log(arr3.flat())
+*/
