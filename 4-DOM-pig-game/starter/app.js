caa387ef3f3385a1871842f80ab0ba41193e779a
mrutyunjayan <jaikrishnanarun@hotmail.com>
2020-02-17 13:48:29 -0800

    Finshed lecture #52

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,11 +9,28 @@ GAME RULES:
 
 */
 var scores, roundScore, activePlayer;
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+  //set all the scores to zero
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
 
+  document.querySelector("#name-0").textContent = "Player 1";
+  document.querySelector("#name-1").textContent = "Player 2";
+
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");
+}
+
+init();
 //document.querySelector("#current-" + activePlayer).textContent = dice;
 
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
@@ -23,12 +40,6 @@ console.log(x);
 
 //using query selector to change the css of an object (hide the die)
 //document.querySelector(".dice").style.display = "none";
-
-//set all the scores to zero
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
 
 //when the mouse is clicked on the "roll dice" button, do this:
 document.querySelector(".btn-roll").addEventListener("click", function() {
@@ -90,3 +101,5 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
     changePlayer();
   }
 });
+
+document.querySelector(".btn-new").addEventListener("click", init);
