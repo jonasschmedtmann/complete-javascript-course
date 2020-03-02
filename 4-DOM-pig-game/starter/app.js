89bb2b7c7ec160059916d6eef5fe72f24818c085
jkaddu <jkaddu@yahoo.com>
2020-03-02 07:34:19 -0800

    created functionality for btn-roll

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,18 +9,61 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+// dice = Math.floor(Math.random() * 6) + 1;
+// console.log(dice);
 
-document.querySelector("#current-" + activePlayer).textContent = dice;
+// document.querySelector("#current-" + activePlayer).textContent = dice;
 // document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "<em>";
 
-var x = document.querySelector("#score-0").textContent;
-console.log(x);
-document.querySelector(".dice").style.display = "none";
+// var x = document.querySelector("#score-0").textContent;
+// document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  //1. Random Number
+  var dice = Math.floor(Math.random() * 6) + 1;
+  //2. Display the result
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+
+  //3. Update roundScore IF the number is not 1
+  if (dice !== 1) {
+    // add score
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    // Next Player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+    // toggle allows you to go back and forth from player to player
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    // add and remove will allow you to go one way, I prefer toggle
+    // document.querySelector(".player-0-panel").classList.remove("active");
+    // document.querySelector(".player-1-panel").classList.add("active");
+
+    document.querySelector(".dice").style.display = "none";
+  }
+});
+
+document.querySelector(".btn-hold").addEventListener("click", function() {
+  // add current score to global score
+  scores[activePlayer] += roundScore;
+  // update the UI
+  document.querySelector("#score-" + activePlayer).textContent =
+    scores[activePlayer];
+  // check if player won the game
+});
