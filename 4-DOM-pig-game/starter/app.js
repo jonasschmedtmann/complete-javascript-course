baa5f3b315a1a49724ae97b6ec7bc2f7c1683d40
jkaddu <jkaddu@yahoo.com>
2020-03-03 08:48:04 -0800

    finished dice game

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,51 +9,77 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
+var lastDice;
 // how to add HTML via JS
 // document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "<em>";
 
 document.querySelector(".btn-roll").addEventListener("click", function() {
-  //1. Random Number
-  var dice = Math.floor(Math.random() * 6) + 1;
-  //2. Display the result
-  var diceDOM = document.querySelector(".dice");
-  diceDOM.style.display = "block";
-  diceDOM.src = "dice-" + dice + ".png";
+  if (gamePlaying) {
+    //1. Random Number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    //2. Display the result
+    var diceDOM = document.querySelector(".dice");
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-" + dice + ".png";
 
-  //3. Update roundScore IF the number is not 1
-  if (dice !== 1) {
-    // add score
-    roundScore += dice;
-    document.querySelector("#current-" + activePlayer).textContent = roundScore;
-  } else {
-    // Next player
-    nextPlayer();
+    //3. Update roundScore IF the number is not 1
+    if (dice === 6 && lastDice === 6) {
+      // total score goes to zero if two 6s are rolled
+      scores[activePlayer] = 0;
+      document.querySelector("#score-" + activePlayer).textContent = "0";
+      nextPlayer();
+    } else if (dice !== 1) {
+      // add score
+      roundScore += dice;
+      document.querySelector(
+        "#current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      // Next player
+      nextPlayer();
+    }
+
+    lastDice = dice;
   }
 });
 
 document.querySelector(".btn-hold").addEventListener("click", function() {
-  // add current score to global score
-  scores[activePlayer] += roundScore;
-  // update the UI
-  document.querySelector("#score-" + activePlayer).textContent =
-    scores[activePlayer];
-  // check if player won the game
-  if (scores[activePlayer] >= 10) {
-    document.querySelector("#name-" + activePlayer).textContent = "Winner!";
-    document.querySelector(".dice").style.display = "none";
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.add("winner");
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.remove("active");
-  } else {
-    // Next player
-    nextPlayer();
+  if (gamePlaying) {
+    // add current score to global score
+    scores[activePlayer] += roundScore;
+
+    // update the UI
+    document.querySelector("#score-" + activePlayer).textContent =
+      scores[activePlayer];
+    var input = document.querySelector(".final-score").value;
+    var winningScore;
+    // undefined, 0, null or '' are coerced to false
+    // anything else is coerced to true
+    if (input) {
+      winningScore = input;
+    } else {
+      winningScore = 100;
+    }
+
+    // check if player won the game
+    if (scores[activePlayer] >= winningScore) {
+      document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+      document.querySelector(".dice").style.display = "none";
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+      gamePlaying = false;
+    } else {
+      // Next player
+      nextPlayer();
+    }
   }
 });
 
@@ -63,8 +89,10 @@ function init() {
   scores = [0, 0];
   activePlayer = 0;
   roundScore = 0;
+  gamePlaying = true;
 
   document.querySelector(".dice").style.display = "none";
+  document.querySelector(".final-score").value = "";
 
   document.getElementById("score-0").textContent = "0";
   document.getElementById("score-1").textContent = "0";
@@ -89,7 +117,7 @@ function nextPlayer() {
   // toggle allows you to go back and forth from player to player
   document.querySelector(".player-0-panel").classList.toggle("active");
   document.querySelector(".player-1-panel").classList.toggle("active");
-  // add and remove will allow you to go one way, I prefer toggle
+  // add and remove will allow one action, I prefer toggle
   // document.querySelector(".player-0-panel").classList.remove("active");
   // document.querySelector(".player-1-panel").classList.add("active");
 
