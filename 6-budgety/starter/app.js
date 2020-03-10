f726d170ca6fe5dfbffe763a0ad85fb3f049ff0c
Sai Bala Krishna Allamsetty <sbk888.sbk@gmail.com>
2020-03-10 19:35:06 +0530

    Created Controllers' structure

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,1 +1,39 @@
-sgsfgsfgsf
+//BudgetContoller
+var budgetController = (function() {// THis is an immediately invoked function expression
+
+    var x = 23;
+
+    var add = function(a){
+        return x + a;
+    }
+
+    return {
+        publicTest : function(b) { 
+          return add(b);
+        }
+    
+    }
+})();
+
+
+//UI Controller
+var UIController = (function() {
+
+
+    // Some Code
+
+
+})();
+
+
+//AppController
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var z = budgetCtrl.publicTest(5);
+
+    return {
+        anotherPublic : function(){
+            console.log(z);
+        }
+    }
+})(budgetController, UIController);
