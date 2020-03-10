3ef963c7b3480f9da9652da5f0773a1a273e7f14
Sai Bala Krishna Allamsetty <sbk888.sbk@gmail.com>
2020-03-10 22:16:26 +0530

    Reading input data

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,39 +1,54 @@
-//BudgetContoller
+// BUDGET CONTROLLER
 var budgetController = (function() {// THis is an immediately invoked function expression
-
-    var x = 23;
-
-    var add = function(a){
-        return x + a;
-    }
-
-    return {
-        publicTest : function(b) { 
-          return add(b);
-        }
-    
-    }
-})();
-
-
-//UI Controller
-var UIController = (function() {
-
 
     // Some Code
 
+})();
+
+
+// UI CONTROLLER
+var UIController = (function() {
+
+    return {
+        getinput : function () {
+            
+        }
+    }
 
 })();
 
 
-//AppController
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function (){
 
-    return {
-        anotherPublic : function(){
-            console.log(z);
-        }
+        //1. Get the field input data
+
+        //2. Add the item to the budget controller
+        
+        //3. Add the item to the UI
+        
+        //4. Calculate the budget
+        
+        //5. Display the budget on UI
+        console.log("It works")
     }
+
+    //EVENT LISTERNERS
+    // 1. Button click event
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+
+    // 2. Global Event Listener for return on enter key
+    document.addEventListener('keypress', function (event) {
+
+        // Filtering the event using keyCode property for keyboard input
+        if (event.keyCode === 13 || event.which === 13){
+            console.log("Enter was pressed");
+            ctrlAddItem();
+        }
+
+    });
+
+
 })(budgetController, UIController);
