55d82d9a752fcd8da86b191b5a5346d1d31f905b
jkaddu <jkaddu@yahoo.com>
2020-03-16 12:52:25 -0700

    created if/else to make html string in UIController

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -77,6 +77,23 @@ var UIController = (function() {
         value: document.querySelector(DOMStrings.inputValue).value
       };
     },
+    addListItem: function(obj, type) {
+      var html, newHtml;
+      // Create HTML string with placeholder text
+      if (type === "inc") {
+        html =
+          '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else if (type === "exp") {
+        html =
+          '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+
+      // Replace placeholder with data
+      newHtml = html.replace("%id%", obj.id);
+      newHtml = newHtml.replace("%description%", obj.description);
+      newHtml = newHtml.replace("%value%", obj.value);
+      // Insert HTML into the DOM
+    },
     getDOMStrings: function() {
       return DOMStrings;
     }
