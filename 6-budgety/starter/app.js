a687becfc20f7238db064f47ea1a4e9454568e20
jkaddu <jkaddu@yahoo.com>
2020-03-16 07:56:43 -0700

    destructed class values into DOMStrings in the UIController and DOM in the controller

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,11 +1,59 @@
+//Budget Controller
 var budgetController = (function() {
   // code
 })();
 
-var UserInterfaceController = (function() {
-  // code
+//UI Controller
+var UIController = (function() {
+  var DOMStrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
+  };
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value, // will be inc value or exp value
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value
+      };
+    },
+    getDOMStrings: function() {
+      return DOMStrings;
+    }
+  };
 })();
 
-var controller = (function(budgetCtrl, UserInterfaceCtrl) {
-  // code
-})(budgetController, UserInterfaceController);
+//Global App Controller
+var controller = (function(budgetCtrl, UICtrl) {
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMStrings();
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+
+    document.addEventListener("keypress", function(event) {
+      if (event.keycode === 13 || event.which === 13) {
+        console.log("Enter was pressed");
+      }
+    });
+  };
+
+  var ctrlAddItem = function() {
+    // 1. get input data
+    var input = UICtrl.getInput();
+    // 2. add item to budget
+    // 3. add item to UI
+    // 4. calculate the budget
+    // 5. display budget
+  };
+
+  return {
+    init: function() {
+      console.log("App has started");
+      setupEventListeners();
+    }
+  };
+})(budgetController, UIController);
+
+controller.init();
