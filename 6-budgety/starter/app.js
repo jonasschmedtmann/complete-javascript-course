5454ec9bf7ecd940aebb5a54c00ac295d9a8ed0c
Zack Bumpous <upwork.ztb@gmail.com>
2020-03-16 22:09:31 -0500

    Used a technique for adding big chunks of HTML into the DOM, how to replace parts of strings useing the replace() method, and how to do DOM manipulation using the insertAdjacentHTML method.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -143,7 +143,9 @@ var UIController = (function () {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expenseContainer: '.expenses__list'
     }
 
 
@@ -158,6 +160,29 @@ var UIController = (function () {
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value,
             }
+        },
+
+        addListItem: function(obj, type){
+            var html, newHtml, element;
+            // Create HTML string w placeholder text
+            if (type === 'inc'){
+                element = DOMstrings.incomeContainer;
+
+                html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i> </button> </div> </div> </div>';
+            } else if (type === 'exp'){
+                element = DOMstrings.expenseContainer;
+
+                html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div <div class="item__value">%value%</div> <div <div class="item__delete"> <button class="item__delete--btn"> <i class="ion-ios-close-outline"> </i> </button> </div> </div> </div>';
+            }
+
+            // Replace the placeholder text with some actual data
+            // replace() searches for a string and then replaces it with the data we put in the method
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
         getDOMstrings: function(){ // this exposes DOMstrings to the public so we can use them elsewhere
@@ -198,7 +223,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
-
+        UICtrl.addListItem(newItem, input.type);
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
