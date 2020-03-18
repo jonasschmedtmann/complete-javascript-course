f0ba5e70f45365bc84ddc1b691c0135893937830
jkaddu <jkaddu@yahoo.com>
2020-03-17 22:15:23 -0700

    arrow functions

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -70,7 +70,7 @@ console.log(i);
 */
 //////////////////////////////////////////////
 // Lecture: Strings
-
+/*
 let firstName = "John";
 let lastName = "Smith";
 const yearOfBirth = 1990;
@@ -107,3 +107,31 @@ console.log(n.endsWith("th"));
 console.log(n.includes("oh"));
 
 console.log(`${firstName} `.repeat(3));
+*/
+
+////////////////////////////////////////
+// Lecture: Arrow Functions
+
+const years = [1990, 1989, 1992, 1938];
+
+//ES5
+var ages5 = years.map(function(el) {
+  return 2020 - el;
+});
+console.log(ages5);
+
+//ES6
+
+let ages6 = years.map(el => 2020 - el);
+console.log(ages6);
+
+// with 2 or more arguments you need parentheses
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2020 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+  const now = new Date().getFullYear();
+  const age = now - el;
+  return `Age element ${index + 1}: ${age}`;
+});
+console.log(ages6);
