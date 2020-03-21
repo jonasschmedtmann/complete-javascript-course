a70a61fb5a4da33a748046a9f74a2ac4f0912958
Zack Bumpous <upwork.ztb@gmail.com>
2020-03-20 21:41:12 -0500

    Updated the budgetController.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -164,7 +164,7 @@ var UIController = (function () {
             return {
                 type: document.querySelector(DOMstrings.inputType).value, // inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value,
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             }
         },
 
@@ -231,15 +231,23 @@ var controller = (function (budgetCtrl, UICtrl) {
         });
     };
 
+    var updateBudget = function(){
+        // 1. Calculate the budget
 
+        // 2. Return the budget
+
+        // 3. Display the budget on the UI
+        // console.log('It works.') // tests for clicking button or key down on ENTER key
+    }
 
     var ctrlAddItem = function(){
         var input, newItem;
 
         // 1. Get the filled input data (calling the getInput method in UIController)
         input = UICtrl.getInput();
-        console.log(input);
+        // console.log(input);
 
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0){
         // 2. Add the item to the budget controller
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
@@ -249,10 +257,9 @@ var controller = (function (budgetCtrl, UICtrl) {
         // 4. Clear the fields
         UICtrl.clearFields();
 
-        // 5. Calculate the budget
-
-        // 6. Display the budget on the UI
-       // console.log('It works.') // tests for clicking button or key down on ENTER key
+        // 5. Calculate and update budget
+        updateBudget();
+        }      
     };
     
 
