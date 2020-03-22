85da3edd3a5115d7518e69df5d1b4eddd057fd57
jcbhass <47954686+jcbhass@users.noreply.github.com>
2020-03-21 22:28:42 -0400

    Budget App starter

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,21 +1,35 @@
 //Want to have modules to keep separate, independent pieces of code, together in organized units.
 
-
+// BUDGET CONTROLLER
 var budgetController = (function() {
-    var x = 23;
-    var add = function(a) {
-        return x + a;
-    }
 
-    return {
-        publicTest: function(b) {
-            console.log(add(b));
-        }
-    }
 })(); //All of this works because of closures. This is an IIFE.
 
-
+// UI CONTROLLER
 var UIController = (function() {
-    //Some code
+    //some code
 })();
 
+// GLOBALL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+    var ctrlAddItem = function() {
+    //1. Get the filed input data
+    //2. Add the item to the budget controller
+    //3. Add the item to the UO 
+    //4. Calculate the budget
+    //5. Display the budget on the UI
+    console.log("It works");
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+        //keycode tells which key is pressed when check object in the console. 13 is keycode for enter.
+        //which is for older browsers
+   });
+
+   
+})(budgetController, UIController);
--- 9-forkify/final/src/js/models/Search.js
+++ 9-forkify/final/src/js/models/Search.js
@@ -8,7 +8,7 @@ export default class Search {
 
     async getResults() {
         try {
-            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
             this.result = res.data.recipes;
             // console.log(this.result);
         } catch (error) {
