6d14ed1a7b7a15a487cce22d05d29473abb6df58
jcbhass <47954686+jcbhass@users.noreply.github.com>
2020-03-23 20:44:46 -0400

    Working on budget app

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -3,36 +3,95 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
 
+    var Expense = function(id, description, value) {
+        this.id = id; 
+        this.description = description;
+        this.value = value;
+    }
+
+    var Income = function(id, description, value) {
+        this.id = id; 
+        this.description = description;
+        this.value = value;
+    }
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        }, 
+        totals: {
+            exp: 0, 
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem;
+ 
+            newItem = new Expense(ID, des, val); 
+
+        }
+    };
 })(); //All of this works because of closures. This is an IIFE.
 
 // UI CONTROLLER
 var UIController = (function() {
-    //some code
+
+    var DOMstrings = {
+        inputType: '.add__type', 
+        inputDescription: '.add__description', 
+        inputValue: '.add__value', 
+        inputBtn: '.add__btn'
+    }
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        }, 
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };  
 })();
 
 // GLOBALL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+            //keycode tells which key is pressed when check object in the console. 13 is keycode for enter.
+            //which is for older browsers
+        });
+    };
+    
+
+
     var ctrlAddItem = function() {
-    //1. Get the filed input data
-    //2. Add the item to the budget controller
-    //3. Add the item to the UO 
-    //4. Calculate the budget
-    //5. Display the budget on the UI
-    console.log("It works");
-    }
+        //1. Get the filed input data
+        var input = UICtrl.getInput();
+        //2. Add the item to the budget controller
+        //3. Add the item to the UO 
+        //4. Calculate the budget
+        //5. Display the budget on the UI
+    };
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();
         }
-        //keycode tells which key is pressed when check object in the console. 13 is keycode for enter.
-        //which is for older browsers
-
-
-        //Must make sure the changes are taking affect. 
-   });
-
+    }
    
 })(budgetController, UIController);
+
+controller.init();
