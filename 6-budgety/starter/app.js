8547eb9245cd949f5f40f1dfef5e2ef7eda990b3
Zack Bumpous <upwork.ztb@gmail.com>
2020-03-23 19:34:25 -0500

    Added a calculateBudget method and getBudget method into the budgetCtrl, stored those methods in a variable updateBudget inside the controller.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -92,7 +92,13 @@ var budgetController = (function () {
         this.value = value;
     };
 
-
+    var calculateTotal = function(type){
+        var sum = 0;
+        data.allItems[type].forEach(function(cur){
+            sum += cur.value;
+        });
+        data.totals[type] = sum;
+    }
 
     var data = {
         // where we will store expenses and incomes
@@ -103,7 +109,9 @@ var budgetController = (function () {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
 
     return {
@@ -131,6 +139,29 @@ var budgetController = (function () {
             // Return the new element
             return newItem;
 
+        },
+
+        calculateBudget: function(){
+
+            // calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            // calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+
+
+            // calculate % of income that we spent
+            data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+        },
+
+        getBudget: function(){
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
         },
 
         testing: function(){
@@ -233,8 +264,10 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     var updateBudget = function(){
         // 1. Calculate the budget
+        budgetCtrl.calculateBudget();
 
         // 2. Return the budget
+        var budget = budgetCtrl.getBudget();
 
         // 3. Display the budget on the UI
         // console.log('It works.') // tests for clicking button or key down on ENTER key
