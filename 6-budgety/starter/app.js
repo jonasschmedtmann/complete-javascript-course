e49b3b1206e2b3e2a920461c731635f3bb4e01cf
Zack Bumpous <upwork.ztb@gmail.com>
2020-03-25 20:28:54 -0500

    Added four new items to the DOMstrings object, making it easier to grab the HTML elements by classname. So now the Available budget updates as you feed it new data, originates at a value of 0, and so does the label for Income and Expenses, as well as the % of income spent will update.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -186,7 +186,11 @@ var UIController = (function () {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expenseContainer: '.expenses__list'
+        expenseContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     }
 
 
@@ -241,6 +245,19 @@ var UIController = (function () {
             fieldsArr[0].focus();
         },
 
+        displayBudget: function(obj){
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+
+            if (obj.percentage > 0){
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
+        },
+
         getDOMstrings: function(){ // this exposes DOMstrings to the public so we can use them elsewhere
             return DOMstrings;
         }
@@ -274,7 +291,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         var budget = budgetCtrl.getBudget();
 
         // 3. Display the budget on the UI
-        console.log(budget);
+        UICtrl.displayBudget(budget);
         // console.log('It works.') // tests for clicking button or key down on ENTER key
     }
 
@@ -304,6 +321,12 @@ var controller = (function (budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Application has started.');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
             setupEventListeners();
         }
     };
