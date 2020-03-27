099a911ad2d4d0e6b56ee8ae52e5070d806499ee
Zack Bumpous <upwork.ztb@gmail.com>
2020-03-26 22:39:47 -0500

    First I set an eventListener on the container, which is the first element that all of the income and expense items have in common, and I did it this way to set up event delegation so I only had to add the one eventListener instead of to all of the children elements. We added the ctrlDeleteItem function to the eventListener, and from there we read the target element and traversed the DOM all the way up to the element we are interested in and retrieved the ID from it and stored it in the itemID variable. In there is the item type aka expense or income, and the item ID. We isolated these items using the split method. So the stage is set so we can delete an item from the data structure and the UI.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -190,7 +190,8 @@ var UIController = (function () {
         budgetLabel: '.budget__value',
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
-        percentageLabel: '.budget__expenses--percentage'
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container'
     }
 
 
@@ -213,7 +214,7 @@ var UIController = (function () {
             if (type === 'inc'){
                 element = DOMstrings.incomeContainer;
 
-                html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i> </button> </div> </div> </div>';
+                html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i> </button> </div> </div> </div>';
             } else if (type === 'exp'){
                 element = DOMstrings.expenseContainer;
 
@@ -270,6 +271,8 @@ var UIController = (function () {
 // budgetCtrl = budgetController // UICtrl = UIController
 var controller = (function (budgetCtrl, UICtrl) {
 
+    
+
     var setupEventListeners = function() {
         var DOM = UICtrl.getDOMstrings();
 
@@ -281,6 +284,8 @@ var controller = (function (budgetCtrl, UICtrl) {
                 ctrlAddItem();
             }
         });
+
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
     };
 
     var updateBudget = function(){
@@ -316,6 +321,27 @@ var controller = (function (budgetCtrl, UICtrl) {
         updateBudget();
         }      
     };
+
+
+
+    var ctrlDeleteItem = function(event){
+        var itemID, splitID, type, ID;
+
+        itemId = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+        if (itemID){
+            splitID = itemID.split('-');
+            type = splitID[0];
+            ID = splitID[1];
+
+            // 1. delete the item from the data structure
+
+            // 2. delete the item from the ui
+
+            // 3. update and show the new budget
+        }
+    }
+
     
 
     return {
