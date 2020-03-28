6669e7dabef3c66d3d743628c544b3a0f3dd99d2
Zack Bumpous <upwork.ztb@gmail.com>
2020-03-27 21:14:53 -0500

    Added the ability to delete an item from the data object, so when you hover over the income or expense and you can click the X, it won't look deleted in the UI, but inside the data structure we can see that it has been deleted from both an exp and the inc arrays.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -140,6 +140,25 @@ var budgetController = (function () {
             return newItem;
 
         },
+        deleteItem: function(type, id) {
+            var ids, index;
+            
+            // id = 6
+            //data.allItems[type][id];
+            // ids = [1 2 4  8]
+            //index = 3
+            
+            ids = data.allItems[type].map(function(current) {
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+            
+        },
 
         calculateBudget: function(){
 
@@ -324,17 +343,20 @@ var controller = (function (budgetCtrl, UICtrl) {
 
 
 
-    var ctrlDeleteItem = function(event){
+    var ctrlDeleteItem = function(event) {
         var itemID, splitID, type, ID;
-
-        itemId = event.target.parentNode.parentNode.parentNode.parentNode.id;
-
-        if (itemID){
+        
+        itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+        
+        if (itemID) {
+            
+            //inc-1
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
-
+            ID = parseInt(splitID[1]);
+            
             // 1. delete the item from the data structure
+            budgetCtrl.deleteItem(type, ID);
 
             // 2. delete the item from the ui
 
