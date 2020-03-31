7f1c641786907ec658d2f2482a565e0297f7f1dd
Zack Bumpous <upwork.ztb@gmail.com>
2020-03-31 14:48:28 -0500

    Our budget numbers are formatted now. Used methods on strings to achieve this and used the substr method to format so that any number would have the correct comma and decimals where the should be regardless of number size.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -242,7 +242,31 @@ var UIController = (function () {
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
         expensesPercLabel: '.item__percentage'
-    }
+    };
+
+    var formatNumber = function(num, type){
+        /*
+        + or - before number exactly 2 decimal points comma separating the thousands
+
+        2310.4567 -> + 2,310.46
+
+        2000 -> + 2,000.00
+        */
+       var numSplit, int, dec, type;
+        
+        num = Math.abs(num);
+        num = num.toFixed(2);
+
+        numSplit = num.split('.');
+
+        int = numSplit[0];
+        if (int.length > 3){
+            int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, int.length)
+        }
+        dec = numSplit[1];
+
+        return (type === 'exp' ? '-' : '+') + ' ' + int + "." + dec
+    };
 
 
 
@@ -275,7 +299,7 @@ var UIController = (function () {
             // replace() searches for a string and then replaces it with the data we put in the method
             newHtml = html.replace('%id%', obj.id);
             newHtml = newHtml.replace('%description%', obj.description);
-            newHtml = newHtml.replace('%value%', obj.value);
+            newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
 
             // Insert the HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
@@ -303,9 +327,13 @@ var UIController = (function () {
         },
 
         displayBudget: function(obj){
-            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+            var type;
+            obj.budget > 0 ? type = 'inc' : 'exp';
+
+            document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
+            document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'inc');
+            document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExp, 'exp');
 
 
             if (obj.percentage > 0){
@@ -333,6 +361,8 @@ var UIController = (function () {
                 }
             })
         },
+
+        
 
         getDOMstrings: function(){ // this exposes DOMstrings to the public so we can use them elsewhere
             return DOMstrings;
