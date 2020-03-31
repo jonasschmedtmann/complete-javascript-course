6cc83f7e8c801c7460fec12372a70303486a309b
Zack Bumpous <upwork.ztb@gmail.com>
2020-03-31 12:54:05 -0500

    Percentages now update on the UI.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -240,7 +240,8 @@ var UIController = (function () {
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
     }
 
 
@@ -313,6 +314,25 @@ var UIController = (function () {
                 document.querySelector(DOMstrings.percentageLabel).textContent = '---';
             }
         },
+        
+        displayPercentages: function(percentages){
+            var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+            var nodeListForEach = function(list, callback){
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+            };
+
+            nodeListForEach(fields, function(current, index){
+
+                if (percentages[index] > 0){
+                    current.textContent = percentages[index] + '%';
+                } else {
+                    current.textContent = '---'
+                }
+            })
+        },
 
         getDOMstrings: function(){ // this exposes DOMstrings to the public so we can use them elsewhere
             return DOMstrings;
@@ -364,7 +384,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         var percentages = budgetCtrl.getPercentages();
 
         // 3. Display the % on the UI
-        console.log(percentages);
+        UICtrl.displayPercentages(percentages);
     }
 
     var ctrlAddItem = function(){
