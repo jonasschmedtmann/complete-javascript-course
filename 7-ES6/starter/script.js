df696d109167c313a75170d42a09730eea9f2169
Zack Bumpous <upwork.ztb@gmail.com>
2020-04-02 21:49:13 -0500

    Learned how to use a block introduced in ES6 and showed its power to provide data privacy, and compared it to the ES5 way of using an IIFE which was a good refresher.

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -1,4 +1,4 @@
-// Lecture: let and const
+// Lecture 1: let and const
 
 
 // // ES5
@@ -17,51 +17,51 @@
 
 
 // ES5
-function driversLicense5(passedTest){
-    if (passedTest){
-        console.log(firstName); // undefined
+// function driversLicense5(passedTest){
+//     if (passedTest){
+//         console.log(firstName); // undefined
 
-        var firstName = 'John'; // function scoped (var)
-        var yearOfBirth = 1990; // function scoped (var)
+//         var firstName = 'John'; // function scoped (var)
+//         var yearOfBirth = 1990; // function scoped (var)
 
-        // console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car')
-    }
-    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car')
+//         // console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car')
+//     }
+//     console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car')
 
-}
+// }
 
-driversLicense5(true);
-
-
-// ES6
-
-function driversLicense6(passedTest){
-    // console.log(firstName); these both print errors
-    // console.log(yearOfBirth);
-
-    let firstName;
-    const yearOfBirth = 1990;
-    if (passedTest){
-        firstName = 'John'; // block scoped (let)
-        yearOfBirth; // block scoped (let)
-
-        // console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car')
-    }
-    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car')
-}
-driversLicense6(true);
+// driversLicense5(true);
 
 
+// // ES6
+
+// function driversLicense6(passedTest){
+//     // console.log(firstName); these both print errors
+//     // console.log(yearOfBirth);
+
+//     let firstName;
+//     const yearOfBirth = 1990;
+//     if (passedTest){
+//         firstName = 'John'; // block scoped (let)
+//         yearOfBirth; // block scoped (let)
+
+//         // console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car')
+//     }
+//     console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car')
+// }
+// driversLicense6(true);
 
 
 
-let i = 23; // block scoped
 
-for (let i = 0; i < 5; i++){
-    console.log(i); // console logs 0 through 5 (i is block scoped)
-}
 
-console.log(i); // will console log 23
+// let i = 23; // block scoped
+
+// for (let i = 0; i < 5; i++){
+//     console.log(i); // console logs 0 through 5 (i is block scoped)
+// }
+
+// console.log(i); // will console log 23
 
 
 
@@ -76,3 +76,35 @@ for (var i = 0; i < 5; i++){
 console.log(i); // will console log 23
 
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Lecture 2: Blocks and IIFEs (iffys)
+
+// this is a block {}
+{
+    const a = 1;
+    let b = 2;
+    var d = 4
+}
+
+console.log(a + b); // the values are undefined, bc they are block scoped, this is some data privacy, kind of like an IIFE but much easier to write.
+console.log(d); // function scoped so will be defined
+
+
+// ES5 IIFE
+(function() {
+    var c = 3;
+})();
+console.log(c) // value undefined, bc we used an IIFE on an var which is function scoped. A lot more code for data privacy.
