fad66496108929a3a9518e670a574388370ab3eb
Zack Bumpous <upwork.ztb@gmail.com>
2020-04-10 11:24:08 -0500

    Learned about adding subclasses to classes. Achieved the same results in ES5 and ES6.

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -606,6 +606,7 @@ console.log(question.get(ans === question.get('correct')));
 
 
 // ES5
+/*
 var Person5 = function (name, yearOfBirth, job){
     this.name = name;
     this.yearOfBirth = yearOfBirth;
@@ -613,7 +614,7 @@ var Person5 = function (name, yearOfBirth, job){
 }
 
 Person5.prototype.calculateAge = function(){
-    var age = new Date().getFullYear - this.yearOfBirth;
+    var age = new Date().getFullYear() - this.yearOfBirth;
     console.log(age);
 }
 var john5 = new Person5('Johny boy ES5', 1990, 'teacher');
@@ -627,7 +628,7 @@ class Person6 {
         this.job = job;
     }
     calculateAge(){
-        var age = new Date().getFullYear - this.yearOfBirth;
+        var age = new Date().getFullYear() - this.yearOfBirth;
         console.log(age);
     }
 
@@ -640,3 +641,79 @@ var john6 = new Person6('Johny boy ES6', 1990, 'teacher');
 
 
 Person6.greeting();
+*/
+
+
+
+
+
+
+
+
+
+// Lesson 13: Classes with Subclasses
+
+// ES5
+var Person5 = function (name, yearOfBirth, job){
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person5.prototype.calculateAge = function(){
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+}
+// create the subclass
+var Athlete5 = function(name, yearOfBirth, job, olympicGames, medals){
+    //call the super class
+    Person5.call(this, name, yearOfBirth, job);
+    this.olympicGames = olympicGames;
+    this.medals = medals;
+}
+
+Athlete5.prototype = Object.create(Person5.prototype);
+
+Athlete5.prototype.wonMedal = function(){
+    this.medals++;
+    console.log(this.medals)
+}
+
+var johnAthlete5 = new Athlete5('John', 1990, 'swimmer', 3, 10);
+
+johnAthlete5.calculateAge();
+johnAthlete5.wonMedal();
+
+
+
+// ES6 -- adding/creating the subclasses for Person6
+class Person6 {
+    constructor (name, yearOfBirth, job){
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+    calculateAge(){
+        var age = new Date().getFullYear() - this.yearOfBirth;
+        console.log(age);
+    }
+}
+
+class Athlete6 extends Person6 {
+    constructor(name, yearOfBirth, job, olympicGames, medals){
+        super(name, yearOfBirth, job); // calls the super, don't have to manually set the this variable
+        this.olympicGames = olympicGames;
+        this.medals = medals;
+    }
+
+    //add method
+    wonMedal(){
+        this.medals++;
+        console.log(this.medals);
+    }
+}
+
+const zackAthlete6 = new Athlete6('zack', 1997, 'swimmer', 2, 7);
+
+zackAthlete6.wonMedal();
+zackAthlete6.calculateAge();
