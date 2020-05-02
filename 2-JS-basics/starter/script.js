ebcaa9ca0387fa809d4031d4d4d01d8e24670f4b
Marie Serykh <marie.serykh@gmail.com>
2020-05-02 13:40:56 +0200

    Basic operators

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -25,7 +25,7 @@ var function = 34;
 /********************************
 * Varible mutation and type coercion
 */
-
+/*
 var firstName = 'John';
 var age = 30;
 
@@ -47,3 +47,34 @@ alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he marries? ' + is
 
 var lastName = prompt('What is his last name');
 console.log(lastName);
+*/
+
+/****************************************
+ * Basic Operators
+ */
+
+ var currentYear, yearJohn, yearMark, ageJohn, ageMark;
+currentYear = 2020;
+ageJohn = 30;
+ageMark = 35;
+
+// Math operators
+yearJohn = currentYear - 30;
+yearMark = currentYear - 35;
+console.log(yearJohn);
+
+console.log(currentYear + 2);
+console.log(currentYear /12 );
+console.log(currentYear * 2);
+
+// Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// typeof operatos
+console.log(typeof johnOlder);
+console.log(typeof ageMark);
+
+var x;
+console.log(typeof x);
+
