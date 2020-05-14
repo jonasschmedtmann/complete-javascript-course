260c29bb849bccc0c44079e1189c42575006e9c4
sbash <sbash@hy-vee.com>
2020-05-14 17:07:54 -0500

    complete lesson 67.

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -23,7 +23,7 @@ mark.calculageAge();
 console.log(shawn.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
-*/
+
 
 // Object.create
 var personProto = {
@@ -76,9 +76,95 @@ function change(a, b) {
     b.city = 'San Francisco';
 }
 
-change(age, obj);
-console.log(age);
-console.log(obj.city);
+// change(age, obj);
+// console.log(age);
+// console.log(obj.city);
+*/
+
+/*// Lecture: Passing functions as arguments
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2020 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.8 - (0.67 * el));
+    }
+    return -1;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullAges);
+console.log(rates);*/
+
+/*// Lecture: Functions returning functions
+function interviewQuestions(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(`${name} can you please explain what UX design is?`)
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log(`What subject do you teach ${name}?`)
+        }
+    } else {
+        return function(name) {
+            console.log(`Hello ${name}, what do you do?`)
+        }
+     }
+}
+
+var teacherQuestion = interviewQuestions('teacher');
+var designerQuestion = interviewQuestions('designer');
+var consultantQuestion = interviewQuestions('consultant')
+teacherQuestion('Shawn');
+designerQuestion('Michelle');
+consultantQuestion('Frank');
+//calling inline
+interviewQuestions('bum')('Joe')
+*/
+
+
+// Lecture: IIFE (Immediate Invoked Function Expression)
+// function game() {
+//     var score = Math.random() * 10;
+//     console.log(score >= 5);
+// }
+// game();
+
+(function () {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+})();
+
+(function (goodLuck) {
+    var score = Math.random() * 10;
+    console.log(score >= 5 - goodLuck);
+})(5);
+
+
+
+
+
+
 
 
 
