774e45fc87cea2c729cc8e519cdd38c9f19d966a
shawn <dboybash@gmail.com>
2020-05-18 07:00:32 -0500

    finish lession 68.

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -143,6 +143,7 @@ interviewQuestions('bum')('Joe')
 */
 
 
+/*
 // Lecture: IIFE (Immediate Invoked Function Expression)
 // function game() {
 //     var score = Math.random() * 10;
@@ -159,10 +160,79 @@ interviewQuestions('bum')('Joe')
     var score = Math.random() * 10;
     console.log(score >= 5 - goodLuck);
 })(5);
+*/
 
 
+/*// Lecture: Closures
+
+function retirement(retirementAge) {
+    var a = ' years left until retirement';
+    return function (yearOfBirth) {
+        var age = 2020 - yearOfBirth;
+        console.log(retirementAge - age + a);
+    }
+}
+
+var retirementUS = retirement(66);
+retirementUS(1980);
+//retirement(66)(1980);
+
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementGermany(1980);
+retirementIceland(1980);
+
+/!*function interviewQuestions(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(`${name} can you please explain what UX design is?`)
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log(`What subject do you teach ${name}?`)
+        }
+    } else {
+        return function(name) {
+            console.log(`Hello ${name}, what do you do?`)
+        }
+    }
+}*!/
+
+function interviewQuestion(job) {
+    return function (name) {
+        switch (job) {
+            case 'designer':
+                console.log(`${name} can you please explain what UX design is?`)
+                break;
+            case 'teacher':
+                console.log(`What subject do you teach ${name}?`);
+                break;
+            default:
+                console.log(`Hello ${name}, what do you do?`);
+        }
+    }
+}
+
+interviewQuestion('teacher')('Kelly');
+interviewQuestion('designer')('Mark');
+interviewQuestion('bum')('Joe');*/
 
 
+// Lecture: Bind, call and apply
+
+var john = {
+    name: 'John',
+    age: 26,
+    job: 'teacher',
+    presentation: function(style, timeOfDay) {
+        if (style === 'formal') {
+            console.log();
+        } else if (style === 'friendly') {
+            console.log();
+        }
+    }
+}
 
 
 
