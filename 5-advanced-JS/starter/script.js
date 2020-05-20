f39abf876e0df5f5afc7675e21494cee6c4476f7
shawn <dboybash@gmail.com>
2020-05-19 21:32:33 -0500

    finish section-5.

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -219,7 +219,7 @@ interviewQuestion('designer')('Mark');
 interviewQuestion('bum')('Joe');*/
 
 
-// Lecture: Bind, call and apply
+/*// Lecture: Bind, call and apply
 
 var shawn = {
     name: 'Shawn',
@@ -278,13 +278,145 @@ function isFullAge(limit, el) {
 var ages = arrayCalc(years, calculateAge);
 var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
 console.log(ages);
-console.log(fullJapan);
+console.log(fullJapan);*/
 
 
+// Coding Challenge 7
+/*
+--- Let's build a fun quiz game in the console! ---
+Regular
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
 
+2. Create a couple of questions using the constructor
 
+3. Store them all inside an array
 
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
 
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+
+Expert
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
+
+/*
+//Regular
+(function () {
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+
+    Question.prototype.displayQuestion = function () {
+        console.log(this.question);
+
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(`${i}: ${this.answers[i]}`)
+        }
+    }
+
+    Question.prototype.checkAnswer = function (ans) {
+        if (ans === this.correct) {
+            console.log('Correct Answer!')
+        } else {
+            console.log('Wrong Answer...try again....')
+        }
+    }
+
+    var question1 = new Question('What color is my house?', ['white', 'brown', 'green'], 1);
+    var question2 = new Question('Am I the best?', ['yes', 'no'], 0);
+    var question3 = new Question('Where\'s the best place to vacation?', ['Jamaica', 'Staycation', 'Hawaii'], 0);
+
+    var questions = [question1, question2, question3];
+    var random = Math.floor(Math.random() * questions.length);
+
+    questions[random].displayQuestion();
+
+    var answer = parseInt(prompt('Please select the correct answer...'));
+    questions[random].checkAnswer(answer);
+})();*/
+
+//Expert
+(function () {
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+
+    Question.prototype.displayQuestion = function () {
+        console.log(this.question);
+
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(`${i}: ${this.answers[i]}`)
+        }
+    }
+
+    Question.prototype.checkAnswer = function (ans, callback) {
+        var sc;
+        if (ans === this.correct) {
+            console.log('Correct Answer!');
+            sc = callback(true);
+        } else {
+            console.log('Wrong Answer...try again....');
+            sc = callback(false);
+        }
+
+        this.displayScore(sc);
+    }
+
+    Question.prototype.displayScore = function (score) {
+        console.log(`Your current score is ${score}`);
+        console.log('------------------------------');
+    }
+
+    var question1 = new Question('What color is my house?', ['white', 'brown', 'green'], 1);
+    var question2 = new Question('Am I the best?', ['yes', 'no'], 0);
+    var question3 = new Question('Where\'s the best place to vacation?', ['Jamaica', 'Staycation', 'Hawaii'], 0);
+
+    var questions = [question1, question2, question3];
+
+    function score() {
+        var sc = 0;
+        return function (correct) {
+            if(correct) {
+                sc ++;
+            }
+            return sc;
+        }
+    }
+    var keepScore = score();
+
+    function nextQuestion() {
+
+        var random = Math.floor(Math.random() * questions.length);
+
+        questions[random].displayQuestion();
+
+        var answer = prompt('Please select the correct answer...');
+
+        if (answer !== 'exit') {
+            questions[random].checkAnswer(parseInt(answer), keepScore);
+            nextQuestion();
+        }
+    }
+
+    nextQuestion();
+})();
 
 
 
