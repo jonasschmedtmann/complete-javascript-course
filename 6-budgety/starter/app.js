c52b01e3067bc95da030c1f318329068178cb931
shawn <dboybash@gmail.com>
2020-05-25 21:57:24 -0500

    ready for lesson 82.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,6 +1,28 @@
 // BUDGET CONTROLLER
 var budgetController = (function () {
-  // some code
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
+
 })();
 
 // UI CONTROLLER
