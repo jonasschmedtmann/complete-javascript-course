37cf40916c90a11974bc10251278c30ba7e9d184
sbash <sbash@hy-vee.com>
2020-05-25 21:03:47 -0500

    ready for lesson 81.

---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,73 @@
+// BUDGET CONTROLLER
+var budgetController = (function () {
+  // some code
+})();
+
+// UI CONTROLLER
+var uiController = (function () {
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    };
+
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function (budgetCtrl, UICtrl) {
+
+    var setupEventListeners = function () {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn)
+            .addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
+
+    var ctrlAddItem = function () {
+
+        // todo: get the field input data
+        var input = UICtrl.getInput();
+
+        // todo: add the item to the budget controller
+
+        // todo: add the item to the UI
+
+        // todo: calculate the budget
+
+        // todo: display the budget on the UI
+
+    }
+
+    return {
+        init: function () {
+            console.log('app has started.');
+            setupEventListeners();
+        }
+    };
+
+})(budgetController, uiController);
+
+controller.init();
