729de56e0ff0bbf65e778298c5832029c40ffbc2
Charles Norman <charlesrichardnorman@gmail.com>
2020-06-11 22:46:05 -0400

    finished section 48

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -18,9 +18,9 @@ activePlayer = 1;
 dice = Math.floor(Math.random() * 6) + 1;
 
 document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = "<em>" + dice + "</em>";
+document.querySelector('#current-' + activePlayer).innerHTML = "<em>" + dice + "</em>";
 
-var x = document.querySelector("#score-0").textContent;
+var x = document.querySelector("#score-" + activePlayer).textContent;
 console.log(x);
 
 document.querySelector('.dice').style.display = "none";
