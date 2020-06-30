3b5e6a8f8710e22789f4f586f4285ae1da559bd7
shawn <dboybash@gmail.com>
2020-06-29 21:39:44 -0500

    finished lesson 109.

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -171,6 +171,38 @@ console.log(ages6);*/
 // new Person('Harry').myFriends6(friends);
 
 
+// Lecture: Destructuring
+
+//ES5
+var shawn = ['Shawn', 39];
+// var name = shawn[0];
+// var age = shawn[1];
+
+//ES6
+const [name, age] = ['Shawn', 39];
+console.log(name);
+console.log(age);
+
+const obj = {
+    firstName: 'Shawn',
+    lastName: 'Bash'
+};
+const{firstName, lastName} = obj;
+console.log(firstName);
+console.log(lastName);
+
+const {firstName: a, lastName: b} = obj;
+console.log(a);
+console.log(b);
+
+function calcAgeRetirement(year) {
+    const age = new Date().getFullYear() - year;
+    return [age, 65 - age];
+}
+const [age2, retirement] = calcAgeRetirement(1990);
+console.log(age2);
+console.log(retirement);
+
 
 
 
