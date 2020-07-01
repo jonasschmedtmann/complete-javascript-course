3f355034624a0c38d2742040a5f17ce66d9c8c31
shawn <dboybash@gmail.com>
2020-06-30 22:41:33 -0500

    finished lesson 110.

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -171,7 +171,7 @@ console.log(ages6);*/
 // new Person('Harry').myFriends6(friends);
 
 
-// Lecture: Destructuring
+/*// Lecture: Destructuring
 
 //ES5
 var shawn = ['Shawn', 39];
@@ -201,7 +201,71 @@ function calcAgeRetirement(year) {
 }
 const [age2, retirement] = calcAgeRetirement(1990);
 console.log(age2);
-console.log(retirement);
+console.log(retirement);*/
+
+
+// Lecture: Arrays
+
+const boxes = document.querySelectorAll('.box');
+
+//ES5
+var boxesArr5 = Array.prototype.slice.call(boxes);
+/*boxesArr5.forEach(function(cur) {
+    cur.style.backgroundColor = 'dodgerblue';
+})*/
+
+//ES6
+const boxesArr6 = Array.from(boxes);
+Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+
+/*//ES5
+for (var i = 0; i < boxesArr5.length; i++) {
+    if(boxesArr5[i].className === 'box blue') {
+        // continue;
+        break;
+    }
+    boxesArr5[i].textContent = 'I changed to blue!';
+}*/
+
+//ES6
+for (const cur of boxesArr6) {
+    if (cur.className.includes('blue')) {
+        continue;
+    }
+    cur.textContent = 'I changed to blue!!'
+}
+
+
+//ES5
+var ages = [12, 17, 8, 21, 14, 11];
+
+var full = ages.map(function (cur) {
+    return cur >= 18;
+});
+console.log(full);
+console.log(full.indexOf(true));
+console.log(ages[full.indexOf(true)]);
+
+//ES6
+console.log(ages.findIndex(cur => cur >= 18));
+console.log(ages.find(cur => cur >= 18));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
