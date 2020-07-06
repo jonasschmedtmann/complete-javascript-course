346a4f8967dc0a0bbdd4fc808bfe0aa65fabf71d
sbash <sbash@hy-vee.com>
2020-07-06 08:30:52 -0500

    finish lesson 114.

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -343,6 +343,48 @@ Array.from(all).forEach(cur => cur.style.color = 'purple');*/
 // var emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish');
 
 
+// Lecture: Maps
+
+const question = new Map();
+question.set('question', 'What is the the official name of the latest major Javascript version?')
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct answer!');
+question.set(false, 'Wrong, please try again!')
+
+console.log(question.get('question'));
+// console.log(question.size);
+
+// if (question.has(4)) {
+//     //question.delete(4);
+//     console.log('Answer 4 is here');
+// }
+//question.clear();
+
+// question.forEach((value, key) =>
+//     console.log(`This is ${key}, and it's set to ${value}`));
+
+
+for (let [key, value] of question.entries()) {
+    if (typeof(key) === 'number') {
+        console.log(`Answer ${key}: ${value}`);
+    }
+}
+
+const ans = parseInt(prompt('Write the correct answer'));
+console.log(question.get(ans === question.get('correct')));
+
+
+
+
+
+
+
+
+
 
 
 
