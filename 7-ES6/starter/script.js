8cedaf269c6d1526b8ead885e2d9a1e9725e7737
sbash <sbash@hy-vee.com>
2020-07-09 15:30:42 -0500

    add coding challenge instructions.

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -414,7 +414,7 @@ const john6 = new Person6('John', 1980, 'consultant');
 Person6.greeting();*/
 
 
-// Lecture: Classes with Subclasses
+/*// Lecture: Classes with Subclasses
 
 //ES5
 var Person5 = function (name, yearOfBirth, job) {
@@ -474,10 +474,30 @@ class Athlete6 extends Person6 {
 
 const johnAthlete6 = new Athlete6('John', 1980, 'swimmer', 3, 10);
 johnAthlete6.wonMedal();
-johnAthlete6.calculateAge();
+johnAthlete6.calculateAge();*/
 
 
+// CODING CHALLENGE
 
+/*
+Suppose that you're working in a small town administration, and you're in charge of two town elements:
+1. Parks
+2. Streets
+
+It's a very small town, so right now there are only 3 parks and 4 streets. All parks and streets have a name and a build year.
+
+At an end-of-year meeting, your boss wants a final report with the following:
+1. Tree density of each park in the town (forumla: number of trees/park area)
+2. Average age of each town's park (forumla: sum of all ages/number of parks)
+3. The name of the park that has more than 1000 trees
+4. Total and average length of the town's streets
+5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown, the default is normal
+
+All the report data should be printed to the console.
+
+HINT: Use some of the ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions, destructuring, etc.
+
+*/
 
 
 
