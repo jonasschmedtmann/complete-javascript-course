762c2312d80fbe85287c4b47a139eaf6ac03171f
Zack Bumpous <upwork.ztb@gmail.com>
2020-07-15 22:47:01 -0500

    Demonstrates falsey values, and how you can even assign these falsey values to variables, including 0, and those variables will still be undefined.

--- 2-JS-basics/starter2/script.js
+++ 2-JS-basics/starter2/script.js
@@ -176,52 +176,67 @@
  * Operator and Switch Statements
  */
 
-var firstName, age;
-firstName = "John";
-age = 16;
+// var firstName, age;
+// firstName = "John";
+// age = 16;
 
-// Example (Ternary)
-age >= 21
-  ? console.log(`${firstName} can drink beer.`)
-  : console.log(`${firstName} can't drink beer.`);
+// // Example (Ternary)
+// age >= 21
+//   ? console.log(`${firstName} can drink beer.`)
+//   : console.log(`${firstName} can't drink beer.`);
 
-// Example assigned to variable (Ternary)
-var drink = age >= 21 ? `beer` : `juice`;
-console.log(drink);
+// // Example assigned to variable (Ternary)
+// var drink = age >= 21 ? `beer` : `juice`;
+// console.log(drink);
 
-// Example (Switch)
-var job;
-job = "teacher";
-switch (job) {
-  case "teacher":
-    console.log(`${firstName} teaches kids how to code.`);
-    break;
-  case "driver":
-    console.log(`${firstName} is a driver! Vroom vroom.`);
-    break;
-  case "skateboarder":
-    console.log(`${firstName} shreds on a skateboard.`);
-    break;
-  default:
-    console.log(`${firstName} does something entirely different.`);
-}
+// // Example (Switch)
+// var job;
+// job = "teacher";
+// switch (job) {
+//   case "teacher":
+//     console.log(`${firstName} teaches kids how to code.`);
+//     break;
+//   case "driver":
+//     console.log(`${firstName} is a driver! Vroom vroom.`);
+//     break;
+//   case "skateboarder":
+//     console.log(`${firstName} shreds on a skateboard.`);
+//     break;
+//   default:
+//     console.log(`${firstName} does something entirely different.`);
+// }
 
-// Using the If/Else from Boolean Logic
-// Lesson, convert it to a Switch Statement
-var firstName, age, gender;
-firstName = "John";
-age = 19;
-gender = "non-binary";
+// // Using the If/Else from Boolean Logic
+// // Lesson, convert it to a Switch Statement
+// var firstName, age, gender;
+// firstName = "John";
+// age = 19;
+// gender = "non-binary";
 
-switch (true) {
-  case age < 13:
-    console.log(`${firstName} is a ${gender} child who is ${age} years old.`);
-    break;
-  case age >= 13 && age < 20:
-    console.log(
-      `${firstName} is a ${gender} teenager who is ${age} years old.`
-    );
-    break;
-  default:
-    console.log(`${firstName} is a ${gender} adult who is ${age} years old.`);
-}
+// switch (true) {
+//   case age < 13:
+//     console.log(`${firstName} is a ${gender} child who is ${age} years old.`);
+//     break;
+//   case age >= 13 && age < 20:
+//     console.log(
+//       `${firstName} is a ${gender} teenager who is ${age} years old.`
+//     );
+//     break;
+//   default:
+//     console.log(`${firstName} is a ${gender} adult who is ${age} years old.`);
+// }
+
+/******************
+ * The Ternary (Conditional)
+ * Operator and Switch Statements
+ */
+
+ var height; // if we don't add a value to height (or add a falsey value),
+ // we will get the else statement, we should set || height = 0 so that won't be
+ // an undefined falsey value
+ height = 0;
+ if (height || height === 0) {
+     console.log('Variable is defined');
+ } else {
+     console.log('Variable has NOT been defined')
+ }
