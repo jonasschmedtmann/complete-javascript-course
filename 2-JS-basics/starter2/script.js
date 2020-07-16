f55f7748d3af91998933c60cc9d50ab49abfe1d7
Zack Bumpous <upwork.ztb@gmail.com>
2020-07-15 19:31:10 -0500

    Starting course from beginning, added a new file. Got through lesson 15. Adds lecture code that is all review to me.

---
+++ 2-JS-basics/starter2/script.js
@@ -0,0 +1,172 @@
+/******************
+ * Variables and data types
+ */
+// console.log("Hello world from script.js file");
+
+// const { Console } = require("console");
+
+// var firstName = 'Zack';
+// console.log(firstName);
+
+// var lastName = 'Smith';
+// var age = 68;
+// var isMarried = true;
+// var lovesVideoGames = undefined;
+
+/******************
+ * Variable mutation and type coercion
+ */
+// var firstName = "Zack";
+// var lastName = "Smith";
+// var age = 68;
+// var isMarried = true;
+// var lovesVideoGames = undefined;
+// var job, isAwesome;
+// job = "developer";
+// isAwesome = true;
+
+// // JavaScript uses type-coercion to automatically allow us
+// // to join variables containing different data-types together
+// // as strings
+// console.log(
+//   firstName +
+//     " " +
+//     lastName +
+//     " is " +
+//     age +
+//     " and it is " +
+//     isMarried +
+//     " that he is married. " +
+//     "But, it is " +
+//     lovesVideoGames +
+//     " whether he likes video games."
+// );
+
+// console.log(
+//   `${firstName} ${lastName} is ${age} and it is ${isMarried} that he is married. But, it is ${lovesVideoGames} whether he likes video games or not. He is a ${job} and it is ${isAwesome} that he is awesome.`
+// );
+
+// // Variable Mutation
+// age = 20;
+// job = 'driver'
+
+// alert(`${firstName} is ${age} and it is ${isMarried} that he is married. But, it is ${lovesVideoGames} whether he likes video games or not. He is a ${job} and it is ${isAwesome} that he is awesome.`)
+
+// var lastName = prompt(`What is ${firstName}'s lastname?`);
+// console.log(`${firstName} ${lastName}`);
+
+/******************
+ * Basic Operators
+ */
+
+// var year, yearRandom, yearRandom2;
+// year = 2020;
+// yearRandom = year - 1600;
+// yearRandom2 = year - 1800;
+
+// console.log(yearRandom);
+// console.log(yearRandom + 2);
+
+// // Logical operators
+// var laterDate = yearRandom > yearRandom2;
+// console.log(laterDate);
+
+// // typeof operator
+// console.log(typeof laterDate);
+// console.log(typeof year);
+
+/******************
+ * Operator Precedence
+ */
+
+// var now, yearJohn, fullAge;
+// now = 2020;
+// yearJohn = 1989;
+// fullAge = 18;
+
+// // Multiple Operators
+// var isFullAge = now - yearJohn >= fullAge;
+// console.log(isFullAge);
+
+// // Grouping
+// var ageJohn = now - yearJohn;
+// var ageMark = 35;
+// var average = (ageJohn + ageMark) / 2;
+// console.log(`The average age of John and Mark is ${average}`);
+
+// // Multiple Assignments
+// // both x and y = (3 + 5) * 4 - 6
+// // which means both x and y = 26
+// var x, y;
+// x = y = (3 + 5) * 4 - 6;
+// console.log(x);
+
+// // More operators
+// // what if we want to double something
+// x *= 2; // this means x = x * 2
+// x += 10; // means x = x + 10
+// x -= 10; // means x = x - 10
+// x++; // means x = x + 1
+// x--; // means x = x - 1
+
+/******************
+ * Coding Challenge 1
+ */
+// var johnHeight, johnMass, markHeight, markMass;
+// //  John
+// johnHeight = 1.8; // meters
+// johnMass = 80; // kg
+// //  Mark
+// markHeight = 1.7; // meters
+// markMass = 76; // kg
+
+// var johnBMI, markBMI;
+// // John BMI
+// johnBMI = johnMass / (johnHeight * johnHeight);
+// // Mark BMI
+// markBMI = markMass / (markHeight * markHeight);
+
+// // Boolean Value Below
+// // Does Mark have higher BMI
+// // than John?
+// var markHigherBMI = markBMI > johnBMI;
+
+// // Console.log of Boolean
+// // value, displays true
+// console.log(`Does Mark have a higher BMI than John? ${markHigherBMI}`);
+// // do this (above) but with an if /else statement
+// if (markBMI > johnBMI){
+//     console.log(`Mark has a higher BMI than John.`)
+// } else {
+//     console.log(`John has a higher BMI than Mark.`)
+// }
+
+/******************
+ * If / else Statements
+ */
+
+//  var firstName, civilStatus;
+// firstName = 'John';
+// civilStatus = 'single';
+
+// if (civilStatus === 'married'){
+//     console.log(`${firstName} is married`);
+// } else {
+//     console.log(`${firstName} is ${civilStatus}`)
+// }
+
+/******************
+ * Boolean Logic
+ */
+var firstName, age, gender;
+firstName = "John";
+age = 19;
+gender = "non-binary";
+
+if (age < 13) {
+  console.log(`${firstName} is a ${gender} child who is ${age} years old.`);
+} else if (age >= 13 && age < 20) {
+  console.log(`${firstName} is a ${gender} teenager who is ${age} years old.`);
+} else {
+  console.log(`${firstName} is a ${gender} adult who is ${age} years old.`);
+}
