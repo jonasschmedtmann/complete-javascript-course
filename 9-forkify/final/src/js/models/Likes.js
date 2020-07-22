a7a7a9aa0e89a7dfd63ac8a99824693c42ac766e
Mehar Fatima Khan <meharfatimak@gmail.com>
2020-07-22 15:22:07 +0500

    Like Module: Added Likes Model

--- 9-forkify/final/src/js/models/Likes.js
+++ 9-forkify/final/src/js/models/Likes.js
@@ -2,23 +2,17 @@ export default class Likes {
     constructor() {
         this.likes = [];
     }
-
-    addLike(id, title, author, img) {
-        const like = { id, title, author, img };
+    addLikes(id, title, author, img){
+        const like = {
+            id,title,author,img
+        }
         this.likes.push(like);
-
-        // Perist data in localStorage
-        this.persistData();
-
         return like;
     }
 
     deleteLike(id) {
         const index = this.likes.findIndex(el => el.id === id);
-        this.likes.splice(index, 1);
-
-        // Perist data in localStorage
-        this.persistData();
+        this.likes.splice(index,1);
     }
 
     isLiked(id) {
@@ -27,16 +21,5 @@ export default class Likes {
 
     getNumLikes() {
         return this.likes.length;
-    }
-
-    persistData() {
-        localStorage.setItem('likes', JSON.stringify(this.likes));
-    }
-
-    readStorage() {
-        const storage = JSON.parse(localStorage.getItem('likes'));
-        
-        // Restoring likes from the localStorage
-        if (storage) this.likes = storage;
     }
 }
