7cc2707506094c48b99a4cd48065685086e505c0
Mehar Fatima Khan <meharfatimak@gmail.com>
2020-07-22 15:42:56 +0500

    Likes Module: Added Likes Controller

--- 9-forkify/final/src/js/index.js
+++ 9-forkify/final/src/js/index.js
@@ -143,38 +143,28 @@ elements.shopping.addEventListener('click', e => {
 /** 
  * LIKE CONTROLLER
  */
+
 const controlLike = () => {
     if (!state.likes) state.likes = new Likes();
     const currentID = state.recipe.id;
 
-    // User has NOT yet liked current recipe
     if (!state.likes.isLiked(currentID)) {
-        // Add like to the state
         const newLike = state.likes.addLike(
             currentID,
             state.recipe.title,
             state.recipe.author,
             state.recipe.img
         );
-        // Toggle the like button
         likesView.toggleLikeBtn(true);
-
-        // Add like to UI list
         likesView.renderLike(newLike);
-
-    // User HAS liked current recipe
-    } else {
-        // Remove like from the state
+    }
+    else {
         state.likes.deleteLike(currentID);
-
-        // Toggle the like button
         likesView.toggleLikeBtn(false);
-
-        // Remove like from UI list
         likesView.deleteLike(currentID);
     }
-    likesView.toggleLikeMenu(state.likes.getNumLikes());
-};
+    likesView.toggleLikeMenu(state.getNumLikes());
+}
 
 // Restore liked recipes on page load
 window.addEventListener('load', () => {
