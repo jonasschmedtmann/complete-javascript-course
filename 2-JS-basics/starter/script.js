18b80257bf10ea4f50ae5b006b5a08df66f536b4
alec dye <alecdye@gmail.com>
2020-09-02 10:51:47 -0700

    Complete challenge 2 🍣

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -263,3 +263,31 @@ if (height || height === 0) {
 if (height == "23") {
 	console.log("The == operator does type coercion!");
 }
+
+/*
+* CODING CHALLENGE 2
+
+John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+
+1. Calculate the average score for each team
+2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
+3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
+
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
+5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
+
+GOOD LUCK 😀
+*/
+
+var avgJohn = (89 + 120 + 103) / 3; // 104
+var avgMike = (116 + 94 + 123) / 3; // 111
+var avgMary = (97 + 134 + 105) / 3; // 112
+console.log("John - " + avgJohn + " | " + "Mike - " + avgMike + " | " + "avgMary - " + avgMary);
+
+if (avgJohn > avgMike && avgJohn > avgMary) {
+	console.log("John's team won the season! ...with a score of " + avgJohn);
+} else if (avgMike > avgJohn && avgMike > avgMary) {
+	console.log("Mike's team won the season! ...with a score of " + avgMike);
+} else {
+	console.log("Mary's team won the season! ...with a score of " + avgMary);
+}
