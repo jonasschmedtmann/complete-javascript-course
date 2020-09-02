916c70835a64c278934986f80c59125d73738a14
alec dye <alecdye@gmail.com>
2020-09-01 19:37:55 -0700

    Add primitive datatypes 🧂

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,24 @@
-console.log("Hello World! (from imported module)");
+// console.log("Hello World! (from imported module)");
+
+// var is depreciated; use const or let (ES6), also ";" not needed
+var firstName = "John";
+console.log(firstName);
+
+var lastName = "Smith";
+var age = 28;
+console.log(typeof age); // number
+
+var fullAge = true;
+
+// create variable without assigning a value (defaults to 'undefined')
+var job; // currently undefined
+console.log(job);
+
+// assigning a value to an existing variable
+job = "Teacher";
+console.log(job);
+
+// let's break some rules
+// var 3years = 3; // cannot start the name of variable with number
+// var john/mark = 'John and Mark'; cannot have a forbidden character inside a variable
+// var function = 23; function is a reserved keyword
--- 3-how-JS-works/what-is-js.md
+++ 3-how-JS-works/what-is-js.md
@@ -13,3 +13,13 @@ Comparing web dev to sentence structure
 HTML: think "nouns" - <p></p> - paragraph
 CSS: think "adjectives" - p {color: red;} - the paragraph text is red
 JS: think "verb" - p.hide(); - hide the paragraph (bad example, because CSS can do this too)
+
+## Primitive JavaScript data types
+
+1. Number - floating point numbers including both decimals & integers
+2. String - text characters
+3. Boolean - true / false
+4. Undefined - Data type of a variable that does not have a value yet (unassigned?)
+5. Null - Non-existent
+
+Note: JS has "dynamic typing" which means data types are automatically assigned to variables
