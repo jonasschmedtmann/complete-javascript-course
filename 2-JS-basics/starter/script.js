6dfae6afd22fbd0e34709e49a73efbb9abc64bd1
alec dye <alecdye@gmail.com>
2020-09-02 08:39:00 -0700

    Add var mutation

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,3 +1,4 @@
+/*
 // console.log("Hello World! (from imported module)");
 
 // var is depreciated; use const or let (ES6), also ";" not needed
@@ -18,7 +19,35 @@ console.log(job);
 job = "Teacher";
 console.log(job);
 
+*/
+
 // let's break some rules
 // var 3years = 3; // cannot start the name of variable with number
 // var john/mark = 'John and Mark'; cannot have a forbidden character inside a variable
 // var function = 23; function is a reserved keyword
+
+// Type coercion
+
+var firstName = "John";
+var age = 28;
+
+// converts age (number) to string. will also convert boolean and undefined to string
+console.log(firstName + " " + age);
+
+// creating multiple variables
+var job, isMarried;
+job = "teacher";
+isMarried = false;
+
+console.log(firstName + " is " + age + " year old " + job + ". Is he married? " + isMarried);
+
+// Variable mutation
+age = "twenty eight";
+job = "driver";
+
+// pop-up message in browser
+alert(firstName + " is " + age + " year old " + job);
+
+// pop-up user input in browser with input saved to variable
+var lastName = prompt("What is his last Name?");
+console.log(firstName + " " + lastName);
