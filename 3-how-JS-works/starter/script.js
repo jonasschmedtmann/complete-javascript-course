e107c4a127aea2ef0e9dc059f1297c0542b70ff4
alec dye <alecdye@gmail.com>
2020-09-03 09:23:32 -0700

    Add hoisting notes 🚦

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,25 +1,52 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// function & variable declarations are hoisted
+// available before the "execution phase" of the execution context starts
 
+// Functions
 
+// function declaration
+calculateAge(1965);
 
+// pointed to by function declaration
+function calculateAge(year) {
+	console.log(2020 - year);
+}
 
+// throws error -> not a funciton declaration
+// this is a function expression
+// retirement(1965);
 
+var retirement = function (year) {
+	console.log(65 - (2020 - year));
+};
 
+retirement(1990);
 
+// Variables
 
+// variable declaration -> all variables are first created as undefined
+// variable is created as undefined
+console.log(age); // undefined
+var age = 23;
+// variable is now defined
+console.log(age); // 23
 
+function foo() {
+	console.log(age); // undefined
+	var age = 65;
+	console.log(age); // 65
+}
 
-
-
-
-
-
+foo();
+// the global scope is executed last (its at the bottom of the stack)
+// because of that, it overrides the function scope of age (65) with the global
+// scope (23)
+console.log(age); //23
 
 ///////////////////////////////////////
 // Lecture: Scoping
-
 
 // First scoping example
 
@@ -37,8 +64,6 @@ function first() {
     }
 }
 */
-
-
 
 // Example to show the differece between execution stack and scope chain
 
@@ -62,16 +87,5 @@ function third() {
 }
 */
 
-
-
 ///////////////////////////////////////
 // Lecture: The this keyword
-
-
-
-
-
-
-
-
-
