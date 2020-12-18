622d2d2ff05d0bda0a1b4d4252328303e9270c01
jonathan VALERE <jonathan.valere@gmail.com>
2020-12-18 14:38:47 -0300

    basic opertors

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,27 @@
+const country = "France";
+const continent = "Europe";
+let population = 68_000_000;
+/*console.log(country);
+console.log(continent);
+console.log(population);*/
+
+let isIsland = false;
+let language;
+/*console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);*/
+
+language = 'French';
+// console.log(language);
+const halfPop = population / 2;
+console.log(halfPop);
+population++;
+console.log(population);
+const finland = 6_000_000;
+console.log(population > finland);
+const average = 33_000_000;
+console.log(population < average);
+const description = country + " is in " + continent + ", and its " + population + " people speak " + language
+console.log(description);
