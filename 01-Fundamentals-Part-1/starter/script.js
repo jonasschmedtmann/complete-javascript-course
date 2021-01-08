e55b6a1c33be5cb48972c51e268cb4aa1418a7f3
jking026 <jamesjking026@gmail.com>
2021-01-08 16:02:43 +0900

    add assigment pdf

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,24 @@
 let js = "awesome";
-if (js === "awesome") alert("JavaScript is FUN!");
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
 
+console.log('Jonas');
+console.log(23);
+
+let firstName = "James";
+
+
+console.log(firstName)
+
+/*reserved words shouldn't be used but some are still legal*/
+let jonas_matilda = "JM"
+let $function = 27;
+let Person = 'Jonas'; /*use lower case letter to start a var*/
+
+let PI = 3.1415; /*a constant number that never changes should be all in caps*/
+
+
+/* use descripted variable names rather than vague*/
+let myFisrstJob = "Pizza Maker";
+let myCurrentJob = "Unemployed";
+
+console.log(myCurrentJob)
