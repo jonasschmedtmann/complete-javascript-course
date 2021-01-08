71c6cfd47e68c605719e8d5652140ece79638bdc
jking026 <jamesjking026@gmail.com>
2021-01-08 16:29:35 +0900

    completed assigment-values and variables

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,15 @@
+// let greeting = "Hello, welcome to a new tommorrow ";
+// let firstNameUser = "James "
+// alert(greeting + firstName + ".");
+
+
+// LECTURE: VALUES AND VARIABLES//
+
+let country = "United States of America";
+console.log(country);
+let continent = "North America";
+console.log(continent);
+let population = 328000000;
+console.log(population);
+
+//LECTURE: DATA TYPES//
