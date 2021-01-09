5bd9159ad8328b669ecbdf803ff547959360766a
jking026 <jamesjking026@gmail.com>
2021-01-09 12:06:11 +0900

    completed Lesson: BASIC OPERATORS

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,17 +1,16 @@
-// let greeting = "Hello, welcome to a new tommorrow ";
-// let firstNameUser = "James "
+// LECTURE: VALUES AND VARIABLES//
+let greeting = "Hello, welcome to a new tommorrow ";
+let firstNameUser = "James "
 // alert(greeting + firstName + ".");
 
-
-// LECTURE: VALUES AND VARIABLES//
-
+//LECTURE: DATA TYPES//
 const country = "United States of America";
 const continent = "North America";
 let population = 328000000;
-//LECTURE: DATA TYPES//
 
+// LECTURE: LET, CONST, AND VAR//
 const isIsland = true;
-isIsland = false;
+// isIsland = false;
 // console.log(typeof isIsland);
 language = "English";
 // console.log(typeof country);
@@ -20,5 +19,16 @@ language = "English";
 // console.log(typeof language);
 // console.log(typeof isIsland);
 
-//LECTURE: LET, CONST, AND VAR//
+//LACTURE: BASIC OPERATORS//
+
+let usaHalfPopulation = population / 2; //outputs 164000000
+let finlandTotalPopulation = 6000000;
+let averageCountryPopulation = 33000000;
+const description = "Portugal is in Europe, and its 11 million people speak portuguese"
+usaHalfPopulation++; //increases val by 1; 164000001
+// console.log(usaHalfPopulation);
+// console.log(usaHalfPopulation > finlandTotalPopulation); //outputs true
+// console.log(usaHalfPopulation < averageCountryPopulation); //outputs false
+// console.log(description); //outputs "Portugal is in Europe, and its 11 million people speak portuguese"//
+
 
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -70,4 +70,39 @@ job = "teacher";
 lastName = "King";
 console.log(lastName)
 
+
+//LESSON: BASIC OPERATORS//
+//MATH OPERATORS
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 *2
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + ' ' + lastName);
+
+//ASSIGNMENT OPERATORS//
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10; returns 25
+x *= 4; //x = x * 4 = 100
+x++; // x = x + 1; 101
+x--; // x = x - 1; 100
+x--; //99
+console.log(x)
+
+//COMPARISON OPERATORS//
+
+console.log(ageJonas > ageSarah); //>, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageJonas >= 18;
+
+console.log(now - 1991 > now - 2018)
+
 */
+
