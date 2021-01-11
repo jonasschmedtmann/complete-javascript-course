939de046a5a3a5b350909c55df07e004c14eae96
jking026 <jamesjking026@gmail.com>
2021-01-11 14:34:30 +0900

    complete LESSON: STRINGS AND TEMPLATE LITERALS

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -179,10 +179,29 @@ console.log(markHigherBMIT2)
 // Coding Challenge #1 -COMPLETE///////
 */
 
+
+//LESSON: STRINGS AND TEMPLATE LITERALS//
 const firstName = 'Jonas';
 const job = 'teacher';
 const birthYear = 1991;
 const year = 2037;
 
-const jonas = "I'm" + firstName ', a ' + (year - birthYear) + 'years old' + job + '!';
-console.log(jonas)
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old' + job + '!';
+console.log(jonas);
+//*ES6-THE KEY IS THE  `` ANYTHING CAN BE INSERTED INSIDE  ``//
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`)//Much easier to replace with a variable//
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String with 
+multiple 
+lines`);
+
+//LESSON: TAKING DECISIONS: IF / ELSE STATEMENTS //
+
+
