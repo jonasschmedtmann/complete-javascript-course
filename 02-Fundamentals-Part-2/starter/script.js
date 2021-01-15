62cb56f730d92473ee16a300a266e108bdd24111
jking026 <jamesjking026@gmail.com>
2021-01-15 20:19:23 +0900

    complete 'use strict' for every js

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+'use strict';  //activates strict mode or secure code to avoid accidental errors//
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive!')
+
+// const interface = "Audio"; //Uncaught SyntaxError: Unexpected strict mode reserved word//
+
+// const private = 534; //reserved word
+
