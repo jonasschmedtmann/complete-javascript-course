4e87cf6e1f6f5a4ad20d9dc2d375c389ed57157a
jking026 <jamesjking026@gmail.com>
2021-01-15 10:34:38 +0900

    add a weekly schedule as an IF statement

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -409,34 +409,51 @@ if (averageScoreDolphins > averageScoreKoalas && averageScoreDolphins >= 100) {
   console.log("No team wins the trophy! 🤬🤬🤬");
 }
 
-*/
 
 //LESSON: THE SWITCH STATEMENT//
+
 //Reprents a weekly schedule as a SWITCH statement//
-const day = "monday";
+const day = "thursday";
 
-switch (day) {
-  case 'monday': // day === 'monday'
-    console.log('Plan course structure');
-    console.log('Go to coding meetup');
-    break;
-  case 'tuesday':
-    console.log('Prepare theory videos');
-    break;
-  case 'wednesday':
-  case 'thursday':
-    console.log('Write code examples');
-    break;
-  case 'friday':
-    console.log('Record videos');
-    break;
-  case 'saturday':
-  case 'sunday':
-    console.log('Enjoy the weekend 🥳');
-    break;
-  default:
-    console.log('Not a valid day!')
+// switch (day) {
+//   case 'monday': // day === 'monday'
+//     console.log('Plan course structure');
+//     console.log('Go to coding meetup');
+//     break;
+//   case 'tuesday':
+//     console.log('Prepare theory videos');
+//     break;
+//   case 'wednesday':
+//   case 'thursday':
+//     console.log('Write code examples');
+//     break;
+//   case 'friday':
+//     console.log('Record videos');
+//     break;
+//   case 'saturday':
+//   case 'sunday':
+//     console.log('Enjoy the weekend 🥳');
+//     break;
+//   default:
+//     console.log('Not a valid day!')
 
-}
+// }
 
 //Reprents a weekly schedule as an IF statement
+
+if (day === 'monday') {
+  console.log('Plan course structure');
+  console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+  console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+  console.log('Write code examples');
+} else if (day === 'friday') {
+  console.log('Record videos')
+} else if (day === 'saturday' || day === 'sunday') {
+  console.log('Enjoy the weekend 🥳');
+} else {
+  console.log('Not a valid day!');
+};
+
+*/
