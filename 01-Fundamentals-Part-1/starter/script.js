557037f13eb218aac78dbcf1d85ab56bb617abcb
jking026 <jamesjking026@gmail.com>
2021-01-15 10:20:22 +0900

    add weekly schedule as a switch statement

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -412,6 +412,31 @@ if (averageScoreDolphins > averageScoreKoalas && averageScoreDolphins >= 100) {
 */
 
 //LESSON: THE SWITCH STATEMENT//
+//Reprents a weekly schedule as a SWITCH statement//
+const day = "monday";
 
+switch (day) {
+  case 'monday': // day === 'monday'
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break;
+  case 'tuesday':
+    console.log('Prepare theory videos');
+    break;
+  case 'wednesday':
+  case 'thursday':
+    console.log('Write code examples');
+    break;
+  case 'friday':
+    console.log('Record videos');
+    break;
+  case 'saturday':
+  case 'sunday':
+    console.log('Enjoy the weekend 🥳');
+    break;
+  default:
+    console.log('Not a valid day!')
 
+}
 
+//Reprents a weekly schedule as an IF statement
