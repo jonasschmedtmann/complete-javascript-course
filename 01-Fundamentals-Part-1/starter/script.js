b0db36539884cb9b9b75e090b879abe2af1cbc4f
jking026 <jamesjking026@gmail.com>
2021-01-15 12:40:46 +0900

    add expression and statement examples

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -456,4 +456,19 @@ if (day === 'monday') {
   console.log('Not a valid day!');
 };
 
+
+// LESSON STATEMENTS & EXPRESSIONS//
+
+3 + 4
+1991
+true && false && !false
+
+
+if (23 > 10) {
+  const str = '23 is bigger';
+}
+const me = "Jonas";//expressions
+console.log(`I'm ${2037 - 1991} years old. ${me}`);
+
 */
+
