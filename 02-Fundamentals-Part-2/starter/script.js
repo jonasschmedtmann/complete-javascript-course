302785466f43e34f0114827e5afbee34c785bde9
jking026 <jamesjking026@gmail.com>
2021-01-16 18:38:29 +0900

    complete LESSON: ARROW FUNCTIONS

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -57,7 +57,18 @@ console.log(age1, age2)
 
 //LESSON: ARROW FUNCTIONS//
 
+// const calcAge3 = birthYear => 2037 - birthYear;
+// const age3 = calcAge3(1991);
+// console.log(age3);
 
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   const age = 2037 - birthYear;
+//   const retirement = 65 - age;
+//   // return retirement;
+//   return `${firstName} retires in ${retirement} years`
+// };
 
+// console.log(yearsUntilRetirement(1991, 'Jonas'));
+// console.log(yearsUntilRetirement(1980, 'Bob'));
 
-
+//LESSON: FUCNTIONS CALLING OTHER FUNCTIONS//
