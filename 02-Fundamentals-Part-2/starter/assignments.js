499f273ac4b160c4b7783901bade3ec2b7e808e0
jking026 <jamesjking026@gmail.com>
2021-01-16 18:15:53 +0900

    complete assignment: Function Decl.  vs. Expr.

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -1,18 +1,52 @@
 'use strict';
 
-//LESSON: FUNCTIONS//
+// //LESSON: FUNCTIONS//
 
-function describeCountry(country, population, capitalCity) {
-  return `${country} has ${population} million people and its capital city is ${capitalCity}`
-}
+// function describeCountry(country, population, capitalCity) {
+//   return `${country} has ${population} million people and its capital city is ${capitalCity}`
+// }
 
-const usa = describeCountry("USA", 328, "Washington D.C.");
-// console.log(usa);
+// const usa = describeCountry("USA", 328, "Washington D.C.");
+// // console.log(usa);
 
-const southKorea = describeCountry("South Korea", 51.71, "Seoul");
-// console.log(southKorea);
+// const southKorea = describeCountry("South Korea", 51.71, "Seoul");
+// // console.log(southKorea);
 
-const cameroon = describeCountry("Cameroon", 25.88, "Yaoundé");
-// console.log(cameroon);
+// const cameroon = describeCountry("Cameroon", 25.88, "Yaoundé");
+// // console.log(cameroon);
 
 //LESSON: FUNCTION DECLARATION VS. EXPRESSIONS//
+
+///// FUNCTION DECLARATION/////
+// function percentageOfWorld1(country, population) {
+//   return `${country} has ${population / 7900 * 100} of the world's population!`;
+// }
+
+// const japan = percentageOfWorld1("Japan", 126.3);//declares the paramaters, but saves to a variable//
+// const usa = percentageOfWorld1("USA", 328.2);
+// const russia = percentageOfWorld1("Russia", 144.4)
+
+
+// console.log(percentageOfWorld1("China", 1441))//desclares the paramaters to the console
+// console.log(usa)
+// console.log(japan)
+// console.log(russia)
+
+///// FUNCTION EXPRESSIONS/////
+
+// const percentageOfWorld2 = function (country, population) {
+//   return `${country} has ${population / 7900 * 100} of the world's population!`;
+// }
+
+// const usa = percentageOfWorld2("USA", 328.2);
+// const japan = percentageOfWorld2("Japan", 126.3);
+// const russia = percentageOfWorld2("Russia", 144.4);
+// console.log(usa);
+// console.log(japan);
+// console.log(russia);
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+//LESSON: ARROW FUNCTIONS//
+
+
