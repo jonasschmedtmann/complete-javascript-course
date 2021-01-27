d852518031b185e6561bdcac6d5efee5e18c66c5
jking026 <jamesjking026@gmail.com>
2021-01-28 08:58:35 +0900

    add .prettierrc to page

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,9 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+//PRETTIER-CODE FORMATTER
+const x = '23';
+if (x === 23) console.log(23);
 
+const calcAge = birthYear => 2037 - birthYear;
+console.log();
+console.log();
