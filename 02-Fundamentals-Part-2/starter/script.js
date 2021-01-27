287ce9f412a62d00cef30f2ab214a569a0b9609d
jking026 <jamesjking026@gmail.com>
2021-01-27 18:50:31 +0900

    calcTip funct. is funtional, will work on bonus

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -539,3 +539,42 @@ while (die !== 6) {
 
 */
 
+///////////////////////SECTION 2: CODING CHALLENGE #4///////////////////////////
+
+
+
+//help from video
+const calcTip = function (bill) {
+  return bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.20;
+};
+
+//*hint* use plural variable names that have multi values
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips = [];
+let totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  tips.push(calcTip(bills[i]));
+  totals.push(tips[i] + bills[i]);
+  console.log(`$$$$$$$$    ${bills[i]}`);
+  console.log(`=========tips ${tips[i]}======`);
+  console.log(totals[i]);
+}
+
+// function calcAverage(arr) {
+
+//   for (let sum = 0; sum < bills.length; sum++) {
+//     console.log()
+//   }
+
+// };
+
+// console.log(tips.push(calcTip(bills)))
+// console.log(calcTip(bills[0]))
+
+
+// const tip = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const total = [(bills[0] + tips[0]), (bills[1] + tips[1]), (bills[2] + tips[2])];
+// console.log(bills);
+// console.log(tips);
+// console.log(total);
