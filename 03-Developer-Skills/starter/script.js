93d9f265814db455765e13f5138d334682068dd4
jking026 <jamesjking026@gmail.com>
2021-01-31 20:54:03 +0900

    complete S5-CC#1 with help

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,5 +1,5 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
 /*
 // //PRETTIER-CODE FORMATTER
@@ -53,6 +53,108 @@ console.log(amplitudeNew);
 //2) Breaking up into sub-problems
 // - How to merge 2 arrays? Merge 2 arrays!
 
+
+//Lesson: Debugging with the Console and Breakpoints
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+
+    // C) FIX
+    // value: Number(prompt('Degrees Celcius:')),
+    value: 10,
+  };
+  // B) FIND
+  console.table(measurement);
+
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) IDENTIFY THE BUG
+console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+  // B) FIX BUG 👇
+  // let max = 0;
+  // let min = 0;
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i]; // Great ex. "Don't repeat yourself!"
+    if (typeof curTemp !== 'number') continue;
+
+    debugger; //built in debugger keyword that will automatically jump to this point
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+//A) IDENTIFY 🐛🐛
+console.log(amplitudeBug);
+
 */
 
-//Lesson: Debugging(Fixing Errors)
+//////////////////////// Section 5: Coding Challenge #1 ///////////////////////////
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a
+string with the given temperatures. 
+
+Example: [17, 21, 23] will print "... 17ºC in 1
+days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Your tasks:
+
+1. Create a function 'printForecast' which takes in an array 'arr' and logs a
+string like the above to the console. Try it with both test datasets.
+
+
+2. Use the problem-solving framework: Understand the problem and break it up
+into sub-problems!
+
+Test data:
+* Data 1: [17, 21, 23]
+* Data 2: [12, 5, -5, 0, 4]
+
+
+const degreeCelsius = "\u2103";
+const days = [1, 2, 3, 4, 5];
+
+function printForecast(arr) {
+  //const forcastDays = []; // Didn't need the empty array
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}${degreeCelsius} in ${i + 1} days ... `;
+  }
+  console.log(" ... " + str);
+}
+// const data1 = printForecast([17, 21, 23]);
+const data2 = printForecast([12, 5, -5, 0, 4]);
+
+// console.log(degreeCelsius);
+
+// const arr = [17, 21, 23];
+// const thermometer = `...${arr[i]}${degreeCelsius} in ${days[i]} days ... ${arr[i]}${degreeCelsius} ... `;
+
+// console.log(thermometer);
+
+// 1) Understanding the problem
+// - Array transformaed to string, seperated by ...
+// //
+// `...${arr[i]}${degreeCelsius} in ${arr.length[i]} days ... ${arr[i]}${degreeCelsius} ...in ${days[i]} days ...  ${arr[i]}${degreeCelsius} ...in ${days[i]} days ... `
+
+*/
+
+//////////////////////// Section 5: Coding Challenge #1  COMPLETE ///////////////////////////
