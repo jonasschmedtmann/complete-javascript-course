c6800f05d54eb79b121a1dc9e4fe1fb3510faadd
jking026 <jamesjking026@gmail.com>
2021-02-03 09:57:01 +0900

    add value to textContent 'GMY' project

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent); //.textContent shows string content
+document.querySelector('.message').textContent = 'Correct Number 🥳';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
