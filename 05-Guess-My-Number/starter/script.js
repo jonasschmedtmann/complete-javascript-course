9ea7fc11de1dc32fd4d81fa3ec97b6dbdbd6e1b4
jking026 <jamesjking026@gmail.com>
2021-02-03 12:41:31 +0900

    add if statement for .message .score .guess

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent); //.textContent shows string content
 document.querySelector('.message').textContent = 'Correct Number 🥳';
 
@@ -8,3 +8,38 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20; //state variable
+document.querySelector('.number').textContent = secretNumber;
+
+//*addEventListener is the Best method & most used today* 2 arguments 1. action 2. function
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number 🥳';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 YOU LOST THE GAME!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 YOU LOST THE GAME!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
