d83af4c188bf028c28e04fb2d2439c9ef44dc60f
jking026 <jamesjking026@gmail.com>
2021-02-03 23:03:55 +0900

    add highScore variable to store properly

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,6 +12,7 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20; //state variable
+let highScore = 0;
 //Remove secret number when finished with project//
 // document.querySelector('.number').textContent = secretNumber;
 
@@ -33,6 +34,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('body').style.backgroundColor = '#60b347';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   } else if (guess > secretNumber) {
     //When guess is too high
     if (score > 1) {
@@ -59,19 +65,6 @@ document.querySelector('.check').addEventListener('click', function () {
 });
 
 ///////////Section-7 Coding Challenge #1 /////////////////////////
-
-// Implement a game rest functionality, so that the player can make a new guess!
-// Your tasks:
-
-// 1. Select the element with the 'again' class and attach a click event handler
-
-// 2. In the handler function, restore initial values of the 'score' and
-// 'secretNumber' variables
-
-// 3. Restore the initial conditions of the message, number, score and guess input
-// fields
-
-// 4. Also restore the original background color (#222) and number width (15rem)
 
 //@@@BEFORE VIDEO HELP@@@@
 //.again btn which resets everything back to default//
