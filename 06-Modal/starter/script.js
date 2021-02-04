8b9ed19a926eb702204c2aa044047734658f6e2a
jking026 <jamesjking026@gmail.com>
2021-02-04 18:21:23 +0900

    var names .model, .overlay, close-model, .show-modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const modal = document.querySelector('.model');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+//only 1 line of code doesn't need {}
+for (let i = 0; i < btnsOpenModal.length; i++)
+  console.log(btnsOpenModal[i].textContent); //NodeList(3) is like an array
