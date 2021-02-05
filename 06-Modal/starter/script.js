f2ef100f42041546fd7759514627874e9c8cab89
jking026 <jamesjking026@gmail.com>
2021-02-05 10:28:35 +0900

    add eventListener functions to modal class

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,11 +1,27 @@
 'use strict';
 
-const modal = document.querySelector('.model');
+const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+
+// modal.getElementsByClassName.display = 'block'; //same as classList but needs alot of work
+
+const openModal = function () {
+  console.log('Button Clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 //only 1 line of code doesn't need {}
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent); //NodeList(3) is like an array
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+//Rather than rewriting modal/overlay.classList over again write a function to avoid repeating code
+//Close modal
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
