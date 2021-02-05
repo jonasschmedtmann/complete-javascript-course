2914dfcc7775e4d968214ba238033cb423ec0e73
jking026 <jamesjking026@gmail.com>
2021-02-05 10:46:52 +0900

    add btn eventListener to esc btn to close modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -25,3 +25,11 @@ const closeModal = function () {
 };
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
