93203501f0e021857bc67ce8858a9d1104137006
jking026 <jamesjking026@gmail.com>
2021-02-07 17:42:22 +0900

    complete Pig Game + reset btn for 'New Game'

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -13,22 +13,35 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-//Set a value to .score
-score0El.textContent = 0;
-score1El.textContent = 0;
-//Hides .dice .png imgs
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0]; // Holds scores for player 0 & 1
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+//Starting conditions
+const init = function () {
+  scores = [0, 0]; // Holds scores for player 0 & 1
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  //Set a value to .score
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  //Hides .dice .png imgs
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
   activePlayer = activePlayer === 0 ? 1 : 0; // What player is active and switches
-  player0El.classList.toggle('player--active'); //.toggle checks for if player sis active
+  player0El.classList.toggle('player--active'); //.toggle checks for if player is active
   player1El.classList.toggle('player--active');
 };
 
@@ -82,4 +95,9 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
-122000;
+
+//Create a function to reset the game
+// 1. use const btnNew to interact with document.querySelector('.btn--new')
+
+// 2. function needs addeventListener
+btnNew.addEventListener('click', init); // Javascript will call init function rather than us
