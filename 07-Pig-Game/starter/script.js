2032af95665fe9ddb1dcd42b776e220f10a52481
jking026 <jamesjking026@gmail.com>
2021-02-07 12:30:26 +0900

    add Rolling dice functionality & storage

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,10 +3,37 @@
 //Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 //Set a value to .score
 score0El.textContent = 0;
 score1El.textContent = 0;
 //Hides .dice .png imgs
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; //CHANGE LATER
+  } else {
+    //switch to next player
+  }
+});
