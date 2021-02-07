e23ef1a68d9472b7877e7473cc6b46a2f98e4e46
jking026 <jamesjking026@gmail.com>
2021-02-07 10:52:30 +0900

    create .hidden class & score0/1El for .score

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+//Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+//Set a value to .score
+score0El.textContent = 0;
+score1El.textContent = 0;
+//Hides .dice .png imgs
+diceEl.classList.add('hidden');
