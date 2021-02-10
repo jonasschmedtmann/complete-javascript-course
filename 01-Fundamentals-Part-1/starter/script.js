e44e24fe33bd923b5add51cb7d8c2c8a62f8ab17
FlintFingerz <36845591+FlintFingerz@users.noreply.github.com>
2021-02-10 12:58:55 -0700

    updates

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,10 @@
+let js = 'amazing';
+if (js === 'amazing') alert('JavaScript is FUN!');
+
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 -10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
