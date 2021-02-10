96987922ffc3cd66b38a0fd6560f09615e2688ca
jking026 <jamesjking026@gmail.com>
2021-02-10 20:32:08 +0900

    add hoisting and TDZ practice examples L:96

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';
-
-function calcAge(birthYear) {
+/*
+function calcAge(birthzear) {
   const age = 2037 - birthYear;
 
   function printAge() {
@@ -34,3 +34,54 @@ const firstName = 'James';
 calcAge(1991);
 // console.log(age); // Won't be able to access age because it is one direction
 // printAge();// Still no access
+*/
+
+// // Hoisting with variables
+
+/*
+// console.log(me); //undefined
+// // console.log(job); //Uncaught ReferenceError: Cannot access 'job' before initialization
+// console.log(year);
+
+// var me = 'James';
+// let job = 'teacher';
+// const year = 1991;
+
+// Hoisting with Functions
+
+// console.log(addDecl(2, 3)); // returns 5 *works
+// console.log(addExpr(2, 3)); // Returns TMZ error
+// console.log(addArrow(2, 3)); // Returns TMZ error
+
+// Only one that works before declaration
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+// Var gets hoisted but is undefined
+var addArrow = (a, b) => a + b;
+
+//Example
+console.log(undefined);
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10; // Shows
+
+function deleteShoppingCart() {
+  console.log('All products deleted!'); // Returns undefined && triggers the if block
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x); //checks if x is a property of window *returns true
+console.log(y === window.y); //returns false
+console.log(z === window.z); //returns false
+
+*/
