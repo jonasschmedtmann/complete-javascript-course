8a813f1fd78dd2cb406a5b1494a3fd7e271f209f
jking026 <jamesjking026@gmail.com>
2021-02-11 10:05:10 +0900

    add notes and ex for Primitive & reference types

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -174,4 +174,23 @@ var addArrow = (a, b) => {
 addArrow(2, 5, 8);
 
 
+
+
+//Primitives Vs. Objects (Primitive Vs. Reference Types)
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+const friend = me;
+friend.age = 27;
+console.log('Friend:', friend); // Returns age: 27
+console.log('Me', me); // Returns age: 27
+
 */
