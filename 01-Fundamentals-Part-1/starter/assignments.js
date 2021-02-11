db2fb8e483f1f2530f1f661eabb2e384bba26fbe
FlintFingerz <36845591+FlintFingerz@users.noreply.github.com>
2021-02-10 18:06:31 -0700

    2/10/21 Updates

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,33 @@
+const country = "USA"
+const continent = "North America";
+let population = 330;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let language;
+
+const isIsland = false;
+language = "English";
+//isIsland = true;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+console.log(population / 2);
+
+population++;
+console.log(population);
+
+let finland = 6;
+console.log(country > finland);
+
+averageCountry = 33;
+console.log(country < averageCountry);
+
+let description1 = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
+console.log(description1)
+
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,10 +1,90 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 -10);
+/*let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
 console.log(23);
 
-let firstName = "Jonas";
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear - 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Schmedtmann';
+console.log(lastName);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// Assignment Operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1;
+x--;
+console.log(x);
+
+// Comparison Operators
+console.log(ageJonas > ageSarah); // You are making a statement and console.log returns the answer true or false. Also can use <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
