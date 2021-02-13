e6f5b1f04056dcf2169c336dab7259e5c3a1d9ae
jking026 <jamesjking026@gmail.com>
2021-02-13 10:48:25 +0900

    complete L: Destructuring Objects

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -25,9 +25,62 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
 
-//LESSON DESTRUCTURING ARRAYS(103)
+/*
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Via del Sole, 21',
+  starterIndex: 1,
+});
+
+//LESSSON: DESTRUCTURING OBJECTS (104)
+const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  opeingHours: hours,
+  categories: tags,
+} = restaurant;
+// console.log(restaurantName, hours, tags);
+
+//Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
+
+//Mutating Variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj); // Cannot assign to a codeblock error **wrap everything in parentheses('It works")
+
+// NESTED OBJECTS
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+// console.log(o, c);
+*/
+
+/*
+//LESSON: DESTRUCTURING ARRAYS(103)
 const arr = [2, 3, 4];
 const a = [0];
 const b = [1];
@@ -64,3 +117,5 @@ console.log(i, j, k);
 //Set default values for the variables when we extract them
 const [p = 1, q = 1, r = 1] = [8, 9]; //useful when using api
 console.log(p, q, r);
+
+*/
