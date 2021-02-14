c1e8362d4db95f84ae33be11bbff23c7e700e977
jking026 <jamesjking026@gmail.com>
2021-02-14 11:20:26 +0900

    S9-CC#1 complete with help will redo

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -259,3 +259,137 @@ const [p = 1, q = 1, r = 1] = [8, 9]; //useful when using api
 console.log(p, q, r);
 
 */
+
+/*
+////////////////////CODING CHALLENGE #1 ////////////////
+We're building a football betting app (soccer for my American friends �)!
+Suppose we get data from a web service about a certain game ('game' variable on
+next page). In this challenge we're gonna work with that data.
+Your tasks:
+*/
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+/*
+
+// //Before help
+
+// 1. Create one player array for each team (variables 'players1' and
+// 'players2')
+// */
+
+// // const { players1 = [], players2 = [] } = printGoals;
+// // const players1 = game.players[0];
+// // const players2 = game.players[1];
+
+// /*
+// 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+// */
+// const bayernMunich = game.players[0][0];
+// console.log(bayernMunich);
+// /*
+// 3. Create an array 'allPlayers' containing all players of both teams (22
+// players)
+// */
+// const allPlayers = [...players1, ...players2].sort(); //sort organizes allPlayers alphabetically
+// console.log(allPlayers);
+// /*
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+// new array ('players1Final') containing all the original team1 players plus
+// 'Thiago', 'Coutinho' and 'Perisic'
+// */
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// console.log(players1Final);
+// /*
+// 5. Based on the game.odds object, create one variable for each odd (called
+// 'team1', 'draw' and 'team2')
+// */
+// const team1 = game.odds.team1;
+// console.log(team1);
+// /*
+// 6. Write a function ('printGoals') that receives an arbitrary number of player
+// names (not an array) and prints each of them to the console, along with the
+// number of goals that were scored in total (number of player names passed in)
+// */
+// const printgoals = function (numPlayers, numGoals, numGoalsTotal) {};
+
+// /*
+// 7. The team with the lower odd is more likely to win. Print to the console which
+// team is more likely to win, without using an if/else statement or the ternary
+// operator.
+// */
+
+//////After help/(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)///////////////
+
+// 1. Destructoring game.players
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// 2. Create one variable 'gk', and ...fieldPlayers (rest operator)
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// 3. One array with allPlayers using a ...spread operator//
+const allPlayers = [...players1, ...players2].sort();
+console.log(allPlayers);
+
+// 4. players1 + substitutions
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
+// 5. Nested destructoring
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+// 6. printGoals function with arbitrary players
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored`);
+};
+
+// printGoals('Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels');
+// printGoals('Davies', 'Muller');
+printGoals(...game.scored); //...sread operator saves so much time
+
+// 7. print lower odds win the game
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
+team1 && team2 === draw && console.log('ITS A FU$$ING DRAW');
