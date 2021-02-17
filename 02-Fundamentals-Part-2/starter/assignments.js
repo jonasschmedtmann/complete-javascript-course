f51b2f2ff1c7c8f738ade0f1ef2bcb3d73be1a35
FlintFingerz <36845591+FlintFingerz@users.noreply.github.com>
2021-02-16 17:44:33 -0700

    fundamentals 2 complete

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -90,3 +90,14 @@ console.log(myCountry[`population`]);
 
 console.log(myCountry.describe());
 console.log(myCountry.checkIsland());
+
+
+// for (let voter = 0; voter <= 50; voter++) {
+//     console.log(`Voter number ${voter} is currently voting`);
+// }
+
+const percentages2 = [];
+for (let i = 0; i < populations.length; i++) {
+    percentages2.push(percentageOfWorld1(populations[i]));
+}
+console.log(percentages2);
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -359,7 +359,6 @@ console.log(jonas.summary());
 
 // // Will work like this
 // console.log(soren.goalWeight.call(abi));
-*/
 
 //////////////////////////////
 // Coding Challenge 3
@@ -387,3 +386,168 @@ mark.calcBmi();
 john.calcBmi();
 
 console.log(`${john.fullName}'s BMI (${john.bmi}) is ${john.bmi > mark.bmi ? `higher` : `lower`} than ${mark.fullName}'s (${mark.bmi})!`);
+
+////////////////////////////////////
+// Iteration: the for loop
+
+// console.log(`Lifting weights repetition 1`);
+// console.log(`Lifting weights repetition 2`);
+// console.log(`Lifting weights repetition 3`);
+// console.log(`Lifting weights repetition 4`);
+// console.log(`Lifting weights repetition 5`);
+// console.log(`Lifting weights repetition 6`);
+// console.log(`Lifting weights repetition 7`);
+// console.log(`Lifting weights repetition 8`);
+// console.log(`Lifting weights repetition 9`);
+// console.log(`Lifting weights repetition 10`);
+
+// for loop keeps runnign while condiition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+}
+
+
+///////////////////////////////////////////////////////
+// Looping Arrays, Breaking and Continuing.
+
+const jonas = [
+    `Jonas`,
+    `Schmedtmann`,
+    2037 - 1991,
+    `teacher`,
+    [`Michael`, `Peter`, `Steven`],
+    true
+];
+
+const types = [];
+
+// console.log(jonas[0])
+// console.log(jonas[1])
+// console.log(jonas[2])
+// console.log(jonas[3])
+// console.log(jonas[4])
+
+for (let i = 0; i < (jonas.length); i++) {
+    console.log(jonas[i], typeof jonas[i]);
+
+    // Filling types array
+    // types[i] = typeof jonas[i];
+    types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i]);
+}
+
+console.log(ages);
+
+// continue and break
+console.log(`--- ONLY STRINGS ---`);
+for (let i = 0; i < (jonas.length); i++) {
+    if (typeof jonas[i] !== `string`) continue;
+
+    console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log(`--- BREAK WITH NUMBER ---`);
+for (let i = 0; i < (jonas.length); i++) {
+    if (typeof jonas[i] === `number`) break;
+
+    console.log(jonas[i], typeof jonas[i]);
+}
+
+const jonas = [
+    `Jonas`,
+    `Schmedtmann`,
+    2037 - 1991,
+    `teacher`,
+    [`Michael`, `Peter`, `Steven`],
+    true
+];
+
+//////////////////////////////////////////////
+// Loop Backwards and Loops in Loops
+
+// 0, 1, ..., 4
+// 4, 3, ..., 0
+
+for (let i = (jonas.length - 1); i >= 0; i--) {
+    console.log(i, jonas[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`--------- Starting exercise ${exercise}`);
+
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+    }
+}
+
+///////////////////////////////////////////////////
+// The While Loop
+
+let rep = 1;
+while (rep <= 10) {
+    console.log(`WHILE: Lifting weights repetition ${rep}`);
+    rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log(`Loop is about to end...`);
+}
+*/
+
+//////////////////////////////////////////
+// Coding Challenge 4
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = function (bill) {
+    if (bill >= 50 && bill <= 300) {
+        const tip = bill * .15;
+        return tip;
+    } else {
+        const tip = bill * .20;
+        return tip;
+    }
+}
+
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
+
+// const calcAverage = function (arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         sum += arr[i];
+//     }
+//     return sum / arr.length;
+// }
+
+// console.log(calcAverage([2, 3, 7]));
+// console.log(calcAverage(totals));
+
+const calcAverage = function (arr) {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+
+console.log(calcAverage(totals));
