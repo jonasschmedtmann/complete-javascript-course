63de12758518817e828bfed6f1af7c1d408ebf71
jking026 <jamesjking026@gmail.com>
2021-02-18 20:14:45 +0900

    complete L: MAP: ITERATION

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -746,5 +746,45 @@ console.log(rest.size);
 
 console.log(rest.get(arr));
 */
-
+/*
 // LESSON: MAPS: ITERATION//
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 👨‍💻'],
+  [false, 'Try again!'],
+]);
+
+console.log(question);
+
+console.log(Object.entries(openingHours));
+//Use Map() when you have an array of objects and need a new map
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+//Iteration - Maps are also iterables
+//Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+const answer = 3;
+console.log(answer);
+// const answer = Number(prompt('Your answer'));
+
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log([...question]);
+console.log(...question.entries());
+console.log(...question.keys());
+console.log(...question.values());
+*/
+
+// LESSON: SUMMARY: WHICH DATA STRUCTURE TO USE? //
