52994271ea428e108b6000123e4a58f146b4fb55
jking026 <jamesjking026@gmail.com>
2021-02-18 19:48:26 +0900

    complete L: MAPS-FUNDAMENTALS

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -712,4 +712,39 @@ console.log(new Set('JamesJeremiahKing').size);
 
 */
 
+/*
 //  LESSON: MAPS: FUNDAMENTALS//
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon Portugal'));
+
+//Set method allows us to chain
+
+rest
+  .set('categories', [['Italian', 'Pizzeria', 'Vegetarian', 'Organic']])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name')); //Classico Italiano
+console.log(rest.get(true)); //=> 'We are open :D'
+console.log(rest.get('true')); //undefined
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+const arr = [1, 2]; //Need to set to a variable due to the location in the heap
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading'); //DOM manipulation
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+*/
+
+// LESSON: MAPS: ITERATION//
