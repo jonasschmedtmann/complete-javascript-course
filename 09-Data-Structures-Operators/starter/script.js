c261d82f946a23e42ea618d7864d93dbf63c6233
jking026 <jamesjking026@gmail.com>
2021-02-22 12:38:48 +0900

    complete: L: STRING METHODS Airline msg board

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1088,3 +1088,28 @@ document.querySelector('textarea').style.backgroundColor = 'yellow';
 document.querySelector('button').style.backgroundColor = 'blue';
 */
 ///////////CODING CHALLENGE #4-complete ///////////////
+/*
+//LESSON: STRING METHODS PRACTICE
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+//can seperate at any specified point
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+
+  // console.log(type);//checked the beginning of 'Delayed'
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(48);
+  console.log(output);
+}
+
+*/
