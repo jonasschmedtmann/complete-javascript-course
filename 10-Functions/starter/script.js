f822c4e3981a171d576da45daf7781653e685bc7
jking026 <jamesjking026@gmail.com>
2021-02-23 09:54:56 +0900

    🏁 L: PASS ARGUMENTS (VAL. vs. REF.)

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -29,5 +29,40 @@ createBooking('LH123', 5);
 //can't 'skip' arguments, unless you use undefined
 createBooking('LH123', undefined, 1000);
 */
-
+/*
 // LESSON: HOW TO PASS ARGUMENTS WORKS: VALUE VS. REFERENCE
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// // Is the same as doing ...spread ---copied
+// const fligthNum = flight;
+// const passenger = jonas;
+
+//change a persons passport number
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+*/
+// LESSON: FIRST-CLASS AND HIGHER ORDER FUNCTIONS
