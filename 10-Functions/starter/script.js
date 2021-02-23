18395fcdba5ff89d866fd558703e8a621143fdea
jking026 <jamesjking026@gmail.com>
2021-02-23 10:46:08 +0900

    🏁 L: FUNCTIONS ACCEPTING CALLBACKS- review

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -41,7 +41,7 @@ const jonas = {
 const checkIn = function (flightNum, passenger) {
   flightNum = 'LH999';
   passenger.name = 'Mr. ' + passenger.name;
-
+  console.log(jonas);
   if (passenger.passport === 24739479284) {
     alert('Check in');
   } else {
@@ -65,4 +65,39 @@ const newPassport = function (person) {
 newPassport(jonas);
 checkIn(flight, jonas);
 */
+/*
 // LESSON: FIRST-CLASS AND HIGHER ORDER FUNCTIONS
+//CHECK GOODNOTES//
+*/
+/*
+// FUNCTIONS ACCEPTING CALLBACK FUNCTIONS
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+
+transformer('JavaScript is the best!', oneWord);
+
+//JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+*/
+
+// LESSON: FUCNTIONS => FUNCTIONS
