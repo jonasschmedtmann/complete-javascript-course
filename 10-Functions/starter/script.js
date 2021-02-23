38fa1755305e6dc6196d604361348de392ac3e2a
jking026 <jamesjking026@gmail.com>
2021-02-23 09:36:14 +0900

    🏁 L: DEFAULT PARAMETERS + NOTES

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+// LESSON: DEFAULT PARAMETERS
+/*
+const bookings = [];
+
+//ES6 parameters can only work this way unless in the beginning
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  //ES5 style- 🤮
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LK123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 5);
+//can't 'skip' arguments, unless you use undefined
+createBooking('LH123', undefined, 1000);
+*/
+
+// LESSON: HOW TO PASS ARGUMENTS WORKS: VALUE VS. REFERENCE
