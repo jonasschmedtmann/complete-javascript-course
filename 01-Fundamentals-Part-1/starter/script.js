33ec47bb4976e54ce88e588521beca6ef4b4aced
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-04-26 02:23:12 +0200

    Coding Challenge #2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,5 @@
+// Coding Challenge #1
+
 // Mark and John are trying to compare their BMI (Body Mass Index), which is 
 // calculated using the formula:
 // BMI = mass / height ** 2 = mass / (height * height) (mass in kg 
@@ -29,3 +31,14 @@ console.log(markHigherBMI)
 // Test data:
 // § Data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 
 // m tall.
+
+
+// Coding Challenge #2
+
+/*
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+1. Print a nice output to the console, saying who has the higher BMI. The message can be either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
+
+HINT: Use an if/else statement 😉
