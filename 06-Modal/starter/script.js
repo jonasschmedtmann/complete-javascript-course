9e022f61009b6c8daa3b21e8e4edf3eeb883a071
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-05-23 01:49:41 +0200

    references

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('overlay');
+const btnCloseModal = document.querySelector('close-modal');
+const btnOpenModal = document.querySelector('show-modal');
+
