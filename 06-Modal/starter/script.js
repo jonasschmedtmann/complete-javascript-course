58cc8af23bf18f7fd297c12fcf03541446d38634
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-05-23 01:56:25 +0200

    event listener

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,4 +3,9 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('overlay');
 const btnCloseModal = document.querySelector('close-modal');
-const btnOpenModal = document.querySelectorAll('show-modal');
+const btnsOpenModal = document.querySelectorAll('show-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener(click, function () {
+    console.log('Butoni u shtyp');
+  });
