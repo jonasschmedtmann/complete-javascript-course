559bf81a876fd7595f983aa6cce16719f25987e6
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-05-23 02:05:34 +0200

    remove overlay class

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -9,4 +9,5 @@ for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener(click, function () {
     console.log('Butoni u shtyp');
     modal.classList.remove('hidden');
+    overlay.classList.remove('overlay');
   });
