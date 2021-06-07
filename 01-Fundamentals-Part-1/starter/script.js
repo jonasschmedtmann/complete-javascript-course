1cdc64e147d85c2e9f940c1dc2c8e46619cdf8ce
dionjwa1 <dionjwa@gmail.com>
2021-06-06 21:51:13 -0700

    myfirstvar

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,15 @@
-console.log('Dion');
+let country = 'US';
+let continent = 'North America';
+let population = '380MM';
+let dog = 'leo';
+let secondDog = 'tyson';
+alert('test');
+
+
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(dog);
+console.log(secondDog);
+
