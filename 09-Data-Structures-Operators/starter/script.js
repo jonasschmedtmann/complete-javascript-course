bed3635bd142a313aa275052d937e53c13d92ec3
KJ Roelke <kj.roelke@gmail.com>
2021-06-08 15:36:04 -0500

    learning

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -5,53 +5,91 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+
+const weekdays = [
+  'Monday',
+  'Tuesday',
+  'Wedesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  order: function (starterIndex, mainIndex) {
+
+  openingHours,
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
 
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
     console.log(
       `Order Received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}.`
     );
   },
 
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
 };
 
+const properties = Object.keys(openingHours);
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+
+console.log(openStr);
+
+const values = Object.values(openingHours);
+console.log(values);
+
+const entries = Object.entries(openingHours);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}.`);
+}
+
+/* // Looping Arrays: The For-Of Loop
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i}: ${el}`);
+}
+ */
+
+/* // CODING CHALLENGE #1
+// PROVIDED DATA
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borussia Dortmund',
@@ -91,27 +129,30 @@ const game = {
     team2: 6.5,
   },
 };
-const players1 = [...game.players[0]];
-console.log(players1);
-const players2 = [...game.players[1]];
-console.log(players2);
-const [gk1, ...fieldPlayers] = [...players1];
-console.log(gk1, fieldPlayers);
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
 
+// BEGIN CHALLENGE
+const [players1, players2] = game.players;
+const [gk, ...fieldPlayers] = players1;
+const allPlayers = [...players1, ...players2];
 const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
 
-const team1 = game.odds[0];
-const draw = game.odds[1];
-const team2 = game.odds[2];
 
-console.log(team1, draw, team2);
-const printGoals = function (...x) {
-  for (let i = 0; i < x.length; i++) {
-    let sum = [];
-    sum.push(x[i]);
-    console.log(`Goals were scored by ${sum}!`);
+// NOTES ON DESTRUCTURING NESTED OBJECTS
+// Destructuring nested objects uses multiple curly braces && parameters.
+// To rename a variable, simply use the : 
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+
+const printGoals = function (...players) {
+  for (let i = 0; i < players.length; i++) {
+    console.log(`${players.length} goals were scored by ${players}!`);
   }
 };
-printGoals(game.scored);
+printGoals(...game.scored);
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+
+// 7.
+
+// END CODING CHALLENGE #1 */
