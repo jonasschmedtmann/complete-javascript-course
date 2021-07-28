fa73a1cd16daf656fdea8bd9723469bcaf18befc
KJ Roelke <kj.roelke@gmail.com>
2021-07-28 17:06:57 -0500

    Init Bankist Landing Page

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -8,19 +8,17 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+function openModal() {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
-};
+}
 
-const closeModal = function () {
+function closeModal() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-};
+}
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
-
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
