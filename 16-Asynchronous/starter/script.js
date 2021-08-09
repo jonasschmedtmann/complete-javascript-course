ba257ae35754a0befe130e298cbbea62e715a177
KJ Roelke <kj.roelke@gmail.com>
2021-08-09 17:00:14 -0500

    Lecture 250

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,53 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+function renderCountry(data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+}
+
+function renderError(msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+}
+
+function getCountryData(country) {
+  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+    .then(
+      response => response.json() // returns a promise
+    )
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      if (!neighbour) return;
+      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data, 'neighbour');
+    })
+    .catch(err => {
+      // catches errors everywhere
+      console.error(err);
+      renderError(`Something went wrong. ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      // this only works on promises
+      countriesContainer.style.opacity = 1;
+    });
+}
+
+btn.addEventListener('click', () => getCountryData('portugal'));
