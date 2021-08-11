8b34055e6323e4112a896472f7a8a48b4751cdc2
KJ Roelke <kj.roelke@gmail.com>
2021-08-11 09:43:11 -0500

    Installing Parcel

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,10 +1,29 @@
 // Importing Module
 import * as ShoppingCart from './shoppingCart.js';
+import add from './shoppingCart.js';
+import cloneDeep from 'lodash';
 // import { addToCart, totalPrice as price, tQ } from './shoppingCart.js';
 
 console.log(`Importing Module`);
 ShoppingCart.addToCart('bread', 5);
 
 console.log(ShoppingCart.totalPrice, ShoppingCart.tQ);
-import add from './shoppingCart.js';
 add('pizza', 2);
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+console.log(`State Object:`);
+console.log(state);
+const stateClone = Object.assign({}, state);
+state.user.loggedIn = false;
+console.log(`State Clone:`);
+console.log(stateClone);
+
+const stateDeepClone = cloneDeep(state);
+console.log(`State Deep Clone:`);
+console.log(stateDeepClone);
