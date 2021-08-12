a246626d09f4ba5082e91c8133076340d65c9734
LK Agbo <71380008+Agbo-Samuel@users.noreply.github.com>
2021-08-12 01:06:22 +0000

    up

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+//using a function declaration 
+
+function calcAge(birthYear) { //this f(x) is defined inthe global scope
+    const age = 2037 - birthYear;
+
+    function printAge() {
+        const output = `${firstName}, YOu are ${age}, born in ${birthYear}`;
+        console.log(output);
+
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            var millenial1 = true;
+            const str = `Oh, and you're a millenila, ${firstName}`;
+        }
+        console.log(millenial1); //var variables are function scoped 
+
+        function add(a, b) {
+            return a + b;
+        }
+        printAge();
+        return age;
+    }
+
+    const firstName = 'Samuel'
+    calcAge(1991);
+
+    //The age variable is out of scope
+    console.log(age);
