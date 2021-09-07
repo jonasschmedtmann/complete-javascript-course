72cbb18ec67db49910e415617b3cbad5600a3422
LK Agbo <71380008+Agbo-Samuel@users.noreply.github.com>
2021-09-07 15:03:22 +0000

    Update script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -161,7 +161,7 @@ console.log(Pizza, Risotto, otherFood);
 const { sat, ...weekdays } = restaurant.openingHours;
 console.log(weekdays);
 
-// USING REST ON FUNCTION(Called REST ARGUEMENT)
+// USING REST ON FUNCTION(Called REST ARGUEMENTS)
 const add = function(...numbers) {
 
     let sum = 0;
@@ -177,3 +177,85 @@ add(8, 2, 5, 2, 4, 1, 4);
 // Taking it a step further we can do this;
 const xi = [23, 5, 7];
 add(...xi);
+
+
+
+//💎💎💎💎💎💎 SHORT-CIRCUITING || AND && 💎💎💎💎💎💎
+// If the first operand is a truthy value JS will not consider the second operand and will return the first
+// e.g
+//--------------- OR -------------------
+console.log(3 || 'Samuel');
+console.log('' || 'Samuel');
+console.log(true || 0);
+console.log(undefined || null);
+
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guest2 = restaurant.numGuests || 10;
+console.log(guest2);
+
+//--------------- AND -------------------
+// The AND operator does the opposite of the OR operator
+console.log(0 && 'Samuel');
+
+
+
+
+/* 🏋️‍♂️🏋️‍♂️ CODING CHALLENGE 
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimich',
+            'Gurezka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov. 9th 2021',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+};
