22dcd1f143dc1deb783f058930c9608d6f301b34
Jacob Cornwell <jrcornwell96@gmil.com>
2021-10-16 14:38:21 +0100

    data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,53 @@
+/*
+// Values and Variables
 let js = 'amazing';
-if (js === 'amazing') alert('JS is FUN');
-
 console.log(40 + 8 + 23 - 10);
+
+// Value is a piece of data
+
+console.log('Jacob');
+console.log(23);
+
+// Declaring a variable firstName, Work like a box which we put the value of Jacob in. If want to access the value we need to use the var name.
+let firstName = "Jacob";
+// Calling the variable firstName
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Conventions and rules to naming variables - camelCase.
+// If its a constant write it in uppercase (it wont change)
+// Be descriptive when naming variables
+let PI = 3.14;
+let myFirstJob = 'Programmer';
+console.log(myFirstJob);
+*/
+
+/*
+// Data Types
+
+// We decalre a new variable using the let key word
+let jsIsFun = true;
+console.log(jsIsFun);
+
+// typeof shows the type of value
+console.log(typeof jsIsFun); // boolean
+// console.log(typeof 12); // number
+// console.log(typeof 'test'); // string
+
+// When re assigning a variable we don't use let
+jsIsFun = 'YES!';
+console.log(typeof jsIsFun); // string
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+// let, const and var
+
