d3d3ce1e9c6f9a77166f4d113b9038d7a023e99b
Jacob Cornwell <jrcornwell96@gmil.com>
2021-10-16 15:24:18 +0100

    basic operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,5 @@
 /*
+///////////////////////////////////////////
 // Values and Variables
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
@@ -21,9 +22,8 @@ console.log(firstName);
 let PI = 3.14;
 let myFirstJob = 'Programmer';
 console.log(myFirstJob);
-*/
 
-/*
+///////////////////////////////////////////
 // Data Types
 
 // We decalre a new variable using the let key word
@@ -66,7 +66,57 @@ job = 'teacher';
 // We dont need to declare the variable (pretty terrible idea JS creates a property on the global object)
 lastName = 'Cornwell';
 console.log(lastName);
-*/
 
+
+///////////////////////////////////////////
 // Basic operators
 
+// Math operators
+const now = 2037;
+const ageJacob = now - 1996; // 42
+const ageSarah = now - 2020; // 17
+console.log(ageJacob, ageSarah);
+
+// 2 ** 3 means 2^3 or 2x2x2
+console.log(ageJacob * 2, ageJacob / 10, 2 ** 3);
+
+// we can use the + operator to concatenate two strings or more
+const firstName = 'Jacob';
+const lastName = 'Cornwell';
+console.log(firstName + ' ' + lastName);
+
+// assignment opperators
+let x = 10 + 5; // x = 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+x--; // x - x - 1 = 99
+console.log(x);
+
+// Comparision operators
+console.log(ageJacob > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+const now = 2037;
+const ageJacob = now - 1996;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+// left to right
+// console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+// user () parenthesis to be computed first
+const averageAge = (ageJacob + ageSarah) / 2;
+console.log(ageJacob, ageSarah, averageAge);
+
+*/
