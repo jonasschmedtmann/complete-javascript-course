dc1a2fed27f240eb56d54ce4e754bf9e78922c34
adnanaljasem~ <adnan.aljasem@powercoders.org>
2021-10-22 11:12:37 +0200

    nnnn

--- 01-Fundamentals-Part-1/final/script.js
+++ 01-Fundamentals-Part-1/final/script.js
@@ -467,7 +467,7 @@ const bill = 430;
 const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
 */
-///////////////////
+
 let js = "amazing";
 
 let firstName = "Jonas"; // declaring a variable
