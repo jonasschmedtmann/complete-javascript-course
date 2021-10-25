eb024deb45425245ecf471409adb9df34fe206dc
Jacob Cornwell <jrcornwell96@gmil.com>
2021-10-25 20:51:51 +0100

    coding challenge stage 4

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -346,7 +346,7 @@ if (hasDriverslicense && hasGoodVision && !isTired) {
     console.log('Someone else should drive...')
 }
 
-*/
+
 
 ////////////////////////////////////
 // Coding Challenge #3
@@ -443,3 +443,95 @@ GOOD LUCK 😀
 // } else {
 //     console.log('Neither team scored over 100 points AND had a better average.')
 // }
+
+////////////////////////////////////
+// Switch statements
+
+/*
+const day = 'saturday';
+
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break; // without the break the code keeps executing
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend');
+} else {
+    console.log('Not a valid day!')
+}
+
+////////////////////////////////////
+// The condinitoal ternary operator
+
+const age = 11;
+// age >= 18 ? console.log('I like to drink wine!') : console.log('I like to drink water!');
+
+// you can define it conditionally. also you can access variables as its not inside a block
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+// Variable not available outside of the block
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine';
+} else {
+    drink2 = 'water';
+}
+console.log(drink2);
+
+// You can use an expression it in a template literal
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+
+*/
+
+////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Steven wants to build a very simple tip calculator for whenever he goes eating in a resturant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+1. Your task is to caluclate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). Example: 'The bill was 275, the tip was 41.25, and the total value 316.25'
+
+TEST DATA: Test for bill values 275, 40 and 430
+
+HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+
+GOOD LUCK 😀
+
+const bill = 430;
+
+const tip = bill >= 50 && bill <= 300 ? bill * 15 / 100 : bill * 20 / 100;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value was ${bill + tip}`);
+
+*/
