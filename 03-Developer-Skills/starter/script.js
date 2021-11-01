cbaffd42a472d22820aa0d32244e2f55b3afa1f3
Guillermo <guillermo.cano.es@gmail.com>
2021-11-01 23:24:47 +0100

    #60 DeveloperSkills

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,53 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+// PROBLEM 1:
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmplitud = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+
+  console.log(max, min);
+
+  return max - min;
+};
+
+const amplitud = calcTempAmplitud(temperatures);
+console.log(amplitud);
+
+// PROBLEM 2:
+// Function should now receive 2 arrays of temps
+
+const calcTempAmplitudNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+
+  console.log(max, min);
+
+  return max - min;
+};
+
+const amplitudNew = calcTempAmplitudNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudNew);
