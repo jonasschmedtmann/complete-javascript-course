b4b4b891f753c5d820f3b5e6f2e76c2624e946e4
Guillermo <guillermo.cano.es@gmail.com>
2021-11-11 11:53:08 +0100

    #125 Section 10

--- 09-Data-Structures-Operators/starter/challenges.js
+++ 09-Data-Structures-Operators/starter/challenges.js
@@ -253,32 +253,133 @@ const game = {
 // }
 
 /************************************* ***********/
-// Coding Challenge #4
-// Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
-// The input will come from a textarea inserted into the DOM (see code below to insert the elements), and conversion will happen when the button is pressed.
-// Test data (pasted to textarea, including spaces):
-// underscore_case
+/*Coding Challenge #4
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below to insert the elements), and conversion will happen when the button is pressed.
+Test data (pasted to textarea, including spaces):
+underscore_case
+  first_name
+Some_Variable
+  calculate_AGE
+  delayed_departure
+
+  Should produce this output (5 separate console.log outputs):
+  underscoreCase ✅
+  firstName ✅✅
+  someVariable ✅✅✅
+  calculateAge ✅✅✅✅
+  delayedDeparture ✅✅✅✅✅
+
+  Hints:
+    § Remember which character defines a new line in the textarea 😉
+    § The solution only needs to work for a variable made out of 2 words, like a_b
+    § Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+    § This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+    Afterwards, test with your own test data!
+
+    GOOD LUCK 😀*/
+
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
+
+// let textName = document.querySelector('textarea');
+// const convertButton = document.querySelector('button');
+
+// textName.value = `underscore_case
 //   first_name
 // Some_Variable
 //   calculate_AGE
-//   delayed_departure
+//   delayed_departure`;
 
-//   Should produce this output (5 separate console.log outputs):
-//   underscoreCase ✅
-//   firstName ✅✅
-//   someVariable ✅✅✅
-//   calculateAge ✅✅✅✅
-//   delayedDeparture ✅✅✅✅✅
+// let textArray = [];
 
-//   Hints:
-//     § Remember which character defines a new line in the textarea 😉
-//     § The solution only needs to work for a variable made out of 2 words, like a_b
-//     § Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
-//     § This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+// //Funciona al darle al botón
+// const convertText = function () {
+//   //console.log(textName);
 
-//     Afterwards, test with your own test data!
+//   if (textName.value) {
+//     let newText = transformText();
 
-//     GOOD LUCK 😀
+//     for (const [i, row] of newText.entries()) {
+//       console.log(row.padEnd(20, ' ') + ` ${'#'.repeat(i + 1)}`);
+//     }
+//   } else {
+//     console.log('No hay nada!!');
+//   }
+// };
 
-//     document.body.append(document.createElement('textarea'));
-//     document.body.append(document.createElement('button'));
+// const transformLowerCase = function (textTotransform) {
+//   return textTotransform.toLowerCase();
+// };
+
+// const transformText = function () {
+//   let newText = [];
+
+//   // let rows = transformLowerCase(textName.value);
+//   const rows = textName.value.split('\n');
+
+//   for (const row of rows) {
+//     //let formatedWord = [];
+
+//     //formatedWord = row.toLowerCase().trim();
+
+//     let [firstPart, secondPart] = row.toLowerCase().trim().split('_');
+
+//     //Primer cáracter en mayúsculas
+//     secondPart = secondPart[0].toUpperCase() + secondPart.slice(1);
+//     //secondPart = secondPart.replace(secondPart[0], secondPart[0].toUpperCase());
+
+//     newText.push(firstPart + secondPart);
+//   }
+
+//   return newText;
+// };
+
+// convertButton.addEventListener('click', convertText);
+
+/**
+ // String exercise /////////////////
+ * //////////////////////////////////////
+ */
+
+// Result
+// @ Delayed Departure from FAO to TXL (11h25)
+//            Arrival from BRU to FAO (11h45)
+// @ Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const get_Flights = function () {
+  for (const flight of flights.split('+')) {
+    getData(flight);
+  }
+};
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+const getData = function (flight) {
+  let [status, from, to, time] = flight.split(';');
+
+  if (status[0] === '_') status = status.slice(1);
+
+  status = status.replace('_', ' ');
+
+  from = getCode(from);
+
+  to = getCode(to);
+
+  time = time.replace(':', 'h');
+
+  let formatedFlight = '';
+
+  formatedFlight = `${
+    status.includes('Delayed') ? '@' : ''
+  } ${status} from ${from} to ${to} (${time})`.padStart(45);
+
+  console.log(formatedFlight);
+};
+
+get_Flights();
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
 const openingHours = {
