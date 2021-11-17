dc5ee0e50b9a537d715ad8cd5257ce1a01d11203
AcChrome <ac.chan@live.com>
2021-11-17 02:00:55 -0800

    added click events handling

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number!';
 
@@ -8,3 +9,13 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number';
+  }
+});
