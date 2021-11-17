7325fec5fb29af400ecf6fb7db3e28d2553d35b7
AcChrome <ac.chan@live.com>
2021-11-16 23:57:00 -0800

    finished selecting and manipulating elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
