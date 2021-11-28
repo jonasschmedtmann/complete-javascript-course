611b5e271383ff97ed28c9b640e9dbe30ffadaf6
Saeedeh Moghimi <moghimi.saeedeh@gmail.com>
2021-11-28 10:46:01 -0500

    D3-data types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,16 +1,37 @@
-let js = 'Amazing'
-if (js === 'Amazing') alert('JavaScript is so FUN!')
-let calculate = 1 + 2 + 3 + 4 + 5 + 6 + 7
-console.log('calculate is: ' + calculate)
+// let js = 'Amazing'
+// if (js === 'Amazing') alert('JavaScript is so FUN!')
+// let calculate = 1 + 2 + 3 + 4 + 5 + 6 + 7
+// console.log('calculate is: ' + calculate)
 
-//camelcase
-let firstName = "Hedeesa"
-let lastName = "M"
+// //camelcase
+// let firstName = "Hedeesa"
+// let lastName = "M"
 
-let country = "Nowhere"
-let continent = "North Mars"
-let population = 1
+// let country = "Nowhere"
+// let continent = "North Mars"
+// let population = 1
 
-console.log(country)
-console.log(continent)
-console.log(population)
+// console.log(country)
+// console.log(continent)
+// console.log(population)
+
+//Data types 
+let javascriopIsFun = true;
+console.log(javascriopIsFun)
+console.log(typeof javascriopIsFun)
+//dynamic typing 
+javascriopIsFun = "YES!"
+console.log(javascriopIsFun)
+console.log(typeof javascriopIsFun)
+javascriopIsFun = 23
+console.log(javascriopIsFun)
+console.log(typeof javascriopIsFun)
+
+//undefined type
+let a
+console.log(a)
+console.log(typeof a)
+// Null story
+var myVar = null;
+console.log(myVar)
+console.log(typeof myVar)
