031bf6cd234f445121054d3fabbecbabb4e5d748
Saeedeh Moghimi <hedeesa@Saeedehs-MacBook-Air.local>
2021-11-27 23:38:56 -0500

    D2-variable

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -2,3 +2,15 @@ let js = 'Amazing'
 if (js === 'Amazing') alert('JavaScript is so FUN!')
 let calculate = 1 + 2 + 3 + 4 + 5 + 6 + 7
 console.log('calculate is: ' + calculate)
+
+//camelcase
+let firstName = "Hedeesa"
+let lastName = "M"
+
+let country = "Nowhere"
+let continent = "North Mars"
+let population = 1
+
+console.log(country)
+console.log(continent)
+console.log(population)
