0208b4b8e1e10df9c6433115c943d45e031fceac
Jacob Cornwell <jrcornwell96@gmil.com>
2022-01-11 20:06:28 +0000

    fundimentals part 2

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,87 @@
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function logger() {
+    console.log('my name is Jacob');
+}
+
+// Calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+///////////////////////////////////////
+// Function Declarations vs. Expressions
+
+// Function Declaration
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+
+///////////////////////////////////////
+// Arrow function
+
+// Arrow function - still a function expression
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years.`
+}
+
+console.log(yearsUntilRetirement(1991, 'Jacob'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
+
+*/
+
+///////////////////////////////////////
+// Functions calling other functions
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
