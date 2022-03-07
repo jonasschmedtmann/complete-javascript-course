e0566f1eeedbf4e33009953ce9567bec2baab6a7
Andy <awilliamscomposing@gmail.com>
2022-03-06 20:46:50 -0800

    Completing all the way through Secion 2, Lesson 24

--- 01-Fundamentals-Part-1/final/script.js
+++ 01-Fundamentals-Part-1/final/script.js
@@ -119,7 +119,9 @@ console.log(ageJonas, ageSarah, averageAge);
 // Coding Challenge #1
 
 /*
-Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: 
+BMI = mass / height ** 2 
+BMI = mass / (height * height). (mass in kg and height in meter).
 
 1. Store Mark's and John's mass and height in variables
 2. Calculate both their BMIs using the formula (you can even implement both versions)
