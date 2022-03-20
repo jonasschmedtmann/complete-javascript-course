d6ff84969e772d20ff3f0df0af209831333ff067
VincenzoGalante <mr.vincenzo.galante@gmail.com>
2022-03-20 23:15:14 +0100

    part 2

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,29 @@
+'use strict'; // has to be the first code in the file, forbids to do certain things, give visibility in dev console
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log('I can drive');
+
+///////////////
+
+function calcAge1(birthYear) { // paramter = placeholder, argument = what you put in when executing function
+    const age = 2022 - birthYear;
+    return age;
+}
+
+// fucntion declaration, can be called before they are declared in the file, js will jump to function
+function calcAge2(birthYear) { // refactored
+    return 2022 - birthYear;
+}  
+
+// function expression, "turning calcAge3 into a function that takes an argument", needs to be declared before usage
+// pro tip: use expressions, (1) forces you to declare them first and (2) values and functions stored in variables
+const calcAge3 = function (birthYear){
+    return 2022 - birthYear;
+}
+
+// arrow function, shorter way for function expression
+// pro tip: good for one liner functions
+const calcAge4 = birthYear => 2022 - birthYear;
