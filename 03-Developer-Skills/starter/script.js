797d34d0f3c20551e5fe555e3502656e0e6c3df6
Andy <awilliamscomposing@gmail.com>
2022-03-24 21:34:03 -0700

    Setting up VSCode

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,15 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = '23';
+if (x === 23) console.log(23);
+
+const calcAge = birthYear => 2037 - birthYear;
+
+// TODO, FIXME, BUG, VIDEO, LEC,  etc.
+
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
