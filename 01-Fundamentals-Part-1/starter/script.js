8252c823d9183e1b24a8b038d6c82684f795a994
Johanpersson97 <89947427+Johanpersson97@users.noreply.github.com>
2022-04-01 22:36:18 +0200

    Create script.js

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,29 @@
+// Values and variables
+
+let js = 'amazing';
+console.log(40+8+23-10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName="Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM ';
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer"
+let job2 = "Teacher"
+
+console.log(myFirstJob)
+
+// Datatypes
