f7a12329374f6fdbd21d2a33f28dea724dada04b
Daniel tukevkin <ekvdnl@gmail.com>
2022-04-15 20:56:24 +0300

    learning js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,6 +61,19 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `<div class="movements__row">
+                    <div class="movements__type movements__type--${type}">
+                    ${i + 1} ${type}</div>
+                    <div class="movements__value">${mov}</div>
+                  </div>`;
+  });
+};
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
