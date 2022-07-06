164c8a8540f81ee303b6b473baa83bd557647000
Erica Park <herykr@hotmail.com>
2022-07-06 14:44:51 -0300

    finish part1-fundamental1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -101,8 +101,16 @@ console.log(now - 1991 > now - 2018);
 
 ////////////////////////////////////
 // Operator Precedence
+*/
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
 
-console.log(now - 1991 > now - 2018);
+//console.log(now - 1991 > now - 2018);
+
+let x, y; // undifined
+x = y = 25 - 10 - 5;  // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;  // precisou colocar em parenteses
+console.log(ageJonas, ageSarah, averageAge);
