4e6afd7d0b7c1916cef90a266fe7de98fa77ff93
Filip Tichánek <filip.tichanek@bootiq.io>
2022-07-27 11:43:45 +0200

    challenge 4

---
+++ 01-Fundamentals-Part-1/starter/challenge4.js
@@ -0,0 +1,7 @@
+let bill = 430;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(
+  `The bill was ${bill}, the tip was ${tip} and the total value is ${
+    bill + tip
+  }`
+);
