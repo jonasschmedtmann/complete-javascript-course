5e27e9b7e2c99212ede135bfd77899e947f304c3
Abdul-Barri <lawalabdulbarri@gmail.com>
2022-07-28 13:21:43 +0100

    code 3 challenge

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,10 @@
+// let country = "Nigeria";
+// let continent = "Africa";
+// let population = 215.08;
+// let isIsland = country;
+// let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof country);
+// console.log(typeof population);
+// console.log(typeof language);
