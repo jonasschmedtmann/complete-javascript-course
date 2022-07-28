fcbf029be12e7136499d96799ddc2021ac761b85
Filip Tichánek <filip.tichanek@bootiq.io>
2022-07-28 14:09:44 +0200

    coding challenge 2 fix

--- 02-Fundamentals-Part-2/starter/challenge2.js
+++ 02-Fundamentals-Part-2/starter/challenge2.js
@@ -1,11 +1,16 @@
 function calcTip(billValue) {
   let tip;
   if (billValue >= 50 && billValue <= 300) {
-    tip = billValue * 1.15;
-  } else tip = billValue * 1.2;
+    tip = billValue * 0.15;
+  } else tip = billValue * 0.2;
   return tip;
 }
 
 const bill = new Array(125, 555, 44);
 const tips = new Array(calcTip(bill[0]), calcTip(bill[1]), calcTip(bill[2]));
-console.log(tips);
+const total = new Array(
+  bill[0] + tips[0],
+  bill[1] + tips[1],
+  bill[2] + tips[2]
+);
+console.log(total);
