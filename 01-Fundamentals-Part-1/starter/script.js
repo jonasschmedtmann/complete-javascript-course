c58f4323f3b2cac757f9d36d03aa06e22f74d556
Vishu Kumar <1992vishukumar@gmail.com>
2022-08-28 11:53:05 +0530

    10: values and variables assignement

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,14 @@
+
+/*
+    10: Values and Variables
+*/
+
+// let country = 'India';
+// let continent = 'Asia';
+// let countryPopulation = 1574113252608;
+
+// console.log(country);
+// console.log(continent);
+// console.log(countryPopulation);
+
+
