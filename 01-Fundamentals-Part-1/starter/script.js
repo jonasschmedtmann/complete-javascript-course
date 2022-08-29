a0fd79d646e8508729ed6048b45ec73dd75ea8d3
Vishu Kumar <1992vishukumar@gmail.com>
2022-08-29 16:57:46 +0530

    14: Basic operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -6,6 +6,7 @@
 const country = 'India';
 const continent = 'Asia';
 let countryPopulation = 1574113252608;
+6000000
 
 console.log(country);
 console.log(continent);
@@ -15,12 +16,12 @@ console.log(countryPopulation);
     12: Data Types
 */
 
-const isIland = false;
+const isIsland = false;
 let language;
 
 console.log(typeof country);
 console.log(typeof countryPopulation);
-console.log(typeof isIland);
+console.log(typeof isIsland);
 console.log(typeof language);
 console.log(typeof null);
 
@@ -32,3 +33,19 @@ language = 'Kannada';
 // isIland = false;
 
 console.log(language);
+
+/* 
+    14: Basic Operators
+*/
+
+console.log(countryPopulation / 2);
+console.log(countryPopulation++);
+
+const finlandPopulation = 6000000;
+console.log(countryPopulation > finlandPopulation);
+
+const avgPopulation = 33000000;
+console.log(countryPopulation < avgPopulation);
+
+const description = country + ' is in ' + continent + ', and its ' + Math.floor(countryPopulation / 100000) + ' million people speak ' + language;
+console.log(description);
