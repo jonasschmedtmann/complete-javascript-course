da549cac7157ab5ca0ec04ff7beb246384c03b10
Vishu Kumar <1992vishukumar@gmail.com>
2022-08-29 07:37:35 +0530

    11: Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,12 +3,23 @@
     10: Values and Variables
 */
 
-// let country = 'India';
-// let continent = 'Asia';
-// let countryPopulation = 1574113252608;
+let country = 'India';
+let continent = 'Asia';
+let countryPopulation = 1574113252608;
 
-// console.log(country);
-// console.log(continent);
-// console.log(countryPopulation);
+console.log(country);
+console.log(continent);
+console.log(countryPopulation);
 
+/* 
+    11: Data Types
+*/
 
+let isIland = false;
+let language;
+
+console.log(typeof country);
+console.log(typeof countryPopulation);
+console.log(typeof isIland);
+console.log(typeof language);
+console.log(typeof null);
