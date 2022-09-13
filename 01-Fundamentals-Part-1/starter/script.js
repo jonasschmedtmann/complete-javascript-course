029ada1eb6e3df9f41ba2ff07a515c4bc91ec376
Vinicius Farias <fsvinicius9@gmail.com>
2022-09-13 09:13:20 -0300

    Typeof boolean

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,13 @@
-let js = 'amazing';
+let js = "amazing";
 
-if(js === 'amazing') alert('Javascript is amazing')
-      40+8+23-10;
+if (js === "amazing") alert("Javascript is amazing");
+40 + 8 + 23 - 10;
+
+console.log("Jonas");
+
+let firstName = "Jonas";
+let jonas_matilda = "JM";
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true)
