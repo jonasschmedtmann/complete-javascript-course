6cd9ebf15f6100d64f2c660e03f1e56e88425a16
Vishu Kumar <1992vishukumar@gmail.com>
2022-09-19 10:44:19 +0530

    33: Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,17 +1,36 @@
 'use strict';
 
-/* 
+/*
     32: Activating strict mode
 */
 
-// let private = false;        // Error: Unexpected strict mode reserved word
-let hasDriversLicense = false;
-const passTest = true;
+// // let private = false;        // Error: Unexpected strict mode reserved word
+// let hasDriversLicense = false;
+// const passTest = true;
 
 
-// Using a wrong spelling intentionally here
-if (passTest) hasDriverLicense = true;      //Uncaught ReferenceError: hasDriverLicense is not defined
+// // Using a wrong spelling intentionally here
+// if (passTest) hasDriverLicense = true;      //Uncaught ReferenceError: hasDriverLicense is not defined
 
-if (hasDriversLicense) {
-    console.log(`Yayy!! I can drive`);
+// if (hasDriversLicense) {
+//     console.log(`Yayy!! I can drive`);
+// }
+
+/* 
+    33: Functions
+*/
+
+function logger () {
+    console.log(`My name is Vishu!`);
 }
+
+logger();   // Calling
+
+function fruitProcessor (applesCount, orangesCount) {
+    console.log(applesCount, orangesCount);
+    const juice = `Juice with ${applesCount} apples and ${orangesCount} oranges`;
+    return juice;
+}
+
+console.log(fruitProcessor(10, 3));
+console.log(fruitProcessor(7, 9));
