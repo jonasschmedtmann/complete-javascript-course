e40abcd2c98aa3290cbc5c3507ed641b015ef772
Tracey <tracy.yan2018@gmail.com>
2022-09-21 22:30:56 +1000

    update

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -15,32 +15,31 @@ const johnMass = 92;
 const johnHeight = 1.95;
 
 const markBMI = markMass / markHeight ** 2;
-const johnBMI =  johnMass / (johnHeight * johnHeight); 
+const johnBMI = johnMass / (johnHeight * johnHeight);
 
-const markHigherBMI = markBMI > johnBMI; 
+const markHigherBMI = markBMI > johnBMI;
 
 console.log(markBMI, johnBMI, markHigherBMI);
 
 // //#2
 
-if(markBMI > johnBMI ) {
-    console.log(`Mark's BMI(${markBMI}) is highter than John's(${johnBMI})!`)
+if (markBMI > johnBMI) {
+  console.log(`Mark's BMI(${markBMI}) is highter than John's(${johnBMI})!`);
 } else {
-    console.log("John's BMI(${johnBMI}) is higher than Mark's(${markBMI})!")
+  console.log("John's BMI(${johnBMI}) is higher than Mark's(${markBMI})!");
 }
-  
-const inputYear = '1991';
+
+const inputYear = "1991";
 console.log(inputYear);
 console.log(typeof imputYear);
-console.log(typeof NaN)
-console.log(Number(inputYear), inputYear)
+console.log(typeof NaN);
+console.log(Number(inputYear), inputYear);
 console.log(String(23), 23);
 
 // type coercion
-console.log('I am ' + 23 + ' years old')
-console.log('I am ' + String(23) + ' years old')
-console.log('23' - '10' - 3);
-console.log('23' + '10' + 3);
-console.log('23' * '2');
-console.log('23' / '2');
-
+console.log("I am " + 23 + " years old");
+console.log("I am " + String(23) + " years old");
+console.log("23" - "10" - 3);
+console.log("23" + "10" + 3);
+console.log("23" * "2");
+console.log("23" / "2");
