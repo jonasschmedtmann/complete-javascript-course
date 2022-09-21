69f6d4f5d133857b23540ab8528c48a1b3ddcbe2
Vishu Kumar <1992vishukumar@gmail.com>
2022-09-21 10:26:00 +0530

    Coding Challenge #1

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -68,23 +68,56 @@
 
 // console.log(yearsUntilRetirement(1992, 55));
 
-/* 
+/*
     36: Functions calling other functions
 */
 
-const cutFruitToPieces = (fruit) => fruit * 4;
+// const cutFruitToPieces = (fruit) => fruit * 4;
 
-const fruitProcessor = (applesCount, orangesCount) => {
+// const fruitProcessor = (applesCount, orangesCount) => {
 
-    const applePieces = cutFruitToPieces(applesCount)
-    const orangePieces = cutFruitToPieces(orangesCount)
+//     const applePieces = cutFruitToPieces(applesCount)
+//     const orangePieces = cutFruitToPieces(orangesCount)
 
-    console.log(applesCount, orangesCount);
-    const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces`;
-    return juice;
+//     console.log(applesCount, orangesCount);
+//     const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces`;
+//     return juice;
+// }
+
+// console.log(fruitProcessor(2, 3));
+
+/* 
+    Coding Challenge #1
+*/
+
+const calcAvg = (scores) => scores.reduce((a, b) => a + b, 0) / scores.length;
+
+const checkWinner = (avgDolphins, avgKoalas) => {
+    if (avgKoalas >= 2 * avgDolphins) {
+        return `Koalas win (${avgKoalas} vs. ${avgDolphins})`;
+    } else if (avgDolphins >= 2 * avgKoalas) {
+        return `Dolphins win (${avgDolphins} vs. ${avgKoalas})`;
+    } else {
+        return `No team wins...😢`
+    }
 }
 
-console.log(fruitProcessor(2, 3));
+// Test Data 1
+let dolphinsScores = [ 44, 23, 71 ];
+let koalasScores = [ 65, 54, 49 ];
+let avgDolphins = calcAvg(dolphinsScores);
+let avgKoalas = calcAvg(koalasScores);
+console.log(avgDolphins, avgKoalas);
+
+console.log(checkWinner(avgDolphins, avgKoalas));
+
+// Test Data 2
+dolphinsScores = [ 85, 54, 41 ];
+koalasScores = [ 23, 34, 27 ];
+avgDolphins = calcAvg(dolphinsScores);
+avgKoalas = calcAvg(koalasScores);
+console.log(avgDolphins, avgKoalas);
+console.log(checkWinner(avgDolphins, avgKoalas));
 
 
 
