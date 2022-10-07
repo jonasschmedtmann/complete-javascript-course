7041403a933ca875a1fc040bbc9053ba2d620636
Vishu Kumar <1992vishukumar@gmail.com>
2022-10-07 09:47:30 +0530

    72: Selecting and manipulating DOM elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+/* 
+    72: Selecting and manipulating DOM elements
+*/
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🍾 Correct Number!!';
+
+document.querySelector('.number').textContent = 20;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
+console.log(
+    document.querySelector('.guess').value
+);
