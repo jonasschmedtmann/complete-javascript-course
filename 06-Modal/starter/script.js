25ad8be487b953103f776dffc5c72cf2f11cc5a6
Vishu Kumar <1992vishukumar@gmail.com>
2022-10-11 18:25:13 +0530

    79: Project #2: Modal Window -- select elements for modal window

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+/* 
+    79: Project #2: Modal Window
+*/
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+
+}
