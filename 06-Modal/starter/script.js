9adacca1ca71cb2573d120e412c4cac464b0308c
Vishu Kumar <1992vishukumar@gmail.com>
2022-10-12 10:35:06 +0530

    81: Handling the 'Esc' Keypress Event

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,6 +3,7 @@
 /* 
     79: Project #2: Modal Window
     80: Working with Classes
+    81: Handling the 'Esc' Keypress Event
 */
 
 const modal = document.querySelector('.modal');
@@ -26,3 +27,11 @@ for (let i = 0; i < btnsShowModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+
+    if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+        console.log(event);
+        closeModal();
+    }
+})
