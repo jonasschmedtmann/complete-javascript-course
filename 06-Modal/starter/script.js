92ecdb71be4883ca8e326d1a2d15bc2162846490
Vishu Kumar <1992vishukumar@gmail.com>
2022-10-12 10:20:11 +0530

    80: Working with Classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -2,6 +2,7 @@
 
 /* 
     79: Project #2: Modal Window
+    80: Working with Classes
 */
 
 const modal = document.querySelector('.modal');
@@ -9,6 +10,19 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsShowModal.length; i++) {
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
 
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+    btnsShowModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
