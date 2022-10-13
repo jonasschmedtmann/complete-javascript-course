dd2dbb4b10c7a6ba84b936e4e5858c730c2c8a20
Vishu Kumar <1992vishukumar@gmail.com>
2022-10-13 10:41:03 +0530

    82: Project #3: PIG Game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+/* 
+    82: Project #3: PIG Game
+*/
+
+// Select the score elements
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
