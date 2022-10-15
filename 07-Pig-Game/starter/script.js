07b1e1906c43e082c74d19597a69d66df58b53ae
Vishu Kumar <1992vishukumar@gmail.com>
2022-10-15 16:11:50 +0530

    86: Resetting the game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -5,6 +5,7 @@
     83: Rolling the Dice
     84: Switching the Active Player
     85: Holding current score
+    86: Resetting the game
 */
 
 // Select the elements
@@ -18,16 +19,30 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnNew = document.querySelector('.btn--new');
 const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
-const current1El = document.querySelector('current--1');
+const current1El = document.getElementById('current--1');
+let currentScore, activePlayer, scores, playing;
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+init();
 
-let currentScore = 0;
-let activePlayer = 0;
-const scores = [0, 0];
-let playing = true;
+// const init = () => {
+function init () {
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    currentScore = 0;
+    activePlayer = 0;
+    scores = [0, 0];
+    playing = true;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--winner', 'player--active');
+}
+
+// init();
 
 btnRoll.addEventListener('click', function () {
     if (playing) {
@@ -54,10 +69,12 @@ btnHold.addEventListener('click', function () {
     }
 });
 
+btnNew.addEventListener('click', init)
+
 const switchPlayer = () => {
     scores[activePlayer] += currentScore;
     document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer];
-    if (scores[activePlayer] < 20) {
+    if (scores[activePlayer] < 100) {
         currentScore = 0;
         document.getElementById(`current--${activePlayer}`).textContent = currentScore;
         activePlayer = activePlayer === 0 ? 1 : 0;
@@ -71,12 +88,3 @@ const switchPlayer = () => {
     }
 }
 
-// const resetGame = () => {
-//     score0El.textContent = 0;
-//     score1El.textContent = 0;
-//     diceEl.classList.add('hidden');
-//     currentScore = 0;
-//     activePlayer = 0;
-//     scores = [0, 0];
-//     document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-// }
