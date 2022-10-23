2991674fae25f0d984188de1453053ffab72857d
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-10-23 23:33:31 +0900

    4 challenges and assignments

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,9 @@
+// let country = 'South Korea';
+// let continent = 'Asia';
+// let population = '52mln';
+
+// console.log(country);
+// console.log(continent);
+// console.log(population)
+
+
---
+++ 01-Fundamentals-Part-1/starter/challenge1.js
@@ -0,0 +1,181 @@
+
+
+
+
+// Challenge #1
+// let markWeigh = 78;
+// let markHeight = 1.69;
+
+// let johnWeigh = 92;
+// let johnHeight = 1.95;
+
+// let markWeigh = 95;
+// let markHeight = 1.88;
+
+// let johnWeigh = 85;
+// let johnHeight = 1.76;
+ 
+// // let bmiMark = markWeigh / markHeight ** 2;
+// let bmiMark = markWeigh / (markHeight * markHeight);
+
+// // let bmiJohn = johnWeigh / johnHeight ** 2;
+// let bmiJohn = johnWeigh / (johnHeight * johnHeight);
+
+// console.log(bmiMark);
+// console.log(bmiJohn);
+
+// let markHigherBMI = bmiMark > bmiJohn;
+
+// console.log(markHigherBMI)
+
+// let bmiMark = 26.87867813490267;
+// let bmiJohn = 27.44059917355372;
+
+// let markWeigh = 78;
+// let markHeight = 1.69;
+// let johnWeigh = 92;
+// let johnHeight = 1.95;
+
+
+
+// console.log(higherBMI);
+// console.log(`John's BMI (${bmiJohn}) is higher than Mark's (${bmiMark})!`)
+
+
+
+
+
+
+
+
+// Challenge #2
+
+// let markWeigh = 95;
+// let markHeight = 1.88;
+
+// let johnWeigh = 85;
+// let johnHeight = 1.76;
+
+// let bmiMark = markWeigh / markHeight ** 2;
+// let bmiJohn = johnWeigh / johnHeight ** 2;
+// console.log(bmiMark, bmiJohn);
+
+// if(bmiMark > bmiJohn) {
+//     console.log(`Mark's BMI (${bmiMark}) is higher than John's(${bmiJohn})`);
+// } else {
+//     console.log(`John's BMI (${bmiJohn}) is higher than Mark's(${bmiMark})`);
+// }
+
+
+
+
+
+
+
+// Challenge #3
+
+// const dolphins = '96, 108, 89'
+// const koalas = '88, 91, 110'
+
+// const dolphinsAverage = (96 + 108 + 89) / 3;
+// const koalasAverage = (88 + 91 + 110) / 3;
+
+// console.log(dolphinsAverage);
+// console.log(koalasAverage);
+
+// if(dolphinsAverage > koalasAverage) {
+//     console.log('Dolphins are the WINNERS!!!!🏆');
+// } else if(koalasAverage > dolphinsAverage) {
+//     console.log('Koalas are hte WINNERS!!!!!🏆');
+// } else {
+//     console.log('DRAW!!!!')
+// }
+
+
+// const dolphins = '97, 112, 101'
+// const koalas = '109, 95, 123'
+
+// const dolphinsAverage = (97 + 112 + 101) / 3;
+// const koalasAverage = (109 + 95 + 123) / 3;
+// const minScore = 100
+
+// console.log(dolphinsAverage);
+// console.log(koalasAverage);
+
+
+// if(dolphinsAverage > koalasAverage) {
+//     console.log('Dolphins are the WINNERS!!!!');
+// } else if(koalasAverage > dolphinsAverage) {
+//     console.log('Koalas are hte WINNERS!!!!!');
+// } else if(koalasAverage < minScore) {
+//     console.log('No winner')
+// } else if(dolphinsAverage < minScore){
+//     console.log('No winner')
+// } else {
+//     console.log('DRAW!!!')
+// }
+
+
+// const dolphins = '97, 112, 101'
+// const koalas = '109, 95, 123'
+
+// const dolphinsAverage = (197 + 12 + 101) / 3;
+// const koalasAverage = (92 + 95 + 123) / 3;
+// const minScore = 100
+
+// console.log(dolphinsAverage);
+// console.log(koalasAverage);
+
+
+// if(dolphinsAverage > koalasAverage && dolphinsAverage >= minScore) {
+//     console.log('Dolphins are the WINNERS!!!!');
+// } else if(koalasAverage > dolphinsAverage && koalasAverage >= minScore) {
+//     console.log('Koalas are the WINNERS!!!!!');
+// } else if( koalasAverage === dolphinsAverage && dolphinsAverage >= minScore && koalasAverage >= minScore){
+//     console.log('Both win!!!')
+// } else{
+//     console.log('No Winner')
+// }
+
+// const dolphins = '97, 112, 101'
+// const koalas = '109, 95, 106'
+// const minScore = 100
+
+
+// const dolphinsAverage = (97 + 112 + 101) / 3;
+// const koalasAverage = (109 + 95 + 106) / 3;
+
+// console.log(dolphinsAverage);
+// console.log(koalasAverage);
+
+// if(koalasAverage < dolphinsAverage >= 100 ) {
+//     console.log('Dolphins are the WINNERS!!!!');
+// } else if(koalasAverage > dolphinsAverage >= 100) {
+//     console.log('Koalas are hte WINNERS!!!!!');
+// } else if(koalasAverage === minScore){
+//     console.log('DRAW!!!!')
+// } else if(dolphinsAverage === minScore){
+//     console.log('DRAW!!!')
+// } else {
+//     console.log('Draw')
+// }
+
+
+
+
+// Challenge #4
+
+// const totalBill = 275
+// const totalBill = 40
+
+// const totalBill = 430
+// const total1 = totalBill + (totalBill * 0.15) ;
+// const total2 = totalBill + (totalBill * .2);
+// const totalTip1 = totalBill * 0.15;
+// const totalTip2 = totalBill * 0.2;
+
+
+// totalBill >= 50 && totalBill <= 300 ? console.log(`The bill was ${totalBill}, the tip was ${totalTip1}, the total value was ${total1}`) : console.log(`The bill was ${totalBill}, the tip was ${totalTip2}, the total value was ${total2}`)
+
+
+
