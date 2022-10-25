9b0d0d89bae7a2a3da2cb06982da4ede6d2a22fa
Jack Hendry <Jack.r.Hendry.p@gmail.com>
2022-10-25 11:23:26 -0400

    191-193; Event Propagation & Delegation & DOM traversing

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -7,6 +7,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+/////////////////////////////////////////////////////////
+
 
 const openModal = function (e) {
   e.preventDefault(); 
@@ -30,6 +34,69 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+btnScrollTo.addEventListener('click', function(e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current Scroll (x/y) ', window.pageXOffset, window.pageYOffset);
+
+  console.log('height/width viewport', document.documentElement.clientHeight, 
+  document.documentElement.clientWidth);
+
+
+  // old way 
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageYOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  // new way for smooth scroll 
+  section1.scrollIntoView({behavior: 'smooth'}); 
+}); 
+
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+//page Navigation 
+
+// not optimal adding event listener for all elements with .nav__link class
+// document.querySelectorAll('.nav__link').forEach( function(el) {
+//   el.addEventListener('click', function(e) {
+//     e.preventDefault(); 
+    
+//     const id = this.getAttribute('href'); 
+//     console.log(id);
+
+//     document.querySelector(id).scrollIntoView({behavior: 'smooth'}); 
+//   })
+// });
+
+
+
+/* 
+1. Add event listner to common parent elemnt 
+2. Determine wht element originated the event
+
+*/
+document.querySelector('.nav__links').addEventListener('click', function(e) {
+  e.preventDefault(); 
+  console.log(e.target);
+
+  // matching strategy 
+  if(e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href'); 
+    console.log(id);
+
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'}); 
+
+  }
+})
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+
+
 
 
 
@@ -81,7 +148,7 @@ document.querySelector('.btn--close-cookie').addEventListener('click', function(
 
 
 // Styles 
-message.style.backgroundColor = "#37383d"; 
+// message.style.backgroundColor = "#37383d"; 
 message.style.width = "120%";
 
 // how to get styles 
@@ -91,7 +158,7 @@ console.log(getComputedStyle(message).height);
 message.style.height = Number.parseFloat(getComputedStyle(message).height,10) + 30 + 'px'; 
 
 //working with custom Css variables 
-document.documentElement.style.setProperty('--color-primary', 'orangered'); 
+// document.documentElement.style.setProperty('--color-primary', 'orangered'); 
 
 //Atributes 
 
@@ -125,49 +192,94 @@ console.log(logo.dataset.versionNumber);
 
 //Implementing Smooth Scroll
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+
 
 console.log(btnScrollTo);
 console.log(section1);
 
-btnScrollTo.addEventListener('click', function(e) {
-  const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
-
-  console.log(e.target.getBoundingClientRect());
-
-  console.log('Current Scroll (x/y) ', window.pageXOffset, window.pageYOffset);
-
-  console.log('height/width viewport', document.documentElement.clientHeight, 
-  document.documentElement.clientWidth);
 
 
-  // old way 
-  // window.scrollTo({
-  //   left: s1coords.left + window.pageYOffset,
-  //   top: s1coords.top + window.pageYOffset,
-  //   behavior: 'smooth',
-  // });
+// const h1 = document.querySelector('h1'); 
 
-  // new way for smooth scroll 
-  section1.scrollIntoView({behavior: 'smooth'}); 
-}); 
+// const alertH1 = function(e) {
+//   alert('addEventListener: Great! '); 
+//   // Method 1 of remove event listener
+//   // h1.removeEventListener('mouseenter',alertH1); 
+// }
+// h1.addEventListener('mouseenter', alertH1); 
 
-const h1 = document.querySelector('h1'); 
-
-const alertH1 = function(e) {
-  alert('addEventListener: Great! '); 
-  // Method 1 of remove event listener
-  // h1.removeEventListener('mouseenter',alertH1); 
-}
-h1.addEventListener('mouseenter', alertH1); 
-
-setTimeout( () => HTMLQuoteElement.removeEventListener(
-  'mouseenter', alertH1), 3000); 
+// setTimeout( () => HTMLQuoteElement.removeEventListener(
+//   'mouseenter', alertH1), 3000); 
 
 // h1.onmouseenter = function(e) {
 //   alert('addEventListener: Great! ')
 // }; 
 
 // Event Propagation: Bubbling and Capturing 
+
+// Event Progpagation Practice 
+
+const randomInt = (min,max) => Math.floor(Math.random() * (max-min+1) + min);
+
+const randomColor = () => `rgb(${randomInt(0,255)}, ${randomInt(0,255)}, ${randomInt(0,255)} )`;
+
+
+// document.querySelector('.nav__link').addEventListener('click', function(e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('LINK', e.target,e.currentTarget);
+//   console.log(e.currentTarget === this);
+
+//   // stop propagation
+//   // e.stopPropagation(); 
+// })
+// document.querySelector('.nav__links').addEventListener('click', function(e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('Container', e.target,e.currentTarget);
+  
+// })
+// document.querySelector('.nav').addEventListener('click', function(e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('nav', e.target,e.currentTarget);
+// },);
+
+
+// DOM traversing 
+
+const h1 = document.querySelector('h1');
+
+// going downwards: child
+
+console.log(h1.querySelectorAll('.highlight'));
+
+console.log(h1.childNodes);
+console.log(h1.children);
+
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'blue';
+
+//going upwards: parents
+
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+// selects closes parent based on  class name 
+h1.closest('.header').style.background = 'var(--gradient-secondary)'
+
+// .closest is like the oppoiste of querySelector 
+h1.closest('h1').style.background = 'var(--gradient-primary)' 
+
+// Going sideways: Siblings 
+
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+//get all siblings 
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function(el) {
+  if(el !== h1) {
+    el.style.transform = 'scale(0.5)';
+  }
+}); 
