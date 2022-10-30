18ac6bc341cb7d92def1eb20aae1884e790b1474
tehila-shpayer <tehila1742@gmail.com>
2022-10-30 14:06:50 +0200

    c

--- 07-Pig-Game/final/script.js
+++ 07-Pig-Game/final/script.js
@@ -57,9 +57,8 @@ btnRoll.addEventListener('click', function () {
     if (dice !== 1) {
       // Add dice to current score
       currentScore += dice;
-      document.getElementById(
-        `current--${activePlayer}`
-      ).textContent = currentScore;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
     } else {
       // Switch to next player
       switchPlayer();
