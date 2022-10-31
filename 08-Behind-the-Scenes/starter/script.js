a3eac9889750e6ca036541e1e280498d5afaf326
Vishu Kumar <1992vishukumar@gmail.com>
2022-10-31 10:39:23 +0530

    100: Primitives vs Objects in Practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -119,49 +119,110 @@
 // vasudha.calcAge = vishu.calcAge;
 // vasudha.calcAge();  // this points to vasudha obj 
 
-/* 
+/*
     98: Regular functions vs Arrow functions
 */
 
-const vishu = {
-    firstName: 'Vishu',
-    birthYear: 1992,
-    calcAge: function () {
-        console.log(this);      // vishu obj
-        console.log(2022 - this.birthYear);
+// const vishu = {
+//     firstName: 'Vishu',
+//     birthYear: 1992,
+//     calcAge: function () {
+//         console.log(this);      // vishu obj
+//         console.log(2022 - this.birthYear);
 
-        // const isMillenial = function () {
-        //     console.log(this);      // this-- undefined as it is a normal function call
-        //     console.log(this.birthYear >= 1981 && this.birthYear <= 1996);  //Error
-        // };
-        // isMillenial();
+//         // const isMillenial = function () {
+//         //     console.log(this);      // this-- undefined as it is a normal function call
+//         //     console.log(this.birthYear >= 1981 && this.birthYear <= 1996);  //Error
+//         // };
+//         // isMillenial();
 
-        // Use arrow function if we need to use 'this' -- as arrow function gets lexical this
-        const isMillenialArrow = () => {
-            console.log(this);
-            console.log(this.birthYear >= 1981 && this.birthYear <= 1996);
-        }
+//         // Use arrow function if we need to use 'this' -- as arrow function gets lexical this
+//         const isMillenialArrow = () => {
+//             console.log(this);
+//             console.log(this.birthYear >= 1981 && this.birthYear <= 1996);
+//         }
 
-        isMillenialArrow();
-    },
+//         isMillenialArrow();
+//     },
 
 
 
-    greet: () => console.log(`Hello ${this.firstName}`)     // this.firstName -- will be undefined as window obj doesn't have firstName property
+//     greet: () => console.log(`Hello ${this.firstName}`)     // this.firstName -- will be undefined as window obj doesn't have firstName property
+// };
+
+// vishu.greet();
+// vishu.calcAge();
+
+// const addExpr = function (a, b) {
+//     console.log(arguments);     // gives the arguments in an array
+//     return a + b;
+// };
+
+// console.log(addExpr(2, 3, 4, 5));       // We can pass any number of arguments. but is handled in a more efficient way in ES6
+
+// const addArrow = (a, b) => {
+//     console.log(arguments);     // undefined
+//     return a + b;
+// };
+// console.log(addArrow(5, 6, 7, 8));
+
+/* 
+    100: Primitives vs Objects in Practice
+*/
+
+
+// Primitives
+let lastName = 'Kumar';
+let oldLastName = 'Pattage';
+lastName = 'Bhat';
+
+console.log(lastName, oldLastName);
+
+// Reference types
+const vasudha = {
+    firstName: 'Vasudha',
+    lastName: 'Pattaje',
+    age: 25
 };
 
-vishu.greet();
-vishu.calcAge();
+const marriedVasudha = vasudha;
+marriedVasudha.lastName = 'Kumar'   // Changes the property in the main vasudha obj
 
-const addExpr = function (a, b) {
-    console.log(arguments);     // gives the arguments in an array
-    return a + b;
+console.log('Before marriage', vasudha);
+console.log('after marriage', marriedVasudha);
+// marriedVasudha = {};    // gives error
+
+// Object Copy
+
+const vasudhaNew = {
+    firstName: 'Vasudha',
+    lastName: 'Pattaje',
+    age: 25,
+    family: ['Gopi', 'Nandalal']
 };
 
-console.log(addExpr(2, 3, 4, 5));       // We can pass any number of arguments. but is handled in a more efficient way in ES6
 
-const addArrow = (a, b) => {
-    console.log(arguments);     // undefined
-    return a + b;
-};
-console.log(addArrow(5, 6, 7, 8));
+// Object.assign -- only makes a shallow copy
+// i.e. -- details in the first level are copied to a new obj, but the objects(like family array here)
+// are not copied and they still point to the old reference
+const vasudhaNewCopy = Object.assign({}, vasudhaNew);
+
+
+// vasudhaNewCopy.family.push('Vishu');
+// vasudhaNewCopy.family.push('Sneha');
+
+console.log(vasudhaNew);
+console.log(vasudhaNewCopy);
+
+// Deep Copy with ...
+const vasudhaNewCopy1 = {...vasudhaNew};
+
+
+vasudhaNewCopy1.family.push('Vishu');
+vasudhaNewCopy1.family.push('Sneha');
+console.log(vasudhaNewCopy1);
+
+
+
+
+
