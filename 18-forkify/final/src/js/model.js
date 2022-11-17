b2e6f48b62b8290b95cc77775c489519876fa1ac
mustafa-suleiman <mustafa.mohammad.dk@gmail.com>
2022-11-17 20:43:50 +0200

    BUGS fixes: better error handling, form show again after close it, show alert when no search query, add API KEY dynamically to AJAX URL.

--- 18-forkify/final/src/js/model.js
+++ 18-forkify/final/src/js/model.js
@@ -31,7 +31,7 @@ const createRecipeObject = function (data) {
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await AJAX(`${API_URL}${id}?key=${KEY}`);
+    const data = await AJAX(`${API_URL}${id}${KEY ? `?key=${KEY}` : ''}`);
     state.recipe = createRecipeObject(data);
 
     if (state.bookmarks.some(bookmark => bookmark.id === id))
@@ -50,7 +50,9 @@ export const loadSearchResults = async function (query) {
   try {
     state.search.query = query;
 
-    const data = await AJAX(`${API_URL}?search=${query}&key=${KEY}`);
+    const data = await AJAX(
+      `${API_URL}?search=${query}${KEY ? `&key=${KEY}` : ''}`
+    );
     console.log(data);
 
     state.search.results = data.data.recipes.map(rec => {
--- 18-forkify/final/src/js/views/addRecipeView.js
+++ 18-forkify/final/src/js/views/addRecipeView.js
@@ -28,6 +28,10 @@ class AddRecipeView extends View {
   _addHandlerHideWindow() {
     this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
     this._overlay.addEventListener('click', this.toggleWindow.bind(this));
+    this._btnClose.addEventListener(
+      'click',
+      this._addHandlerGenerateForm.bind(this)
+    );
   }
 
   addHandlerUpload(handler) {
@@ -39,7 +43,87 @@ class AddRecipeView extends View {
     });
   }
 
-  _generateMarkup() {}
+  //persist the form after render an error
+  _addHandlerGenerateForm() {
+    //check if the form is not exist then generate it when press the close btn
+    if (!this._parentElement.querySelector('.upload__column'))
+      this._renderFormMarkup();
+  }
+
+  _renderFormMarkup() {
+    const markup = this._generateMarkup();
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  generateMarkup() {
+    return `<div class="upload__column">
+    <h3 class="upload__heading">Recipe data</h3>
+    <label>Title</label>
+    <input value="TEST12" required name="title" type="text" />
+    <label>URL</label>
+    <input value="TEST12" required name="sourceUrl" type="text" />
+    <label>Image URL</label>
+    <input value="TEST12" required name="image" type="text" />
+    <label>Publisher</label>
+    <input value="TEST12" required name="publisher" type="text" />
+    <label>Prep time</label>
+    <input value="23" required name="cookingTime" type="number" />
+    <label>Servings</label>
+    <input value="23" required name="servings" type="number" />
+  </div>
+
+  <div class="upload__column">
+    <h3 class="upload__heading">Ingredients</h3>
+    <label>Ingredient 1</label>
+    <input
+      value="0.5,kg,Rice"
+      type="text"
+      required
+      name="ingredient-1"
+      placeholder="Format: 'Quantity,Unit,Description'"
+    />
+    <label>Ingredient 2</label>
+    <input
+      value="1,,Avocado"
+      type="text"
+      name="ingredient-2"
+      placeholder="Format: 'Quantity,Unit,Description'"
+    />
+    <label>Ingredient 3</label>
+    <input
+      value=",,salt"
+      type="text"
+      name="ingredient-3"
+      placeholder="Format: 'Quantity,Unit,Description'"
+    />
+    <label>Ingredient 4</label>
+    <input
+      type="text"
+      name="ingredient-4"
+      placeholder="Format: 'Quantity,Unit,Description'"
+    />
+    <label>Ingredient 5</label>
+    <input
+      type="text"
+      name="ingredient-5"
+      placeholder="Format: 'Quantity,Unit,Description'"
+    />
+    <label>Ingredient 6</label>
+    <input
+      type="text"
+      name="ingredient-6"
+      placeholder="Format: 'Quantity,Unit,Description'"
+    />
+  </div>
+
+  <button class="btn upload__btn">
+    <svg>
+      <use href="${icons}#icon-upload-cloud"></use>
+    </svg>
+    <span>Upload</span>
+  </button>`;
+  }
 }
 
 export default new AddRecipeView();
