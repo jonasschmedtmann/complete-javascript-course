54dbcff3094f39b887af2e1857d38a3a39ed8e3b
Vishu Kumar <1992vishukumar@gmail.com>
2022-11-25 09:12:15 +0530

    120: Coding Challenge#3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -537,35 +537,89 @@ const game = {
     118: Maps: Iteration
 */
 
-const question = new Map([
-    ['question', 'What is the best programming language?'],
-    [1, 'C'],
-    [2, 'Java'],
-    [3, 'JavaScript'],
-    ['correct', 3],
-    [true, 'Correct 🏆'],
-    [false, 'Please try again!']
+// const question = new Map([
+//     ['question', 'What is the best programming language?'],
+//     [1, 'C'],
+//     [2, 'Java'],
+//     [3, 'JavaScript'],
+//     ['correct', 3],
+//     [true, 'Correct 🏆'],
+//     [false, 'Please try again!']
+// ]);
+
+// console.log(question);
+
+// // Structure is similar to Object.entries(). So, we can use it to create a new Map
+
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
+
+// // Quiz App
+// console.log(question.get('question'));
+// for (const [key, value] of question) {
+//     if (typeof key === 'number') {
+//         console.log(`Answer ${key}: ${value}`);
+//     }
+// }
+// const answer = +prompt('Your answer?');
+// console.log(question.get(answer === question.get('correct')));
+
+// // Convert map to array
+// console.log([...question]);
+// console.log(question.keys());
+// console.log([...question.values()]);
+
+/* 
+    120: Coding Challenge#3
+*/
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game.
+The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+    [17, '⚽️ GOAL'],
+    [36, '🔁 Substitution'],
+    [47, '⚽️ GOAL'],
+    [61, '🔁 Substitution'],
+    [64, '🔶 Yellow card'],
+    [69, '🔴 Red card'],
+    [70, '🔁 Substitution'],
+    [72, '🔁 Substitution'],
+    [76, '⚽️ GOAL'],
+    [80, '⚽️ GOAL'],
+    [92, '🔶 Yellow card'],
 ]);
 
-console.log(question);
+// 1: different game events
+const gameEventsSet = new Set(gameEvents.values());
+console.log(gameEventsSet);
+const events = [...gameEventsSet];
+console.log(events);
 
-// Structure is similar to Object.entries(). So, we can use it to create a new Map
+// remove (64) event from the game events log
+gameEvents.delete(64)
+console.log(gameEvents);
 
-console.log(Object.entries(openingHours));
-const hoursMap = new Map(Object.entries(openingHours));
-console.log(hoursMap);
+// Avg time for an event
+const gameKeyArray = [...gameEvents.keys()]
+const time = gameKeyArray[gameKeyArray.length - 1]
+// console.log(`An event happened, on average, every ${90 / gameEvents.size} minutes`);
+console.log(`An event happened, on average, every ${time / gameEvents.size} minutes`);
 
-// Quiz App
-console.log(question.get('question'));
-for (const [key, value] of question) {
-    if (typeof key === 'number') {
-        console.log(`Answer ${key}: ${value}`);
-    }
+for (const [minute, event] of gameEvents) {
+    const halfTime = 90 / 2;
+    const displayStr = minute <= halfTime ? '[FIRST HALF]' : '[SECOND HALF]';
+    console.log(`${displayStr} ${minute}: ${event}`);
 }
-const answer = +prompt('Your answer?');
-console.log(question.get(answer === question.get('correct')));
 
-// Convert map to array
-console.log([...question]);
-console.log(question.keys());
-console.log([...question.values()]);
