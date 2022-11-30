5abca3fe54aa193b7600e8eeeb1b1ba137a93b58
KP-Tim <kancanha@gmail.com>
2022-11-30 12:11:13 -0800

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -8,9 +8,23 @@
 // keepTry();
 // keepTry();
 
+// function declaration because it uses function keyword to declare a function
 function juiceMaker(apple, juice) {
   const juices = `${apple} apples and ${juice} juices`;
   return juices;
 }
 
 console.log(juiceMaker(3, 2));
+
+const appleJuice = juiceMaker(10, 3);
+console.log(appleJuice);
+
+// function declaration
+function calcAge1(birthYear) {
+  return 2022 - birthYear;
+}
+
+const age1 = calcAge1(1982);
+console.log(age1);
+
+// function expression
