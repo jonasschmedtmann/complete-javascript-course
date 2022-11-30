c48e8858615e318c07cd05472999c6c81150a088
KP-Tim <kancanha@gmail.com>
2022-11-30 09:44:53 -0800

    Update script.js

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,16 @@
+// let abs = "hello";
+
+// function keepTry(s) {
+//   console.log(s);
+// }
+
+// keepTry(abs);
+// keepTry();
+// keepTry();
+
+function juiceMaker(apple, juice) {
+  const juices = `${apple} apples and ${juice} juices`;
+  return juices;
+}
+
+console.log(juiceMaker(3, 2));
