4d13ff79e8f77662fe4ba124f836df0f06e32fd0
newfoundWebDev <117590761+newfoundWebDev@users.noreply.github.com>
2022-12-02 16:53:50 -0330

    Guess My Number Game +
    DOM Manip and guess game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,48 @@
 'use strict';
+
+// DOM Manipulation
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!';
+// //console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  //console.log(document.querySelector('.guess').value);
+  //document.querySelector('.message').textContent = 'Correct Number!';
+
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number Entered 😮‍💨';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = ' 👍 Correct Number';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '😂 Too High Bro';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🤪 Game Over';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '😂 Too Low Bro';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🤪 Game Over';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
