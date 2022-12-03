d431cc975f65fd6a872aa492e30dcd550e7fafc2
newfoundWebDev <117590761+newfoundWebDev@users.noreply.github.com>
2022-12-03 16:18:06 -0330

    Implemented reset on guess game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,7 +11,7 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -42,7 +42,20 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = '🤪 Game Over';
-      document.querySelector('.score').textContent = 0;
+      //document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+// Problem: Reset the game state to the original settings so player can start over
+// 1. create an event handler to lesten for the "Again Btn Click"
+// 2 Reset the initial vals
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.message').textContent = 'Lets Play again';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = '';
+  //console.log(guess);
 });
