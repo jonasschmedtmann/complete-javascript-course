c25f866b034935ebb43449c09a08e1a78524df56
KP-Tim <kancanha@gmail.com>
2022-12-08 14:49:54 -0800

    Guess my number proejct
    in the middle just in case i forget to submit

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+let text = document.querySelector('.message').textContent;
+
+text = 'hello';
+
+document.querySelector('.guess').textContent = 'x';
+
+document.querySelector('.check').addEventListener('click', function () {
+  document.querySelector('.guess').value = 5;
+});
