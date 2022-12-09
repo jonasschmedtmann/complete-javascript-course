f5e5c76c1bbb204f8ad5ac93e8e89115797bad5e
KP-Tim <kancanha@gmail.com>
2022-12-09 07:50:35 -0800

    I guess we changed it

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,10 +1,10 @@
 'use strict';
 
 // Generate random number
-let randomNumber = Math.ceil(Math.random() * 10);
+const randomNumber = Math.trunc(Math.random() * 10) + 1;
 console.log(randomNumber);
 
-document.querySelector('.score').textContent = 10;
+const score = document.querySelector('.score').textContent;
 
 // hide the randomNumber later
 document.querySelector('.number').textContent = randomNumber;
@@ -14,5 +14,7 @@ document.querySelector('.check').addEventListener('click', function () {
   console.log(guess);
   if (guess === randomNumber) {
     console.log('match');
-  } else if ()
+  } else if (guess > randomNumber) {
+    score;
+  }
 });
