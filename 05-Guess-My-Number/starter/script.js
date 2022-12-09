917a61e76a0999bdccfee655e59c20c1fba13640
KP-Tim <kancanha@gmail.com>
2022-12-09 14:40:08 -0800

    Keep going random game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,20 +1,30 @@
 'use strict';
 
 // Generate random number
-const randomNumber = Math.trunc(Math.random() * 10) + 1;
-console.log(randomNumber);
+const secretNumber = Math.trunc(Math.random() * 10) + 1;
+console.log(secretNumber);
 
-const score = document.querySelector('.score').textContent;
+// hide the secretNumber later
+document.querySelector('.number').textContent = secretNumber;
 
-// hide the randomNumber later
-document.querySelector('.number').textContent = randomNumber;
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
-  if (guess === randomNumber) {
-    console.log('match');
-  } else if (guess > randomNumber) {
-    score;
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (score <= 1) {
+    document.querySelector('.message').textContent = 'Game Over!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too high!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too low!';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
