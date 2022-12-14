5698c7ae887ce6634b422c6bb2947797f29f6063
KP-Tim <kancanha@gmail.com>
2022-12-14 12:18:29 -0800

    Pig-Game Done Initially
    I created the project and finished it.

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -29,50 +29,82 @@ diceEl.classList.add('hidden');
 let currentScore = 0;
 let activePlayer = 0;
 let totalScore = [0, 0];
+let isGameOver = false;
+
+// Switch player function
+const switchPlayer = function () {
+  // Switch player
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  // Switch player scores and background color
+  activePlayer = activePlayer ? 0 : 1;
+  // Set current score to 0
+  currentScore = 0;
+  // Switch player scores and background color
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // User rolls dice
 btnRoll.addEventListener('click', function () {
-  // Generate randome dice roll
-  const randomDice = Math.trunc(Math.random() * 6) + 1;
-  // Display dice roll
-  diceEl.classList.remove('hidden');
-  // Display rolled dice number
-  diceEl.src = `dice-${randomDice}.png`;
-  // Check if a number is one
-  if (randomDice !== 1) {
-    currentScore += randomDice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // If it is not one
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    console.log('changed');
-    // Switch player
-    activePlayer = activePlayer ? 0 : 1;
-    currentScore = 0;
-    // Switch player scores and background color
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
-    console.log(activePlayer);
+  if (!isGameOver) {
+    // Generate randome dice roll
+    const randomDice = Math.trunc(Math.random() * 6) + 1;
+    // Display dice roll
+    diceEl.classList.remove('hidden');
+    // Display rolled dice number
+    diceEl.src = `dice-${randomDice}.png`;
+    // Check if a number is one
+    if (randomDice !== 1) {
+      currentScore += randomDice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+    console.log(currentScore);
   }
-  console.log(currentScore);
 });
 
 btnHold.addEventListener('click', function () {
-  totalScore[`${activePlayer}`] = currentScore;
-  document.querySelector(`#score--${activePlayer}`).textContent =
-    totalScore[`${activePlayer}`];
-  console.log(totalScore);
-  document.getElementById(`score--${activePlayer}`).textContent = currentScore;
-  // Switch player scores and background color
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  activePlayer = activePlayer ? 0 : 1;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
-  totalScore[`${activePlayer}`] = currentScore;
-  document.querySelector(`#score--${activePlayer}`).textContent =
-    totalScore[`${activePlayer}`];
+  if (!isGameOver) {
+    totalScore[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      totalScore[activePlayer];
+    console.log(totalScore);
+    // If a player wins
+    if (totalScore[activePlayer] >= 20) {
+      // Game over
+      isGameOver = true;
+      // Display winner
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      // Hide the dice and score to zero
+      diceEl.classList.add('hidden');
+      document.getElementById(`current--${activePlayer}`).textContent = 0;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', function () {
+  if (isGameOver || !isGameOver) {
+    isGameOver = false;
+    totalScore = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--winner');
+    console.log(totalScore);
+  }
   console.log(activePlayer);
 });
