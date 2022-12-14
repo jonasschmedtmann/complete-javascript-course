d512ba1466546a1900c1510d4b42f7fd01677ae6
KP-Tim <kancanha@gmail.com>
2022-12-14 13:08:40 -0800

    Revert "Pig-Game Done Initially"
    This reverts commit 5698c7ae887ce6634b422c6bb2947797f29f6063.

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -18,57 +18,46 @@ const diceEl = document.querySelector('.dice');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
-let currentScore, activePlayer, totalScore, isGameOver;
+// Intial Setting
+// Set the starting score to 0
+score0El.textContent = 0;
+score1El.textContent = 0;
+// Hide the dice
+diceEl.classList.add('hidden');
 
-// Starting setting
-const init = function () {
-  currentScore = 0;
-  activePlayer = 0;
-  totalScore = [0, 0];
-  isGameOver = false;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  diceEl.classList.add('hidden');
-  player1El.classList.remove('player--winner');
-  player0El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
-};
-init();
-// Switch player function
-const switchPlayer = function () {
-  // Switch player
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  // Switch player scores and background color
-  activePlayer = activePlayer ? 0 : 1;
-  // Set current score to 0
-  currentScore = 0;
-  // Switch player scores and background color
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
-};
+// Current score
+let currentScore = 0;
+let activePlayer = 0;
+let totalScore = [0, 0];
 
 // User rolls dice
 btnRoll.addEventListener('click', function () {
-  if (!isGameOver) {
-    // Generate randome dice roll
-    const randomDice = Math.trunc(Math.random() * 6) + 1;
-    // Display dice roll
-    diceEl.classList.remove('hidden');
-    // Display rolled dice number
-    diceEl.src = `dice-${randomDice}.png`;
-    // Check if a number is one
-    if (randomDice !== 1) {
-      currentScore += randomDice;
-      document.getElementById(`current--${activePlayer}`).textContent =
-        currentScore;
-    } else {
-      switchPlayer();
-    }
-    console.log(currentScore);
+  // Generate randome dice roll
+  const randomDice = Math.trunc(Math.random() * 6) + 1;
+  // Display dice roll
+  diceEl.classList.remove('hidden');
+  // Display rolled dice number
+  diceEl.src = `dice-${randomDice}.png`;
+  // Check if a number is one
+  if (randomDice !== 1) {
+    currentScore += randomDice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // If it is not one
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    console.log('changed');
+    // Switch player
+    activePlayer = activePlayer ? 0 : 1;
+    currentScore = 0;
+    // Switch player scores and background color
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+    console.log(activePlayer);
   }
+  console.log(currentScore);
 });
 
 btnHold.addEventListener('click', function () {
@@ -97,4 +86,20 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNew.addEventListener('click', init);
+btnNew.addEventListener('click', function () {
+  if (isGameOver || !isGameOver) {
+    isGameOver = false;
+    totalScore = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--winner');
+    console.log(totalScore);
+  }
+  console.log(activePlayer);
+});
