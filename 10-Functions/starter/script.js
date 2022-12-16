a633f0cc6dbc83ebf26ca781cafe189887015707
Vishu Kumar <1992vishukumar@gmail.com>
2022-12-16 10:29:37 +0530

    131: Functions accepting callback functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,30 +1,62 @@
 'use strict';
 
-/* 
+/*
     128: Default Parameters
 */
 
-const bookings = [];
-const createBooking = function (flightNum,
-    numPassengers = 1,
-    price = 199 * numPassengers) {
+// const bookings = [];
+// const createBooking = function (flightNum,
+//     numPassengers = 1,
+//     price = 199 * numPassengers) {
 
-    // ES5
-    // numPassengers = numPassengers || 1;
-    // price = price || 199;
+//     // ES5
+//     // numPassengers = numPassengers || 1;
+//     // price = price || 199;
 
-    const booking = {
-        flightNum,
-        numPassengers,
-        price
-    };
-    bookings.push(booking);
+//     const booking = {
+//         flightNum,
+//         numPassengers,
+//         price
+//     };
+//     bookings.push(booking);
 
-    console.log(booking);
-    console.log(bookings);
+//     console.log(booking);
+//     console.log(bookings);
+// }
+
+// createBooking('1549');
+// createBooking('1549', 4);
+// createBooking('1549', 5);
+// createBooking('1549', undefined, 210);  // use default value for the second param
+
+/* 
+    131: Functions accepting callback functions
+*/
+
+const oneWord = (string) => string.replace(/ /g, '').toLowerCase();
+
+
+const upperCaseFirst = (str) => {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher order function
+const transformer = function (str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+
+    console.log(`Tranforming function name: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!!', oneWord);
+transformer('JavaScript is the best!!', upperCaseFirst);
+
+// JS uses callbacks all the time 😛
+
+const high5 = function () {
+    console.log('✋👋');
 }
 
-createBooking('1549');
-createBooking('1549', 4);
-createBooking('1549', 5);
-createBooking('1549', undefined, 210);  // use default value for the second param
+document.body.addEventListener('click', high5);
+['Vishu', 'Vasudha', 'Bharathi'].forEach(high5);
