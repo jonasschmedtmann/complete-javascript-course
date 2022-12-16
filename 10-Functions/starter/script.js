1351a6bc8e102ed1adad10fd1b52a30c2366c84d
Vishu Kumar <1992vishukumar@gmail.com>
2022-12-16 10:45:41 +0530

    132: Functions returning functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -29,34 +29,57 @@
 // createBooking('1549', 5);
 // createBooking('1549', undefined, 210);  // use default value for the second param
 
-/* 
+/*
     131: Functions accepting callback functions
 */
 
-const oneWord = (string) => string.replace(/ /g, '').toLowerCase();
+// const oneWord = (string) => string.replace(/ /g, '').toLowerCase();
 
 
-const upperCaseFirst = (str) => {
-    const [first, ...others] = str.split(' ');
-    return [first.toUpperCase(), ...others].join(' ');
-};
+// const upperCaseFirst = (str) => {
+//     const [first, ...others] = str.split(' ');
+//     return [first.toUpperCase(), ...others].join(' ');
+// };
 
-// Higher order function
-const transformer = function (str, fn) {
-    console.log(`Original string: ${str}`);
-    console.log(`Transformed string: ${fn(str)}`);
+// // Higher order function
+// const transformer = function (str, fn) {
+//     console.log(`Original string: ${str}`);
+//     console.log(`Transformed string: ${fn(str)}`);
 
-    console.log(`Tranforming function name: ${fn.name}`);
-};
+//     console.log(`Tranforming function name: ${fn.name}`);
+// };
 
-transformer('JavaScript is the best!!', oneWord);
-transformer('JavaScript is the best!!', upperCaseFirst);
+// transformer('JavaScript is the best!!', oneWord);
+// transformer('JavaScript is the best!!', upperCaseFirst);
 
-// JS uses callbacks all the time 😛
+// // JS uses callbacks all the time 😛
 
-const high5 = function () {
-    console.log('✋👋');
+// const high5 = function () {
+//     console.log('✋👋');
+// }
+
+// document.body.addEventListener('click', high5);
+// ['Vishu', 'Vasudha', 'Bharathi'].forEach(high5);
+
+/* 
+    132: Functions returning functions
+*/
+
+const greet = function (greeting) {
+    return function (name) {
+        console.log(`${greeting} ${name}`);
+    }
 }
 
-document.body.addEventListener('click', high5);
-['Vishu', 'Vasudha', 'Bharathi'].forEach(high5);
+const greeter = greet('Hi');
+greeter('Vishu');
+greeter('Vasudha');
+
+greet('Hello')('Bharathi');
+
+// Arrow
+const greetArrow = (greeting) => (name) => console.log(`${greeting} ${name}`);
+
+greetArrow('Hello')('Vishu');
+greetArrow('Hello')('Vasudha');
+greetArrow('Hello')('Bharathi');
