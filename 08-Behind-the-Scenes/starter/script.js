b3ca849ae55f7bb578d11f8054a4b52ef47a826e
KP-Tim <79594296+KP-Tim@users.noreply.github.com>
2022-12-24 07:17:06 -0800

    Keep going data structure lecture

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,95 @@
 'use strict';
+
+// const tim = {
+//   name: 'tim',
+//   year: 1982,
+//   calcAge: function () {
+//     return 2022 - this.year;
+//   },
+// };
+
+// console.log(tim.calcAge());
+
+// const tim = {
+//   year: 1977,
+//   name: 'Tim',
+//   calcAge4: function () {
+//     console.log(2022 - this.year);
+//     console.log(this);
+//   },
+// };
+
+// const matilda = {
+//   year: 2017,
+// };
+
+// tim.calcAge4();
+// matilda.calcAge5 = tim.calcAge4;
+// console.log(tim.calcAge4);
+// console.log(matilda.calcAge5);
+
+// matilda.calcAge5();
+
+// const tim = {
+//   firstName: 'tim',
+//   year: 1982,
+//   calcAge: function () {
+//     console.log(2022 - this.year);
+//     // console.log(this);
+
+//     const isMillenial = () => {
+//       console.log(this);
+//     };
+//     isMillenial();
+//   },
+// };
+
+// tim.calcAge();
+
+// let myAge = 30;
+// let oldAge = myAge;
+// myAge = 31;
+// console.log(oldAge);
+// console.log(myAge);
+
+// const me = {
+//   name: 'Tim',
+//   age: 40,
+// };
+
+// const friend = me;
+// friend.name = 'Jonas';
+// console.log(friend);
+// console.log(me);
+
+let lastName = 'Williams';
+let oldLastName = lastName;
+
+lastName = 'Davis';
+// console.log(lastName);
+// console.log(oldLastName);
+
+const tim = {
+  firstName: 'Tim',
+  lastName: 'Ha',
+  age: 39,
+};
+
+const jade = tim;
+jade.age = 44;
+jade.firstName = 'Tony';
+// console.log(jade);
+// console.log(tim);
+
+const tim2 = {
+  firstName: 'Tim',
+  lastName: 'Ha',
+  age: 39,
+  family: ['David', 'Jennifer', 'Lynn'],
+};
+
+const timCopy = Object.assign({}, tim2);
+timCopy.age = 99;
+timCopy.family[0] = 'Yong';
+console.log(tim2);
+console.log(timCopy);
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,116 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery: function ({
+    time = '20:00',
+    address,
+    mainIndex = 0,
+    starterIndex = 1,
+  }) {
+    console.log(
+      `Your main dish ${this.mainMenu[mainIndex]} and starter dish ${this.starterMenu[starterIndex]} will be delivered to ${address} at around ${time}.`
+    );
+  },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}.`);
+  },
 };
+const ing = prompt('Enter the ingredients').split(', ');
+console.log(ing);
+console.log(...ing);
+
+restaurant.orderPasta(...ing);
+
+/* ---------------------------------------------------------------
+restaurant.orderDelivery({
+  time: '12',
+  address: '257 s. manhattan',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+restaurant.orderDelivery({
+  address: '257 s. manhattan',
+  mainIndex: 2,
+});
+
+const { name, openingHours, categories } = restaurant;
+const {
+  sat: { open: o, close: c },
+} = openingHours;
+// console.log(o, c);
+
+// Nested objects
+
+// Default Values
+const {
+  ross = [],
+  name: restaurantName = [],
+  openingHours: hours,
+  location: loc,
+} = restaurant;
+// console.log(ross, restaurantName, hours, loc);
+
+// const { sat } = hours;
+// console.log(sat);
+
+// Mutating object variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+// console.log(obj);
+({ a, b } = obj);
+// console.log(a, b);
+
+/*
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+};
+
+const arr = [2, 3, 4];
+const [a, b, c] = arr;
+console.log(a, b, c);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+[secondary, main] = [main, secondary];
+console.log(main, secondary);
+const [starter, mains] = restaurant.order(2, 0);
+console.log(starter, mains);
+
+const nested = [2, 4, [5, 6]];
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default vales
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+---------------------------------------------------------------
+*/
+
+/*
+const arr = [7, 8, 9];
+const newArr = [5, 6, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+
+const newMenu = [...restaurant.mainMenu, 'push'];
+console.log(newMenu);
+
+const mainMenuCopy = [...restaurant.mainMenu];
+console.log(mainMenuCopy);
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu);
+
+*/
