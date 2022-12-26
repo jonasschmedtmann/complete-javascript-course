87c288aeb5375ca35fccf0a1503241e1c782e71b
Wessxn <parkermarcus282@gmail.com>
2022-12-26 00:17:11 -0500

    Completed coding challenge 3

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,82 @@
+/*const markMass = 95;
+const markHeight= 1.88; 
+const johnMass = 85; 
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnWeight ** 2;
+
+if(markBMI > johnBMI){
+    console.log(`Mark's BMI is higher than John's!\nMark's BMI (${markBMI}) is higher than 
+    John's (${johnBMI})`); 
+} else {
+    console.log(`John's BMI is higher than Mark's!\nJohn's BMI (${johnBMI}) is higher than 
+    Mark's (${markBMI})`);
+}
+*/
+
+/*
+const money = 0; 
+if(money) {
+    console.log("don't spend it all");
+} else {
+    console.log("get a job"); 
+}
+
+let height; 
+if(height){
+    console.log(`${height}`)
+} else {
+    console.log("state your height dumbass")
+}
+
+const age = 18;
+if(age === 18) console.log("Adult");
+
+const favourite = Number(prompt("What is your favourite number"));
+console.log(favourite);
+
+if(favourite === 23){
+    console.log('Nice')
+} else if (favourite === 7) {
+    console.log('Cool')
+} else {
+    console.log('aww')
+}
+const johnWeight = 1.76;
+
+if(favourite !== 23) console.log("ok fuck you then"); 
+*/ 
+
+/* const hasDriversLicense = true;
+const hasGoodVision = true; 
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+
+
+const isTired = false; 
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if(hasDriversLicense && hasGoodVision && !isTired){
+    console.log("Let her drive "); 
+} else {
+    console.log("No");
+}
+*/
+
+/*
+const dolphinsAverageScore = (97 + 112 + 101) / 3;
+const koalasAverageScore = (88 + 91 + 110) / 3 ; 
+console.log(dolphinsAverageScore);
+console.log(koalasAverageScore);
+
+if(dolphinsAverageScore > koalasAverageScore &&  dolphinsAverageScore >= 100) {
+    console.log('Dolphins win!');
+} else if(dolphinsAverageScore === koalasAverageScore && dolphinsAverageScore >= 100 && koalasAverageScore >= 100){
+    console.log("We're tied!");
+}else if(koalasAverageScore > dolphinsAverageScore && koalasAverageScore >= 100) {
+   console.log("Koalas win!");
+} else{
+    console.log("No one wins");
+}
+*/
