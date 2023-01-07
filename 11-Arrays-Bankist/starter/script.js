1c0005de72d282b7649c194bd213b4763e0648d8
Vishu Kumar <1992vishukumar@gmail.com>
2023-01-07 18:28:58 +0530

    148: Coding Challenge #1

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,6 +61,23 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+    containerMovements.innerHTML = '';
+    movements.forEach((movement, i) => {
+        const type = movement > 0 ? 'deposit' : 'withdrawal'
+        const html = `
+            <div class="movements__row">
+                <div class="movements__type movements__type--${type}">${i + 1} deposit</div>
+                <div class="movements__date">3 days ago</div>
+                <div class="movements__value">${movement}</div>
+            </div>`;
+
+        containerMovements.insertAdjacentHTML('afterbegin', html);
+    });
+}
+
+displayMovements(account1.movements)
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -139,26 +156,69 @@ const inputClosePin = document.querySelector('.form__input--pin');
 //     }
 // });
 
-/* 
+/*
     145: forEach with Maps and Sets
 */
 
-// Map
-const currencies = new Map([
-    ['USD', 'United States dollar'],
-    ['EUR', 'Euro'],
-    ['GBP', 'Pound sterling'],
-]);
+// // Map
+// const currencies = new Map([
+//     ['USD', 'United States dollar'],
+//     ['EUR', 'Euro'],
+//     ['GBP', 'Pound sterling'],
+// ]);
 
-currencies.forEach((value, key, map) => {
-    console.log(`${key}: ${value}`);
-});
+// currencies.forEach((value, key, map) => {
+//     console.log(`${key}: ${value}`);
+// });
 
-// SET
-const uniqueCurrencies = new Set(['USD', 'EUR', 'INR', 'GBP', 'USD', 'YEN']);
-console.log(uniqueCurrencies);
+// // SET
+// const uniqueCurrencies = new Set(['USD', 'EUR', 'INR', 'GBP', 'USD', 'YEN']);
+// console.log(uniqueCurrencies);
 
-// Key not available in set. So, value appears in that place
-uniqueCurrencies.forEach((value, _, set) => {
-    console.log(value, _, set);
-})
+// // Key not available in set. So, value appears in that place
+// uniqueCurrencies.forEach((value, _, set) => {
+//     console.log(value, _, set);
+// })
+
+/* 
+    148: Coding Challenge #1
+*/
+
+
+/* 
+    Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, 
+    and stored the data into an array (one array for each). 
+    For now, they are just interested in knowing whether a dog is an adult or a puppy. 
+    A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+    Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+    1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! 
+        So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+    2. Create an array with both Julia's (corrected) and Kate's data
+    3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+    4. Run the function for both test datasets
+
+    HINT: Use tools from all lectures in this section so far 😉
+
+    TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+    TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const checkDogs = function (dogsJulia, dogsKate) {
+    const dogsJuliaCorrect = dogsJulia.slice(1, 3);
+    const allDogs = dogsJuliaCorrect.concat(dogsKate);
+    console.log(dogsJuliaCorrect, dogsKate, allDogs);
+    allDogs.forEach((dog, i) => {
+        if (dog >= 3) {
+            console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+        } else {
+            console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+        }
+    });
+}
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
