5ec82dccd91f9c0ba220364bd52e3a480a412369
Vishu Kumar <1992vishukumar@gmail.com>
2023-01-08 17:17:27 +0530

    151: Computing Usernames

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -76,7 +76,32 @@ const displayMovements = function (movements) {
     });
 }
 
-displayMovements(account1.movements)
+displayMovements(account1.movements);
+
+/*
+    151: Computing Usernames
+*/
+
+
+const name1 = 'Steven Thomas Williams';
+
+const calculateUserName = (accounts) =>
+
+    accounts.forEach(account => {
+        account.username = account.owner.toLowerCase()
+            .split(' ')
+            .map(word => word[0])
+            .join('');
+
+    });
+
+calculateUserName(accounts)
+console.log(accounts);
+// const userNames = accounts.map((account) =>
+//     calculateUserName(account.owner))
+// console.log(userNames);
+
+
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -180,20 +205,20 @@ displayMovements(account1.movements)
 //     console.log(value, _, set);
 // })
 
-/* 
+/*
     148: Coding Challenge #1
 */
 
 
-/* 
-    Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, 
-    and stored the data into an array (one array for each). 
-    For now, they are just interested in knowing whether a dog is an adult or a puppy. 
+/*
+    Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age,
+    and stored the data into an array (one array for each).
+    For now, they are just interested in knowing whether a dog is an adult or a puppy.
     A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
 
     Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
 
-    1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! 
+    1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs!
         So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
     2. Create an array with both Julia's (corrected) and Kate's data
     3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
@@ -207,18 +232,40 @@ displayMovements(account1.movements)
 GOOD LUCK 😀
 */
 
-const checkDogs = function (dogsJulia, dogsKate) {
-    const dogsJuliaCorrect = dogsJulia.slice(1, 3);
-    const allDogs = dogsJuliaCorrect.concat(dogsKate);
-    console.log(dogsJuliaCorrect, dogsKate, allDogs);
-    allDogs.forEach((dog, i) => {
-        if (dog >= 3) {
-            console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
-        } else {
-            console.log(`Dog number ${i + 1} is still a puppy 🐶`);
-        }
-    });
-}
+// const checkDogs = function (dogsJulia, dogsKate) {
+//     const dogsJuliaCorrect = dogsJulia.slice(1, 3);
+//     const allDogs = dogsJuliaCorrect.concat(dogsKate);
+//     console.log(dogsJuliaCorrect, dogsKate, allDogs);
+//     allDogs.forEach((dog, i) => {
+//         if (dog >= 3) {
+//             console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+//         } else {
+//             console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+//         }
+//     });
+// }
 
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
-checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+/*
+    150: The map method
+*/
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// const euroToUsd = 1.1;
+// const movementsUSD = movements.map((mov) => mov * euroToUsd);
+
+// console.log(movements);
+// console.log(movementsUSD);
+
+// const movementsUSDfor = [];
+// for (const mov of movements) {
+//     movementsUSDfor.push(mov * euroToUsd);
+// }
+// console.log(movementsUSDfor);
+
+// const movementsDescription = movements.map((mov, i) =>
+//     `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`);
+// console.log(movementsDescription);
