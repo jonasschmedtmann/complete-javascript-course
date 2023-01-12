f307233b72b6cee2a9fdf6e2e8de4641450fbb72
Yanina Amatore <yaninaamatore@gmail.com>
2023-01-13 11:14:31 +1300

    while loops

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -196,21 +196,96 @@ GOOD LUCK 😀
 // console.log(myCountry.checkIsland())
 
 
-const jonasArray = [
-  'jonas',
-  'Schmedtmann',
-  20097-1991,
-  'teacher',
-  ['michael', 'peter', 'steven'], 
-  true
-]
+// const jonasArray = [
+//   'jonas',
+//   'Schmedtmann',
+//   2097-1991,
+//   'teacher',
+//   ['michael', 'peter', 'steven'], 
+//   true
+// ]
 
-let types = []
+// let types = []
 
-for(let i= 0 ; i < jonasArray.length ; i++) {
-  console.log(jonasArray[i], typeof jonasArray[i])
+// for(let i= 0 ; i < jonasArray.length ; i++) {
+//   console.log(jonasArray[i], typeof jonasArray[i])
 
-  types[i] = typeof jonasArray[i]
-}
+  // types[i] = typeof jonasArray[i]
+//   types.push(typeof jonasArray[i])
+// }
 
-console.log(types)
+// console.log(types)
+
+// const years =[1991, 2007, 1969, 2020]
+
+// const ages=[]
+// for ( let i=0 ; i<years.length ; i++ ) { 
+//  ages.push(2037 - years[i])
+// }
+
+// console.log(ages)
+
+// Continue & Break
+
+// for ( let i = 0 ; i < jonasArray.length ; i++ ) {
+//   if (typeof jonasArray[i] !== 'string') continue 
+
+//   console.log( jonasArray[i], typeof jonasArray[i])
+// }
+
+// for ( let i = 0 ; i < jonasArray.length ; i++ ) {
+//   if (typeof jonasArray[i] === 'number') break
+
+//   console.log( jonasArray[i], typeof jonasArray[i])
+// }
+
+// for (let exercise =1 ; exercise < 4 ; exercise++) {
+//   console.log( `------ starting exercise ${exercise}`)
+
+//     for (let rep = 1 ; rep < 6 ; rep++) {
+//        console.log( ` ${exercise}: Lifting weight repetition ${rep}`)
+//     }
+// }
+
+// While loop
+
+// only need the condition , the counter goes outside the loop
+// it can be any condition 
+
+// let rep = 1
+// while (rep<6){
+//   console.log( `While: Lifting weight repetition ${rep}`)
+//   rep ++
+// }
+
+// let dice =  Math.trunc(Math.random() *6)+1
+
+// console.log(dice)
+
+// while(dice !==6){
+//   console.log(` you rolled a ${dice}`)
+//   dice = Math.trunc(Math.random() *6)+1
+//   if (dice === 6) console.log(` Loop is about the end ${dice}`)
+
+// }
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Let's improve Steven's tip calculator even more, this time using loops!
+
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+
+TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+HINT: Call calcTip in the loop and use the push method to add values to the tips and totals arrays 😉
+
+4. BONUS: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it:
+  4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
+  4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
+  4.3. Call the function with the 'totals' array
+
+GOOD LUCK 😀
