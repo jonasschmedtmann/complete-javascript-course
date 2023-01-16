095ca7ed478513ee530dbbe23be57fff9ea44d18
Vishu Kumar <1992vishukumar@gmail.com>
2023-01-16 10:29:13 +0530

    162: flat and flatMap: ES2019

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -473,15 +473,35 @@ const euroToUsd = 1.1;
 // console.log(ofAccount);
 
 
-/* 
+/*
     161: some and every
 */
 
-console.log(movements);
-const isAnyDeposit = movements.some(mov => mov > 0);
-console.log(isAnyDeposit);
-const largeDeposit = movements.some(mov => mov > 5000);
-console.log(largeDeposit);
+// console.log(movements);
+// const isAnyDeposit = movements.some(mov => mov > 0);
+// console.log(isAnyDeposit);
+// const largeDeposit = movements.some(mov => mov > 5000);
+// console.log(largeDeposit);
 
-console.log(movements.every(mov => mov > 0));
-console.log(account4.movements.every(mov => mov > 0));
+// console.log(movements.every(mov => mov > 0));
+// console.log(account4.movements.every(mov => mov > 0));
+
+/* 
+    162: flat and flatMap: ES2019
+*/
+
+const arr = [[1, 2, 3], [4, 5], 6, 7, 8];
+console.log(arr.flat());
+const arrDeep = [[[1, 2], 3], [4, 5], [[[6, 7]]], [8, 9]];
+console.log(arrDeep.flat(3));
+
+const overAllBalance = accounts
+    .map(acc => acc.movements)
+    .flat()
+    .reduce((acc, cur) => acc + cur, 0);
+console.log(overAllBalance);
+
+const overAllBalanceFlatMap = accounts
+    .flatMap(acc => acc.movements)      // combines map and flat
+    .reduce((acc, cur) => acc + cur, 0);
+console.log(overAllBalanceFlatMap);
