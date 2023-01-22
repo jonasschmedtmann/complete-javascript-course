b0e7488ad44eb3145d339bd3a5c34a70b8fd2882
MChrupczalski <m.chrupczalski@outlook.com>
2023-01-22 22:15:43 +0000

    Update script.js
    Replace the object variable name with `this` keyword within the object method

--- 02-Fundamentals-Part-2/final/script.js
+++ 02-Fundamentals-Part-2/final/script.js
@@ -352,7 +352,7 @@ const jonas = {
   },
 
   getSummary: function () {
-    return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+    return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
   }
 };
 
