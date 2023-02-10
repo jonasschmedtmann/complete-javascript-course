f6c76304890e9f9729081e0404512296feed0f71
Vishu Kumar <1992vishukumar@gmail.com>
2023-02-10 11:03:13 +0530

    188: Implementing smooth scrolling

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -36,94 +36,124 @@ document.addEventListener('keydown', function (e) {
     186: Selecting, Creating and Deleting elements
 */
 
-// Selecting Elements
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
-console.log(document.querySelector(`.header`));
-const allSections = document.querySelectorAll(`.section`);      // Returns a nodelist
-console.log(allSections);
+// // Selecting Elements
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.querySelector(`.header`));
+// const allSections = document.querySelectorAll(`.section`);      // Returns a nodelist
+// console.log(allSections);
 
-console.log(document.getElementById(`section--1`));
-const allButtons = document.getElementsByTagName(`button`);     // returns a HTMLCollection
-console.log(allButtons);
-const allButtonsByClassName = document.getElementsByClassName(`btn`);  // returns a HTMLCollection
-console.log(allButtonsByClassName);
+// console.log(document.getElementById(`section--1`));
+// const allButtons = document.getElementsByTagName(`button`);     // returns a HTMLCollection
+// console.log(allButtons);
+// const allButtonsByClassName = document.getElementsByClassName(`btn`);  // returns a HTMLCollection
+// console.log(allButtonsByClassName);
 
-// Creating and inserting elements
-// .insertAdjacentHTML -- Bankist app
-const header = document.querySelector(`.header`)
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-message.textContent = `We use cookies for improved functionality and analytics`;
-message.innerHTML = `We use cookies for improved functionality and 
-    analytics. <button class="btn btn--close-cookie">Got it!</button>`
-// Prepend/append -- adds the element as a child element
-// Every element is unique, so prepend/append on same element will move the element
-// header.prepend(message);
-header.append(message);
+// // Creating and inserting elements
+// // .insertAdjacentHTML -- Bankist app
+// const header = document.querySelector(`.header`)
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.textContent = `We use cookies for improved functionality and analytics`;
+// message.innerHTML = `We use cookies for improved functionality and 
+//     analytics. <button class="btn btn--close-cookie">Got it!</button>`
+// // Prepend/append -- adds the element as a child element
+// // Every element is unique, so prepend/append on same element will move the element
+// // header.prepend(message);
+// header.append(message);
 
-// // before/after will insert the new element as a sibling element
-// header.before(message);
-// header.after(message);
+// // // before/after will insert the new element as a sibling element
+// // header.before(message);
+// // header.after(message);
 
-// Delete elements
-document.querySelector(`.btn--close-cookie`).addEventListener('click', () => {
-    message.remove();   // very recent method
-    // message.parentElement.removeChild(message);     // Old method of traversing the DOM tree and removing the child element
-})
+// // Delete elements
+// document.querySelector(`.btn--close-cookie`).addEventListener('click', () => {
+//     message.remove();   // very recent method
+// message.parentElement.removeChild(message);     // Old method of traversing the DOM tree and removing the child element
+// })
 
-/* 
+/*
     187: Styles, attributes and classes
 */
 
-// Styles
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
+// // Styles
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
 
-console.log(message.style.color);       // Empty as it is not defined here
-console.log(message.style.backgroundColor);
+// console.log(message.style.color);       // Empty as it is not defined here
+// console.log(message.style.backgroundColor);
 
-// get all the styles
-console.log(getComputedStyle(message));
-console.log(getComputedStyle(message).height);
-// Increase the height of cookie banner
-message.style.height = Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
+// // get all the styles
+// console.log(getComputedStyle(message));
+// console.log(getComputedStyle(message).height);
+// // Increase the height of cookie banner
+// message.style.height = Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
 
-//change the custom css variables -- now defined in root
-document.documentElement.style.setProperty('--color-primary', 'orangered');
+// //change the custom css variables -- now defined in root
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
 
-// Attributes
+// // Attributes
 
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.className);
-logo.alt = 'Beautiful minimalist logo';     // Update the attribute
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.className);
+// logo.alt = 'Beautiful minimalist logo';     // Update the attribute
 
-// Non-standard/custom attributes
-console.log(logo.designer);                     // gives undefined
-console.log(logo.getAttribute('designer'));     // give the custom attribute value
-logo.setAttribute('company', 'bankist');
+// // Non-standard/custom attributes
+// console.log(logo.designer);                     // gives undefined
+// console.log(logo.getAttribute('designer'));     // give the custom attribute value
+// logo.setAttribute('company', 'bankist');
 
-console.log(logo.src);                      // absolute path
-console.log(logo.getAttribute('src'));      // relative path
+// console.log(logo.src);                      // absolute path
+// console.log(logo.getAttribute('src'));      // relative path
 
-const link = document.querySelector('.btn--show-modal');
-console.log(link.href);                     // absolute path
-console.log(link.getAttribute('href'));     // relative path
+// const link = document.querySelector('.btn--show-modal');
+// console.log(link.href);                     // absolute path
+// console.log(link.getAttribute('href'));     // relative path
 
-// Data attributes
-console.log(logo.dataset.versionNumber);
+// // Data attributes
+// console.log(logo.dataset.versionNumber);
 
-// Classes
+// // Classes
 
-logo.classList.add('abc', 'jef');
-logo.classList.remove('abc');
-logo.classList.toggle('jef');
-logo.classList.contains('jef');
+// logo.classList.add('abc', 'jef');
+// logo.classList.remove('abc');
+// logo.classList.toggle('jef');
+// logo.classList.contains('jef');
 
-// Don't do -- removes all other classes and updates to given class
-logo.className = 'vishu-logo';
+// // Don't do -- removes all other classes and updates to given class
+// logo.className = 'vishu-logo';
+
+/* 
+    188: Implementing smooth scrolling
+*/
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+btnScrollTo.addEventListener('click', (e) => {
+    const sec1coords = section1.getBoundingClientRect();
+    console.log(sec1coords);
+    // console.log(e.target.getBoundingClientRect());
+    console.log('Current (X/Y)', window.pageXOffset, window.pageYOffset);
+    // console.log(
+    //     'height/width of view port', document.documentElement.clientHeight, document.documentElement.clientWidth
+    // );
+
+    /* 
+        When we have scrolled on the page, the co-ords won't be exactly relative to the top of the page
+        So, scrolling programmatically doesn't work properly. Hence, find the offset of the btn from top of the page
+        and add it to the co-ords 
+    */
+    // window.scrollTo({
+    //     left: sec1coords.left,
+    //     top: sec1coords.top + window.pageYOffset,
+    //     behavior: 'smooth'
+    // });
+
+    // New browser approach
+    section1.scrollIntoView({behavior: 'smooth'});
+});
 
 
 
