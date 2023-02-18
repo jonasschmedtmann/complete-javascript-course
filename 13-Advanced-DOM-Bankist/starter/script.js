58ad28140ecf18d7063922b390658fb88584a5ea
Vishu Kumar <1992vishukumar@gmail.com>
2023-02-18 17:44:03 +0530

    193: DOM Traversing

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -241,7 +241,44 @@ document.querySelector('.nav__links').addEventListener('click', (e) => {
 // }, {capture: true});
 
 
+/* 
+    193: DOM Traversing
+*/
 
+// traverse down
+const h1 = document.querySelector('h1');
+// All child elements with this class will be selected -- no matter how deep they are
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.children);   // direct children
+console.log(h1.firstElementChild);      // returns the first child element
+console.log(h1.firstChild);     // can give the text inside the element
+
+h1.firstElementChild.style.color = 'pink';
+h1.lastElementChild.style.color = 'orangered';
+
+// traverse up
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+// closest will traverse up, but not to sibling
+h1.closest('.header__title').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';  // closest element is itself here
+// h1.closest('.section').style.background = 'var(--color-tertiary-opacity)';  // error -- as section is a sibling element
+
+// going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+// get all the siblings
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach((el) => {
+    if (el !== h1) {
+        el.style.transform = 'scale(0.5)';
+    }
+})
 
 
 
