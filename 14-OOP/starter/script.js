a26fba322e567c0ac8e22bc7a0fce5f4c8ae296f
Vishu Kumar <1992vishukumar@gmail.com>
2023-03-02 09:41:24 +0530

    208: Constructor functions and the new operator

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+/* 
+    208: Constructor functions and the new operator
+*/
+
+const Person = function (firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+}
+
+const vishu = new Person('Vishu', 1992);
+const vasudha = new Person('Vasudha', 1997);
+console.log(vishu);
+
+/* 
+    1: New {} is created
+    2: function is called, this = {}
+    3: {} is linked to prototype
+    4: function returns the object
+*/
