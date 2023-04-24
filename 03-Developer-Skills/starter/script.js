b89ed86078614faa5c03a25842e7ddf1181f4250
Zien Mohammed Abdelmonaem <78083890+zenab12@users.noreply.github.com>
2023-04-24 23:02:10 +0200

    developer skills solutions

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,22 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+// problem :
+// conver array of tempratures to string in thermometer like this
+// "...17 ْc in 1day... 20 ْc in 2day ... 23 ْc in 3day ..."
+
+// TODO :
+// - transform array to num ْC in {ind} day .
+// - convert array to string with ... at start and end and between them .
+
+let printForecast = (arr) => {
+    let NumToStrArr = arr.map(
+      (item, ind) => item.toString() + ` ْC in ${ind + 1} day`
+    );
+    let convertedToStr = NumToStrArr.join("...");
+    return `... ${convertedToStr} ...`;
+  };
+  
+  let arr = [17, 20, -6, 10];
+  
+  console.log(printForecast(arr));
