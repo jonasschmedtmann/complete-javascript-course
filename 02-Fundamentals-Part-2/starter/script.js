6e370412ab8207b652dd2a2b96e34b4773586c4e
kassrojas <kassandrarojass@gmail.com>
2023-05-11 14:40:12 -0400

    coding challenges

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -110,6 +110,8 @@ Use the problem-solving framework: Understand the problem and break it up into s
 TEST DATA 1: [17, 21, 23]
 TEST DATA 2: [12, 5, -5, 0, 4]
 
+// SOLVED
+
 const printForecast = ([...arr]) => {
   for (let i = 0; i < arr.length; i++) {
     const current = arr[i];
@@ -123,3 +125,44 @@ printForecast(testData1);
 printForecast([12, 5, -5, 0, 4]);
 
 */
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+
+HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+
+GOOD LUCK 😀
+*/
+
+// Solved
+function calcTip([...bills]) {
+  let tip;
+  let total;
+
+  for (let i = 0; i < bills.length; i++) {
+    const bill = Number(bills[i]);
+
+    if (bill >= 50 && bill <= 300) {
+      tip = Number(parseFloat(bill * 0.15).toFixed(2));
+      total = Number(parseFloat(bill + tip).toFixed(2));
+      console.log(`Bill: $${bill} \nTip (15%): $${tip} \nTotal: $${total}`);
+    } else {
+      tip = Number(parseFloat(bill * 0.15).toFixed(2));
+      total = Number(parseFloat(bill + tip).toFixed(2));
+      console.log(`Bill: $${bill} \nTip (20%): $${tip} \nTotal: $${total}`);
+    }
+  };
+};
+
+const billsArr = [125, 555, '44'];
+calcTip(billsArr);
