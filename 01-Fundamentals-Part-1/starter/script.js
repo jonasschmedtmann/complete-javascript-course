adb3f786e9be4fb80c38e621dff24f1cf68b3818
pukulove <stselung@gmail.com>
2023-05-11 11:04:55 -0400

    lecture 11 complete

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,14 @@
 let js = 'amazing'; 
 if (js === 'amazing') alert('JavaScript is FUN!');
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
