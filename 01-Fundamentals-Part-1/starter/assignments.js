c74a506a390ca7c0f8eedfea71e81a1e67ed6f74
pukulove <stselung@gmail.com>
2023-05-12 13:38:38 -0400

    assignment

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,30 @@
+// Values and Variables
+const country = "America";
+const continent = "North America";
+let population = 300000000;
+console.log(country, continent, population);
+
+// Data Types
+const isIsland = false;
+let language;
+console.log(isIsland, population, country, language)
+
+// let, const, var
+language = "English";
+
+// basic operators
+
+let populationHalf = population/2;
+population = population + 1;
+
+let populationFinland = 6000000;
+const isPopulationGreaterThanFinland = population > populationFinland;
+console.log(isPopulationGreaterThanFinland);
+
+let populationAverage = 33000000;
+const isPopulationLessThanAverage = population < populationAverage;
+console.log(isPopulationLessThanAverage);
+
+let description = "Portugal is in Europe, and its 11 million people speak portuguese"
+
+// 
