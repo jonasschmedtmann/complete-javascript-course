4b3ee73fae9c25944023091fd0c3ab138bfc2672
pukulove <stselung@gmail.com>
2023-05-18 15:24:04 -0400

    coding challenge 2

--- 02-Fundamentals-Part-2/starter/coding_challenge.js
+++ 02-Fundamentals-Part-2/starter/coding_challenge.js
@@ -22,3 +22,18 @@ avgKoalas = calcAverage(23, 34, 27);
 avgDolphins = calcAverage(85, 54, 41);
 
 checkWinner(avgDolphins, avgKoalas);
+
+//coding challenge 2
+const calcTip = (bill) => {
+    return bill >=50 && bill <= 300 ? 0.15 * bill : 0.20 * bill;
+}
+
+console.log(calcTip(100));
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills);
+console.log(tips);
+console.log(totals);
