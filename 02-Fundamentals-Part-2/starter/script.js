360cd5290c462abe6409f2c331395166ed1d167c
pukulove <stselung@gmail.com>
2023-05-17 23:40:51 -0400

    fundamentals part2

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,21 @@
+'use strict';
+//strict mode activated as long as it's the first line of code
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) {
+    hasDriversLicense = true;
+}
+
+//Functions
+
+function logger(name) {
+    console.log(`My name is ${name}`);
+}
+
+logger(Betty);
+logger(Robin);
+
+//name is a parameter but Betty is an argument
+
