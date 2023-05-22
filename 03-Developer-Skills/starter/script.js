02716780116fd778aea94430b2ef171e1b6bf77f
pukulove <stselung@gmail.com>
2023-05-21 21:04:19 -0400

    section 3 challenge completed

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,18 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+//challenge 1
+
+const arr = [17, 21, 23];
+
+const printForecast = function(arr) {
+    let days = 0;
+    let string = "";
+    for (let i = 0; i < arr.length; i++) {
+        days++;
+        string += `... ${arr[i]} degC in ${days} days `;
+    }
+    return string;
+};
+
+console.log(printForecast(arr));
