dadccbec8f3f6f406dfa6d5b5490cfc757b50c7b
tursem08 <amigostos@gmail.com>
2023-05-27 18:51:58 -0700

    JS practice 1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,170 @@
+/*
+let js = 'amazing';
+console.log(40+8+23-10);
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Bob";
+console.log(firstName);
+
+let name = 3;
+console.log(name);
+
+let country = "USA", continent = "North America", population = 35000;
+console.log(country + " " + continent + " " + population);
+
+let isIsland = true;
+let language = "";
+
+console.log(isIsland);
+
+const LANGUAGE = "English";
+
+
+let isFun = true;
+console.log(isFun);
+console.log(typeof true)
+console.log(typeof isFun)
+console.log(typeof 23)
+console.log(typeof "Jonas")
+
+isFun = "YES!";
+console.log(typeof isFun);
+
+let year;
+console.log(year);
+
+year = 1991;
+console.log(year);
+
+console.log(typeof null);
+
+
+let age = 30;
+const year = 1988;
+// year = 2004;
+console.log(year);
+
+var job = 'programmer';
+job = "teacher"
+console.log(job);
+
+lastName = 'Turgun';
+console.log(lastName);
+
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+// console.log(ageJonas);
+const ageSarah = now - 2019;
+console.log(ageJonas, ageSarah)
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = 'Jonas';
+const lastName = "Shmidt";
+console.log(firstName + ""+ lastName);
+
+// Assignment operators
+let x = 10+5;
+
+x+= 10;
+x *= 4;
+x++;
+x--;
+console.log(x);
+*/
+
+// Comparison operators
+/*
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+// console.log(25 - 10 -5);
+
+let x, y;
+x = y = 25-10-5;
+console.log(x,y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas,ageSarah, averageAge)
+*/
+
+//let markHeight, markWeight, johnHeight, johnWeight;
+// const markWeight = 78;
+// const markHeight = 1.69;
+// const johnWeight = 92;
+// const johnHeight = 1.95;
+/*
+const markWeight = 95;
+const markHeight = 1.88;
+const johnWeight = 85;
+const johnHeight = 1.76;
+
+const markBMI = markWeight / markHeight ** 2; 
+const johnBMI = johnWeight / johnHeight ** 2;
+
+const markHigherBMI = markBMI > johnBMI;
+console.log(markBMI, johnBMI, markHigherBMI);
+*/
+
+// Template literals
+/*
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job +'!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+
+console.log(`I'm ${firstName} and I like being a ${job} for ${year- birthYear} year old people`);
+
+console.log(`Who is doing whatever?`);
+
+console.log(`String with 
+multiple 
+lines`);
+*/
+
+const age = 15;
+
+if(age >= 18) {
+console.log('Sarah can start driving license 🚗');
+} else {
+      const yearsLeft = 18 - age;
+      console.log(`Sarah is too young. 🥹  Wait another ${yearsLeft} years.`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
