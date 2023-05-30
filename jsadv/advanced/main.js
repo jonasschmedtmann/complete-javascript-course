0d7083ea229c07e0593f772ee35b68e175f39914
tursem08 <amigostos@gmail.com>
2023-05-29 21:06:10 -0700

    Binding

--- jsadv/advanced/main.js
+++ jsadv/advanced/main.js
@@ -37,28 +37,65 @@
 // fn(); // 1
 // fn(); // 2 ==> instead of just executing inner function within the outer function, we're returning it and then invoking it 2 times
 
-// Function Currying - used for logs, info, error
+// *** Function Currying - used for logs, info, error
 // is a process in functional programming in which we transform a function with multiple arguments into a sequence of nesting functions that take one argument at a time
 // function f(a, b, c) is transformed to f(a)(b)(c)
-function sum(a, b, c) {
-  return a + b + c;
-}
-console.log(sum(2, 3, 5));
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
+// console.log(sum(2, 3, 5));
 
-function curry(fn) {
-  return function (a) {
-    return function (b) {
-      return function (c) {
-        return fn(a, b, c);
-      };
-    };
-  };
-}
-const curriedSum = curry(sum);
-console.log(curriedSum(2)(3)(5));
+// function curry(fn) {
+//   return function (a) {
+//     return function (b) {
+//       return function (c) {
+//         return fn(a, b, c);
+//       };
+//     };
+//   };
+// }
+// const curriedSum = curry(sum);
+// console.log(curriedSum(2)(3)(5));
 
-// same as above
-const add2 = curriedSum(2);
-const add3 = add2(3);
-const add4 = add3(5);
-console.log(add4);
+// // same as above
+// const add2 = curriedSum(2);
+// const add3 = add2(3);
+// const add4 = add3(5);
+// console.log(add4);
+
+// ***** this
+// Order of precedence -> New binding, Explictit binding, Implicit binding, Default binding
+//
+
+// // Implicit binding
+// person.sayMyName();
+
+//  ***Explicit binding
+// const person = {
+//   name: "John",
+//   sayMyName: function () {
+//     console.log(`My name is ${this.name}`);
+//   },
+// };
+
+// sayMyName.call(person);
+
+// ***New binding
+// function Person(name) {
+//   this.name = name;
+// }
+// const person1 = new Person("Batman");
+// const person2 = new Person("Superman");
+// console.log(person1.name, person2.name);
+
+// ***Default binding
+
+//globalThis.name = "1234";
+function sayMyName() {
+  console.log(`My name is ${this.name}`);
+}
+
+sayMyName();
+function sayMyName() {
+  console.log(`My name is ${this.name}`);
+}
