a7b23e5bd10b977b07bb3375866154d8ddc32ffe
Mohan-k7 <133000612+Mohan-k7@users.noreply.github.com>
2023-05-31 23:58:32 +0530

    Update script.js

--- 07-Pig-Game/final/script.js
+++ 07-Pig-Game/final/script.js
@@ -4,7 +4,7 @@
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
+const score1El = document.getElementById('#score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
