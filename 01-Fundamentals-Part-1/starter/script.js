e669788d8b5b42432ea515642a492ad714272b3a
James-McTighe <james.mctighe97@gmail.com>
2023-06-06 15:48:39 -0400

    continued lesson

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,37 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("jonas");
+console.log(23);
+
+let firstName = "jimmy";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "jm";
+let $function = 27;
+
+let myFirstJob = "Chemist";
+let myCurrentJob = "Filterer";
+
+console.log(myFirstJob);
+
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+let age = 30;
+age = 31;
+
+const birthyear = 1991;
+// birthyear = 1990;
