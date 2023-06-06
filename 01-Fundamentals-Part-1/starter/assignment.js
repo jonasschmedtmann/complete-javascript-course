0197928643508357f46a1b5fceef520add75c295
Hamdi Abdikarin <hamdiabdikarim14@gmail.com>
2023-06-06 22:36:08 +0300

    Created new files

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,56 @@
+// Values variables // 
+
+var continent;
+var country;
+var population;
+
+continent = "Africa";
+country = "Somalia";
+population = 18;
+
+console.log(continent);
+console.log(country);
+ console.log(population +' ' + "Million");
+
+// Data Type //
+
+var isIsland = false;
+console.log(isIsland);
+console.log(typeof isIsland);
+
+
+
+// let const and var
+
+const language = "somali";
+console.log(language);
+
+// Basic Operator
+population++;
+console.log(population / 2 + ' ' + 'Million');
+console.log(population);
+
+var popFinland = 6;
+
+console.log(popFinland > population);
+console.log(population < 33);
+
+var description;
+
+description = country + ' is in '  + continent + ' and its '  +
+population + ' Million people speak ' + language;
+
+console.log(description);
+
+var desSomalia = `${country} is in ${continent} and its ${population} 
+Million people speak ${language}`
+console.log(desSomalia);
+
+
+
+
+
+
+
+
+
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,24 @@
+// // Basic Operator//
+
+// const now = 2037;
+
+// const ageAli = now - 1995;
+// const ageAsha = now - 1998;
+
+// console.log(ageAli * 2, ageAsha + 5);
+
+// Strings and Template literals //
+
+const firstName = "Hamda";
+const myJob = "Developer";
+const year = 2023;
+const birthYear = 2000;
+
+const hamdi = 'I am' +' '+ firstName + ' a '  + (year - birthYear) + 
+' years old ' + myJob + '!';
+console.log(hamdi);
+
+const hamda = `I'm  ${firstName} a ${year-birthYear} years old ${myJob}!`
+console.log(hamda);
+
+
