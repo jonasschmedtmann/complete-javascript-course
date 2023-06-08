d8d67330802a1524c414166c8f20113c97683c92
James-McTighe <james.mctighe97@gmail.com>
2023-06-08 11:54:44 -0400

    continued lesson

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -30,8 +30,63 @@ console.log(typeof year);
 console.log(typeof null);
 */
 
+/*
 let age = 30;
 age = 31;
 
 const birthyear = 1991;
 // birthyear = 1990;
+
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+// always DECLARE variables
+// lastName = "schmedtmann"; 
+console.log(lastName);
+*/
+
+// *Operators*
+
+// math operators
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2019;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// // 2**3 means 2 to the power of 3 or 2*2*2
+
+// const firstName = "jonas";
+// const lastName = "Schmedtmann";
+// console.log(firstName + " " + lastName);
+
+// // Assignment operators
+// let x = 10 + 5;
+// x += 10; //x = x + 10 = 25
+// x *= 4; //x = x*4= 100
+// x++; //x = x + 1
+// x--; //x = x - 1
+// x--;
+// console.log(x);
+
+// // comparision operators
+// console.log(ageJonas > ageSarah); //>, <, >=, <=
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2019);
+
+// *operator precedence*
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2019;
+console.log(now - 1991 > now - 2019);
+
+console.log(25 - 10 - 5);
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
