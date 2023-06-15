c34beccaf883856250574b76c8315672ee101ae8
tursem08 <amigostos@gmail.com>
2023-06-15 10:17:13 -0700

    strict

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -413,9 +413,11 @@ console.log(n); // 10
 
 // Challenge #4
 //const bill = 250;
-const bill = 430;
+// const bill = 430;
 
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-console.log(
-  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
-);
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// console.log(
+//   `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+// );
+
+// JS Versions - ES5, ES6
---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("I can drive");
