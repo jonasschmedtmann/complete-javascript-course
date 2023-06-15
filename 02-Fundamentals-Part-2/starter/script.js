53ed71d15a16aea50542c3bcfe4d0071806fbe9b
tursem08 <amigostos@gmail.com>
2023-06-15 12:04:46 -0700

    function declaration and expression

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -5,3 +5,39 @@
 
 // if (passTest) hasDriversLicense = true;
 // if (hasDriversLicense) console.log("I can drive");
+
+// function logger() {
+//   console.log("My name is Jonas");
+// }
+
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// //console.log(fruitProcessor(5, 0)); // directly print
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+// const num = Number("23");
+
+// Function declaration
+
+//const age1 = calcAge1(1991);  // hoisting - can declare variable before function
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+console.log(age1);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+console.log(age2);
