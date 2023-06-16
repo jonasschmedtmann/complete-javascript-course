d4e41888aee7a5ffb3172272781d9451e9a8b6cb
tursem08 <amigostos@gmail.com>
2023-06-16 16:31:07 -0700

    Array challenge

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -162,22 +162,40 @@ friends.unshift("John");
 console.log(friends); // add element at the beginning
 
 // Remove elements
-friends.pop(); // from end
-const popped = friends.pop();
-console.log(friends);
-console.log(popped);
+// friends.pop(); // from end
+// const popped = friends.pop();
+// console.log(friends);
+// console.log(popped);
 
-friends.shift(); // from beginning
-console.log(friends);
+// friends.shift(); // from beginning
+// console.log(friends);
 
-console.log(friends.lastIndexOf("Steven"));
-console.log(friends.lastIndexOf("Bob"));
+// console.log(friends.lastIndexOf("Steven"));
+// console.log(friends.lastIndexOf("Bob"));
 
-friends.push(23);
-console.log(friends.includes("Steven")); // true
-console.log(friends.includes("Bob")); //false
-console.log(friends.includes(23)); //true
+// friends.push(23);
+// console.log(friends.includes("Steven")); // true
+// console.log(friends.includes("Bob")); //false
+// console.log(friends.includes(23)); //true
 
-if (friends.includes("Steven")) {
-  console.log("You have a friend Steven");
-}
+// if (friends.includes("Steven")) {
+//   console.log("You have a friend Steven");
+// }
+
+// challenge #2
+// with expression function
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+console.log(calcTip(230));
+
+// with arrow function
+const calcTip2 = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+console.log(calcTip2(230));
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(`Bill is ${bills}, tip is ${tips} and tolal is ${totals}`);
