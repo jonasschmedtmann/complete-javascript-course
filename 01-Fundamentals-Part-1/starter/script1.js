5b7c3805c70cff662025b72cdb57f37bf51a0aea
KartikBhatnagar <kartikbhatnagar111@gmail.com>
2023-06-26 16:47:32 +0530

    part1-assignment-1

---
+++ 01-Fundamentals-Part-1/starter/script1.js
@@ -0,0 +1,5 @@
+let user="kartik"
+alert("this is first alert")
+if (user=="kartik") alert ("this is second alert , \n User name : Kartik Bhatnagar")
+console.log("this is first log")
+console.log(10+20*33/4)
