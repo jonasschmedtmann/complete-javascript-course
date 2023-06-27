13ad0a73e772b054942ecfb5dff3fb2fc1597b45
KartikBhatnagar <kartikbhatnagar111@gmail.com>
2023-06-27 08:52:52 +0530

    coding challenge 1

---
+++ 01-Fundamentals-Part-1/starter/coding_challenge1.js
@@ -0,0 +1,26 @@
+alert("Coding challenge 1")
+let markHeight = 1.69
+let markWeight = 78
+let johnHeight = 1.95
+let johnWeight = 92
+
+let bmiMark; 
+bmiMark = markWeight / markHeight ** 2
+let bmiJohn;
+bmiJohn = johnWeight / johnHeight **2
+
+console.log("Data 1")
+console.log("BMI Of Mark is ",bmiMark)
+console.log("BMI Of John is : ",bmiJohn)
+
+markHeight = 1.88
+markWeight = 95
+johnHeight = 1.76
+johnWeight = 85
+
+bmiMark = markWeight / markHeight ** 2
+bmiJohn = johnWeight / johnHeight **2
+
+console.log("Data 2")
+console.log("BMI Of Mark is ",bmiMark)
+console.log("BMI Of John is : ",bmiJohn)
