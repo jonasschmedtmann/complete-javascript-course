18a7342fa1fffe667fc4da8823765e1305210484
KartikBhatnagar <kartikbhatnagar15@gmail.com>
2023-06-28 18:40:01 +0530

    changes in type conversion

--- 01-Fundamentals-Part-1/starter/type_conversion_coercion.js
+++ 01-Fundamentals-Part-1/starter/type_conversion_coercion.js
@@ -1,14 +1,14 @@
 alert("Type conversion and coercion file started ")
 
 //type conversion
-let age="24"
+let age0="24"
 // string to Number()
-ageFuture = `Current age is ${age} yearas old \n\
-after 10 years the age will be ${Number(age)+10}`
+ageFuture = `Current age is ${age0} yearas old \n\
+after 10 years the age will be ${Number(age0)+10}`
 console.log(ageFuture)
-let age1 =String(24)
-let age2 = 24
-console.log(age1,typeof(age1), age2,typeof(age2))
+let age01 =String(24)
+let age02 = 24
+console.log(age01,typeof(age01), age02,typeof(age02))
 
 //type coercion
 const ageBirth = 1999
