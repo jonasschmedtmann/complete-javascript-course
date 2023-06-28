76072b3be26ab098d309776e84830e82ec5f3eb5
KartikBhatnagar <kartikbhatnagar15@gmail.com>
2023-06-28 18:48:02 +0530

    edit if else if"

--- 01-Fundamentals-Part-1/starter/ifElse_learn.js
+++ 01-Fundamentals-Part-1/starter/ifElse_learn.js
@@ -1,12 +1,14 @@
 alert("iF-ELSE- starting")
 const legalDriveAge = 18;
-let age1 = 14;
+let age1 = Number(prompt("enter age to check driving eligibility"));
 
 const isLegal = age1 >= legalDriveAge;
 
 if (isLegal){
-    console.log("User is eligible for driving ✔🚗 ")
+    alert("User is eligible for driving ✔🚗 ");
 }
-else{
-    console.log(`User is not🚫 yet eligible for driving . \nStill ${legalDriveAge - age1} years are left to start driving`)
+else if (age1<5){
+    alert("You can drive a tricycle 😊")
+}else{    
+    alert(`User is not🚫 yet eligible for driving . \nStill ${legalDriveAge - age1} years are left to start driving`); 
 }
