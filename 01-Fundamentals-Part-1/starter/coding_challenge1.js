63ace18ce92174499ddb80cababf2decfb173ae1
KartikBhatnagar <kartikbhatnagar15@gmail.com>
2023-06-29 12:58:48 +0530

    coding challange 4

--- 01-Fundamentals-Part-1/starter/coding_challenge1.js
+++ 01-Fundamentals-Part-1/starter/coding_challenge1.js
@@ -66,3 +66,25 @@ if(scoreKoalas>scoreDolphins){
 }else{
     console.log("Both win the trophy")
 }
+
+alert("Coding challange 4 started")
+/* CHALLENGE #4
+Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+Your tasks:
+
+Calculate the tip, depending on the bill value. Create a variable called tip for this. It's not allowed to use an if...else statement (if it's easier for you, you can start with an if...else statement, and then try to convert it to a ternary operator).
+
+Print a string to the console containing the bill value, the tip, and the final value (bill + tip).
+
+Example: The bill was 275, the tip was 41.25, and the total value 316.25.*/
+
+const bill = Number(prompt("enter the bill amount to view the Tip and the total bill💵"))
+let tip_per=0;
+
+bill < 300 && bill > 50 ? tip_per = 15 : tip_per = 20
+
+let tip =  (bill * (tip_per/100))
+let str = `The bill💵 was ${bill}, the tip💁‍♀️ was ${tip}, and the total value ${bill+tip}`
+console.log(str)
+alert(str)
