f9c245fc080ec1ca3a5c445593e24f03430e3098
abhinavan <abhinav.15oct1993@gmail.com>
2023-07-08 21:26:36 +0530

    completed till Looping Objects

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -5,28 +5,64 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+  // Es6 enhanced literals for properties in object
+  openingHours,
+  openingHours1: null,
+  // ES6 enhanced literals for method
+  printName() {
+    console.log(this.name);
+  },
+  orderFood(...items) {
+    console.log(
+      `Items ordered are ${this.starterMenu[items[0]]} and ${
+        this.mainMenu[items[1]]
+      } `
+    );
   },
 };
+for (let [i, { open: openingTime, close: closingTime }] of Object.entries(
+  openingHours
+)) {
+  console.log(
+    `On ${i} , we are open at ${openingTime} and close at ${closingTime}`
+  );
+}
+// ES6 2020 has optional chaining which will return undefined as soon as it recieves undefined or null values
+console.log(restaurant?.openingHours1?.fri.open);
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+
+for (let day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day} , we are open from ${open}`);
+}
+// optional chaining on method
+restaurant.orderFood1?.(0, 1) ?? console.log("Method doesn't exists");
+
+// optional chaining on arrays
+
+const userArray = [{ name: 'Abhinav' }];
+console.log(userArray[1]?.name ?? 'Array doesnot exist');
+restaurant.printName();
 // use of spread operator in copying elements of object
 const restaurantFullInfo = {
   foundingYear: '2023',
@@ -65,3 +101,9 @@ console.log(student1, student2);
 
 student2.name &&= 'Arunit'; // AND assignment operator
 console.log(student2);
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+for (let [i, item] of menu.entries()) console.log(`${i + 1} -> ${item}`);
+
+console.log(restaurant);
+console.log(openingHours);
