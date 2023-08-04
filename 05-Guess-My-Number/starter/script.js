ad1011c9e672c060892daa0b2712e277fb5d1054
Stephen <popoolastephen19@gmail.com>
2023-08-04 21:36:56 +0100

    next commit

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,14 +1,14 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = 'Correct Number!';
-// console.log(document.querySelector('.message').textContent);
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+console.log(document.querySelector('.message').textContent);
 
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
 
-//document.querySelector('.guess').value = 34;
-//console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 34;
+console.log(document.querySelector('.guess').value);
 
 //Handling click events
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
