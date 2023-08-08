945b16369492cf04a727ff81b157b018b4bd58ee
James-McTighe <james.mctighe97@gmail.com>
2023-08-08 16:56:20 -0400

    setup vscode with extensions

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,11 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = '23';
+if (x === 23) {
+  console.log(23);
+}
+
+const calcAge = birthYear => 2037 - birthYear;
+
+console.log();
