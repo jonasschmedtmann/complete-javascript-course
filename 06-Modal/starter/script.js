1a899a679c3f60e57a6867d4e8253ce6cd0990dc
Larissa <larissarf.dev@gmail.com>
2023-08-09 15:56:48 -0300

    Revert "modal-window"
    This reverts commit b81dfd88eaa948c9d2113486afb007a7cc8f1f63.

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,12 +1,1 @@
 'use strict';
-
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnOpenModal);
-
-for (let i = 0; i < btnOpenModal.length; i++)
-  console.log(btnOpenModal[i].textContent);
-
-// just like and if-else statement, when there's only one line of code that I want to execute, I actually don't have to use curly braces (a block)
