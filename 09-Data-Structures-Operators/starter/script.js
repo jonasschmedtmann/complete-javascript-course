fbdb169c6f269d6b61ffc1d47301548250d18825
pukulove <stselung@gmail.com>
2023-08-14 16:16:04 -0400

    finished exercise 9

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -27,3 +27,68 @@ const restaurant = {
     },
   },
 };
+
+//Coding challenge 1
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [ players1, players2] = game.players;
+
+console.log(players1, players2);
+
+const [gk, ...fieldPlayers] = players1;
+
+const allPlayers = [...players1, ...players2];
+
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
+const { team1, x: draw, team2} = game.odds;
+console.log(team1, draw, team2)
+
+function printGoals(...number) {
+  const scores = number.length;
+  for (let i = 0; i < scores; i++) {
+    console.log(`${number[i]}, ${scores} goals were scored`);
+  }
+}
+printGoals('Davies', 'Muller', 'Lewandowski','Kimmich');
+
+(team1 < team2) && console.log(team1);
