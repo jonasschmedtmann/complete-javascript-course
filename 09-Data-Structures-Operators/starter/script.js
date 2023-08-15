7945fe8d290b80ca3744c596e5d759952139ed46
pukulove <stselung@gmail.com>
2023-08-15 17:33:06 -0400

    completed challenge 3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -111,4 +111,31 @@ for (const [team, odd] of Object.entries(game.odds)) {
   console.log(`Odd of ${teamStr}: ${odd}`);
 }
 
+//coding challenge 3
 
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '� Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '� Substitution'],
+  [64, '� Yellow card'],
+  [69, '� Red card'],
+  [70, '� Substitution'],
+  [72, '� Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '� Yellow card'],
+]);
+console.log(gameEvents.values());
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+gameEvents.delete(64);
+console.log(gameEvents)
+
+console.log(`An event happened, on average, every ${90 / gameEvents.size} minutes`)
+
+for (const [time, event] of gameEvents) {
+  const statement = time <= 45 ? '[FIRST HALF]' : '[SECOND HALF]';
+  console.log(`${statement} ${time}: ${event} `);
+}
