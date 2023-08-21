84653878ea65355a2c4677f827d1dc284f2dc253
pukulove <stselung@gmail.com>
2023-08-21 19:59:56 -0400

    passing-arguments

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,49 @@
 'use strict';
+
+//default parameters
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 199 * numPassengers) { 
+    const booking = {
+        flightNum,
+        numPassengers,
+        price,
+    }; 
+    console.log(booking); 
+    bookings.push(booking); 
+} 
+
+createBooking('LH123');
+createBooking('LH123', 2);
+createBooking('LH123', 2, 3);
+createBooking('LH123', 3);
+
+//passing arguments (values vs references)
+const flight = 'LH234';
+
+const jonas = {
+    name: 'Jonas Ro',
+    passport: 1213423432,
+}
+
+const checkIn = function(flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Mr.' + passenger.name;
+    if (passenger.passport === 1213423432) {
+        console.log('Checked in');
+    } else {
+        console.log('Wrong passport!');
+    }
+}
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+const flightNum = flight;
+const passenger = jonas;
+
+console.log(flightNum);
+console.log(passenger);
+
+//
