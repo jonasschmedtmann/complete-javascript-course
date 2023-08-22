2f80cb47715d4e90e3ee8913099bf472e9c87688
pukulove <stselung@gmail.com>
2023-08-22 10:50:01 -0400

    coding challenge 1

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -69,3 +69,38 @@ const tax = rate => value => value + value * rate;
 
 const incomeTax = tax(0.23);
 console.log(incomeTax(100));
+
+//coding challenge 1
+
+const poll = {
+    question: "What is your favourite programming language?",
+    options: ["0: JavaScript", "1: Python", "2: Rust", "3:C++"],
+    // This generates [0, 0, 0, 0]. More in the next section!
+    answers: new Array(4).fill(0),
+    registerNewAnswer() {
+        const answer = Number(prompt(`${this.question} 
+        ${this.options[0]}
+        ${this.options[1]}
+        ${this.options[2]}
+        ${this.options[3]}
+        (Write option number)`));
+        typeof answer === "number" && answer >=0 && answer < 4
+        && this.answers[answer]++;  
+        this.displayResults();
+        this.displayResults('string');
+    }, 
+    displayResults (type = 'array') {
+        if(type === "string") {
+            console.log(`Poll results are ${this.answers[0]},  ${this.answers[1]},  ${this.answers[2]},  ${this.answers[3]}`);
+        } else {
+            console.log(this.answers);
+        }
+    }
+};
+
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
+const data1 = [5, 2, 3];
+const data2 = [1, 5, 3, 9, 6, 1];
+poll.displayResults.call({answers: data1});
+poll.displayResults.call({answers: data2});
+
