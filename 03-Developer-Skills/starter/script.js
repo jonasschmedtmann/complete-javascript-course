c9b08ec61435417a1adff41409fd448601bad8fe
James-McTighe <james.mctighe97@gmail.com>
2023-08-28 20:26:09 -0400

    Update script.js

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,11 +1,30 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = '23';
-if (x === 23) {
-  console.log(23);
-}
+// const x = '23';
+// if (x === 23) {
+//   console.log(23);
+// }
 
-const calcAge = birthYear => 2037 - birthYear;
+// const calcAge = birthYear => 2037 - birthYear;
 
-console.log();
+// console.log();
+
+// Actually learning to code //
+// Example John
+// John didn't have a clear goal -> set a REAL goal, think of your career change and a big project you want to be able to build by the end of this course
+// He just would copy code from tutorials without caring what any of it meant -> always learn what your code means, don't move on if you understand.  Always TYPE your code, don't copy and paste.  People learn by using, this is especially true for me.  It's ok to challenge yourself
+// He got easily frustrated because his code wasn't immediately clean and efficient
+// lost motivation because he didn't know everything
+// learning in isolation, he never shared his work
+// finished a couple of courses and thought he could just start applying to jobs, but he couldn't even build an app on his own
+// it's not to late for john
+
+/* YOU WILL NEVER KNOW EVERYTHING ABOUT WEB DEVELOPMENT
+
+THAT'S OK
+
+DON'T LOSE MOTIVATION AND DON'T COMPARE YOURSELF.
+Lots of these guys have had years of consitent practice.  You can get there with time.
+
+*/
