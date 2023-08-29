5738be64a67cb0aa704d3f62eb162a6b369d271a
pukulove <stselung@gmail.com>
2023-08-29 17:09:36 -0400

    prototypal inheritance

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear
+}
+//prototypal inheritance
+const jon = new Person('Joe', 1111);
+console.log(jon)
+Person.prototype.calcAge = function() {
+    console.log("hi" + this.firstName)
+}
+console.log(Person.prototype);
+console.log(jon.calcAge());
+
