b7ffcd5eb8b5975e2363a88a3daa1ee589e4190d
Ryan Park <ryan.c.park44@gmail.com>
2023-09-03 18:00:58 -0400

    completed sections 1-17

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,22 @@
+// Values and Variables
+const country = 'United States'
+const continent = 'North America'
+let population = 300000000
+
+console.log(country, continent, population)
+
+// Data Types
+let isIsland = false
+let language
+
+console.log("isIsland is a", typeof isIsland)
+console.log("language is a", typeof language)
+console.log("country is a", typeof country)
+
+// let, const, var
+language = "english"
+
+// Strings and Template Literals
+const myTemplate = `The ${country} is in ${continent}, and has a population of ${population}`
+
+console.log(myTemplate)
