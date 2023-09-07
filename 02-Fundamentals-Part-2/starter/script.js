b7f111108bfa5baa37d611bec021a7946d630a2e
Ryan Park <ryan.c.park44@gmail.com>
2023-09-07 11:57:28 -0400

    completed functions and objects

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,2 +1,76 @@
 'use strict';
 
+// Function declaration
+function calcAge1(birthYear) {
+    return 2023 - birthYear;
+}
+
+// Fuction expression
+const calcAge2 = function (birthYear) {
+    return 2023 - birthYear;
+}
+
+// Arrow function
+const calcAge3 = birthYear => 2023 - birthYear;
+
+const myBirthYear = 1994;
+console.log(
+    calcAge1(myBirthYear),
+    calcAge2(myBirthYear),
+    calcAge3(myBirthYear)
+);
+
+// Arrays
+const friends = ['Brandon', 'Jon', 'Reid']
+const friends2 = new Array('Brandon', 'Jon', 'Reid')
+
+// Add elements
+friends.push('Brad')
+friends2.unshift('Brad')
+
+// Remove Elements
+friends.pop()
+friends2.shift()
+
+console.log(friends.indexOf('Jon'))
+
+console.log(friends.includes('Jon'))
+
+/***************************************
+ * Objects
+ ***************************************/
+
+const ryan = {
+    firstName: 'Ryan',
+    lastName: 'Park',
+    birthYear: 1995,
+    job: 'cybersecurity engineer',
+
+    calcAge: function (birthYear) {
+        return 2023 - birthYear
+    }
+};
+
+console.log(ryan.job)
+console.log(ryan['job'])
+
+console.log(ryan.calcAge(ryan.birthYear))
+console.log(ryan['calcAge'](ryan.birthYear))
+
+const ryan2 = {
+    firstName: 'Ryan',
+    lastName: 'Park',
+    birthYear: 1995,
+    job: 'cybersecurity engineer',
+
+    calcAge: function () {
+        this.age = 2023 - this.birthYear;
+        return this.age;
+    }
+};
+
+console.log(ryan2.calcAge())
+console.log(ryan2.age)
+
+console.log(`${ryan2.firstName} is a ${ryan2.age}-year old ${ryan2.job}, and \
+he ${ryan2.age >= 16 ? 'has' : 'does not have'} a driver's license.`)
