fad23c629486ed2fdf25d631b037e86f518cdcf9
demitryedwards <dxyz773@gmail.com>
2023-11-01 14:24:12 -0400

    Working 01-fundamentals part 1

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,13 @@
+// 1. Values and Variables
+const country = "USA";
+const continent = "North America";
+let population = 334233854;
+
+// console.log(country, continent, population);
+
+// 2. Data Types
+const isIsland = false;
+let language;
+
+// 3. let, const, var
+language = "English";
