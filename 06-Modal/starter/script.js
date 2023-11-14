c7b83b589dd83683b8ba86be1d2dc327fbae685c
jayaprakash <jayaprakash.s@adappt.ai>
2023-11-14 17:35:07 +0530

    modalWindow task

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+const showModalBtn = document.querySelectorAll('.show-modal');
+
+// console.log(modal, 'modal');
+// console.log(overlay, 'overlay');
+// console.log(showModalBtn, 'showModal');
+
+const addModal =() => {
+        // console.log(modal.classList, 'classList');
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+}
+
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for(let i = 0; i < showModalBtn.length; i++) {
+    // console.log(showModalBtn[i].textContent);
+    showModalBtn[i].addEventListener('click', addModal)
+    }
+
+closeModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown' , function (e) {
+    // document.querySelector(e.key);
+    if(e.key === 'Escape') {
+        closeModal();
+    }
+})
