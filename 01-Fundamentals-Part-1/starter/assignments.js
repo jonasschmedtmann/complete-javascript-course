0eb99dcc09b1c969586514e90e87f5519603f059
Michal-Ploszczyca <michal.ploszczyca98@gmail.com>
2023-11-18 16:20:18 +0100

    lesson about variables

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,5 @@
+let country = 'Poland'
+let continent = "Europe"
+let population = 39000000
+
+console.log(country, continent, population)
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,12 @@
+let js = 'AMAZING'
+console.log(1 + 1 + " kebaby NA cienkim");
+
+
+
+
+let first_name = "Matilda";
+
+console.log(first_name);
+
+let myFirstJob = "Programmer";
+console.log(myFirstJob)
