d5bf12dac8ca9a6395048b72bc3e9e7c3b370a09
Ryan Park <ryan.c.park44@gmail.com>
2023-12-22 12:37:03 -0500

    Completed Section 11 and 13

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -2,6 +2,273 @@
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
+// LECTURES
+//
+//const currencies = new Map([
+//  ['USD', 'United States dollar'],
+//  ['EUR', 'Euro'],
+//  ['GBP', 'Pound sterling'],
+//]);
+//
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+//
+/////////////////////////////////////////////////
+//console.log(`--- For Loop ---`);
+//// for (const movement of movements) {
+//for (const [i, movement] of movements.entries()) {
+//  if (movement > 0) {
+//    console.log(`Transaction ${i + 1}: You deposited ${movement}`);
+//  } else {
+//    console.log(`Transaction ${i + 1}: You withdrew ${Math.abs(movement)}`);
+//  }
+//}
+//
+//console.log(`--- forEach Method ---`);
+//movements.forEach(function (movement, index, array) {
+//  if (movement > 0) {
+//    console.log(`Transaction ${index + 1}: You deposited ${movement}`);
+//  } else {
+//    console.log(`Transaction ${index + 1}: You withdrew ${Math.abs(movement)}`);
+//  }
+//});
+//
+///* forEach with Maps and Sets */
+//currencies.forEach(function (value, key, map) {
+//  console.log(`Value: ${value}, Key: ${key}`);
+//});
+//
+//const currenciesUnique = new Set(['USD', 'GBP', 'EUR']);
+///* set have no keys, therefore the first two arguments are both value for sets*/
+//currenciesUnique.forEach(function (value, key, set) {
+//  console.log(`Value: ${value}, Key: ${key}`);
+//});
+
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+const diceRolls = Array.from({ length: 100 }, _ =>
+  Math.floor(Math.random() * 100)
+);
+console.log(diceRolls);
+
+const movementsUI = Array.from(document.querySelectorAll('.movements__value'));
+console.log(movementsUI);
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners 
+about their dog's age, and stored the data into an array (one array for each). 
+For now, they are just interested in knowing whether a dog is an adult or a 
+puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's
+ less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia'
+ and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually 
+have cats, not dogs! So create a shallow copy of Julia's array, and remove the 
+cat ages from that copied array (because it's a bad practice to mutate function 
+parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number
+ 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a 
+puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+//const checkAges = function (dogsJulia, dogsKate) {
+//  const correctedDogArr = [dogsJulia.slice(2, -2)];
+//  const combinedDogArr = [...correctedDogArr, ...dogsKate];
+//  combinedDogArr.forEach(function (age, i) {
+//    console.log(
+//      `${
+//        age >= 3
+//          ? `Dog number ${i + 1} is an adult, and is ${age} years old`
+//          : `Dog number ${i + 1} is still a puppy 🐶`
+//      }`
+//    );
+//  });
+//};
+//
+//checkAges([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+//checkAges([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to 
+convert dog ages to human ages and calculate the average age of the dogs in 
+their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages 
+('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog 
+is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge 
+= 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as 
+keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know 
+from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+//
+//const calcAverageHumanAge = function (ages) {
+//  return ages
+//    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+//    .filter(age => age >= 18)
+//    .reduce((acu, age, i, arr) => acu + age / arr.length, 0);
+//};
+//console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+//console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+//
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this
+ time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+//const calcAverageHumanAge = ages =>
+//  ages
+//    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+//    .filter(age => age >= 18)
+//    .reduce((acu, age, i, arr) => acu + age / arr.length, 0);
+//
+//console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+//console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Julia and Kate are still studying dogs, and this time they are studying if dogs 
+are eating too much or too little.
+Eating too much means the dog's current food portion is larger than the 
+recommended portion, and eating too little is the opposite.
+Eating an okay amount means the dog's current food portion is within a range 
+10% above and 10% below the recommended portion (see hint).
+
+1. Loop over the array containing dog objects, and for each dog, calculate the 
+recommended food portion and add it to the object as a new property. Do NOT 
+create a new array, simply loop over the array. Forumla: 
+recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the 
+weight needs to be in kg)
+2. Find Sarah's dog and log to the console whether it's eating too much or too 
+little. HINT: Some dogs have multiple owners, so you first need to find Sarah 
+in the owners array, and so this one is a bit tricky (on purpose) 🤓
+3. Create an array containing all owners of dogs who eat too much 
+('ownersEatTooMuch') and an array with all owners of dogs who eat too little 
+('ownersEatTooLittle').
+4. Log a string to the console for each array created in 3., like this: 
+"Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and 
+Michael's dogs eat too little!"
+5. Log to the console whether there is any dog eating EXACTLY the amount of 
+food that is recommended (just true or false)
+6. Log to the console whether there is any dog eating an OKAY amount of food 
+(just true or false)
+7. Create an array containing the dogs that are eating an OKAY amount of food 
+(try to reuse the condition used in 6.)
+8. Create a shallow copy of the dogs array and sort it by recommended food 
+portion in an ascending order (keep in mind that the portions are inside the 
+array's objects)
+
+HINT 1: Use many different tools to solve these challenges, you can use the 
+summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: 
+current > (recommended * 0.90) && current < (recommended * 1.10). Basically, 
+the current portion should be between 90% and 110% of the recommended portion.
+
+TEST DATA:
+*/
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+// GOOD LUCK 😀
+/* Q1 */
+dogs.forEach(obj => {
+  obj.recommendedFood = Math.round(obj.weight ** 0.75 * 28);
+  console.log(obj);
+});
+/* Q2 */
+const sarahsDog = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(
+  `Sarah's dog is eating too ${
+    sarahsDog.curFood > sarahsDog.recommendedFood ? 'much' : 'little'
+  }`
+);
+/* Q3 */
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.curFood > dog.recommendedFood)
+  .flatMap(dog => dog.owners);
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recommendedFood)
+  .flatMap(dog => dog.owners);
+
+console.log(ownersEatTooMuch);
+console.log(ownersEatTooLittle);
+/* Q4 */
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
+/* Q5 */
+console.log(
+  `EXACTLY: ${dogs.some(obj => {
+    obj.curFood === obj.recommendedFood;
+  })}`
+);
+/* Q6 */
+console.log(
+  `OKAY: ${dogs.some(
+    obj =>
+      obj.curFood < obj.recommendedFood * 1.1 &&
+      obj.curFood > obj.recommendedFood * 0.9
+  )}`
+);
+/* Q7 */
+const okayFoodDogs = dogs.filter(
+  obj =>
+    obj.curFood < obj.recommendedFood * 1.1 &&
+    obj.curFood > obj.recommendedFood * 0.9
+);
+
+console.log(okayFoodDogs);
+
+/* Q8 */
+const sortedDogsbyRecFood = dogs
+  .slice()
+  .sort((dogA, dogB) => dogA.recommendedFood - dogB.recommendedFood);
+
+console.log(sortedDogsbyRecFood);
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
 // BANKIST APP
 
 // Data
@@ -61,16 +328,202 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+const displayMovements = function (movements, sort = false) {
+  containerMovements.innerHTML = '';
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  movs.forEach(function (movement, i) {
+    const type = movement < 0 ? 'withdrawal' : 'deposit';
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__date"></div>
+      <div class="movements__value">${movement}€</div>
+    </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+const CalcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acu, mov) => acu + mov, 0);
+  labelBalance.textContent = `${acc.balance}€`;
+};
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const calcDisplaySummary = function (account) {
+  const incomes = account.movements
+    .filter(mov => mov > 0)
+    .reduce((acu, mov) => acu + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
 
-/////////////////////////////////////////////////
+  labelSumOut.textContent = `${Math.abs(
+    account.movements.filter(mov => mov < 0).reduce((acu, mov) => acu + mov, 0)
+  )}€`;
+  labelSumInterest.textContent = `${account.movements
+    .filter(mov => mov > 0)
+    .map(deposits => (deposits * account.interestRate) / 100)
+    .filter(interest => interest > 1)
+    .reduce((acu, interest) => acu + interest)}€`;
+};
+
+const euroToUsd = 1.1;
+
+const movementsUSD = account1.movements.map(function (mov) {
+  return Math.round((mov * euroToUsd * 100) / 100);
+});
+
+const CreateUsernames = function (accounts) {
+  accounts.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+CreateUsernames(accounts);
+//console.log(accounts);
+//
+//const withdrawals = movements.filter(mov => mov < 0);
+//console.log(withdrawals);
+//
+//const balance = movements.reduce((acu, mov) => acu + mov, 0);
+//console.log(balance);
+//
+//const totalDeposits = movements
+//  .filter(mov => mov > 0)
+//  .map(mov => mov * euroToUsd)
+//  .reduce((acu, mov) => acu + mov, 0);
+//console.log(parseInt(totalDeposits));
+
+const UpdateUi = function (acc) {
+  // Display movements
+  displayMovements(acc.movements);
+  // Display balance
+  CalcDisplayBalance(acc);
+  // Display summary
+  calcDisplaySummary(acc);
+};
+
+const clearUi = function () {
+  containerApp.style.opacity = 0;
+  labelWelcome.textContent = 'Login to get started';
+};
+
+// Event Handlers
+let currentAccount;
+btnLogin.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Clear inpurt fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+    UpdateUi(currentAccount);
+  }
+});
+
+// Transfer Event Handler
+btnTransfer.addEventListener('click', function (event) {
+  event.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const recieverAccount = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+  inputTransferAmount.value = inputTransferTo.value = '';
+  if (
+    amount > 0 &&
+    recieverAccount &&
+    currentAccount.balance >= amount &&
+    recieverAccount?.username !== currentAccount.username
+  ) {
+    console.log('Valid Transfer');
+    currentAccount.movements.push(-amount);
+    recieverAccount.movements.push(amount);
+    UpdateUi(currentAccount);
+  }
+});
+
+btnLoan.addEventListener('click', function (event) {
+  event.preventDefault();
+  const loanAmount = Number(inputLoanAmount.value);
+
+  if (
+    loanAmount > 0 &&
+    currentAccount.movements.some(mov => mov >= loanAmount * 0.1)
+  ) {
+    currentAccount.movements.push(loanAmount);
+    UpdateUi(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
+btnClose.addEventListener('click', function (event) {
+  event.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    accounts.splice(
+      accounts.findIndex(acc => acc.username === inputCloseUsername.value),
+      1
+    );
+    clearUi();
+  } else {
+    alert('Invalid User: Accounts can only be closed by the owner.');
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+  console.log(accounts);
+});
+
+labelBalance.addEventListener('click', event => {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+
+  console.log(movementsUI);
+});
+
+let sortState = false;
+btnSort.addEventListener('click', function (event) {
+  event.preventDefault();
+  sortState = !sortState;
+  displayMovements(currentAccount.movements, sortState);
+});
+
+console.log(movements);
+console.log(movements.includes(-130));
+/* SOME Method */
+const anyDeposits = movements.some(mov => mov > 1500);
+console.log(anyDeposits);
+/* EVERY Method */
+console.log(movements.every(mov => mov > 0));
+
+const arr = [[1, 2, 3], [4, 29, 28], 4, 5];
+console.log(arr.flat());
+
+const totalBankBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acu, mov) => acu + mov);
+
+console.log(totalBankBalance);
+
+const totalBankBalanceToo = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acu, mov) => acu + mov, 0);
+
+console.log(totalBankBalanceToo);
+
+console.log(movements.sort((a, b) => a - b));
+console.log(movements.sort((a, b) => b - a));
--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,5 +1,117 @@
 'use strict';
 
+console.log(document.documentElement);
+console.log(document.head);
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+const cookieMessage = document.createElement('div');
+const header = document.querySelector('.header');
+cookieMessage.classList.add('cookie-message');
+cookieMessage.innerHTML = `We use cookies for improved functionality and analytics \
+<button class="btn btn--close-cookie">Got it!</button>`;
+header.append(cookieMessage);
+//header.append(cookieMessage.cloneNode(true));
+
+// Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', () => cookieMessage.remove());
+
+// Styles, Attributes, Classes
+cookieMessage.style.width = '120%';
+cookieMessage.style.backgroundColor = '#37383d';
+console.log(getComputedStyle(cookieMessage).color);
+cookieMessage.style.height =
+  Number.parseFloat(getComputedStyle(cookieMessage).height, 10) + 40 + 'px';
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.src);
+console.log(logo.alt);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Backist');
+console.log(logo.getAttribute('src'));
+
+// Classes
+logo.classList.add('name', 'j');
+logo.classList.remove('name', 'j');
+logo.classList.toggle('name');
+console.log(logo.classList.contains('name'));
+
+// Types of events and event handlers
+//const h1 = document.querySelector('h1');
+//
+//const mouseDetected = function (e) {
+//  console.log('addEventListener: mouse detected in h1');
+//  h1.removeEventListener('mouseenter', mouseDetected);
+//};
+//
+//h1.addEventListener('mouseenter', mouseDetected);
+//
+//h1.onmouseenter = () => console.log('onmouseenter: mouse detected in h1');
+
+// Event Propagation
+const randomeInt = function (min, max) {
+  return Math.floor(Math.random() * (max - min) + 1) + min;
+};
+
+const randomColor = () =>
+  `rgb(${randomeInt(0, 255)}, ${randomeInt(0, 255)}, ${randomeInt(0, 255)})`;
+console.log(randomColor());
+
+//navLink.forEach(link =>
+//  link.addEventListener('click', function (e) {
+//    e.preventDefault();
+//    link.style.backgroundColor = randomColor();
+//  })
+//);
+//navLinksContainer.addEventListener(
+//  'click',
+//  () => (navLinksContainer.style.backgroundColor = randomColor())
+//);
+//nav.addEventListener(
+//  'click',
+//  () => (nav.style.backgroundColor = randomColor())
+//);
+
+/* DOM Traversal */
+//const h1 = document.querySelector('h1');
+//
+//// Downwards
+//console.log(h1.querySelectorAll('.highlight'));
+//console.log(h1.childNodes);
+//console.log(h1.children);
+//h1.firstElementChild.style.color = 'white';
+//h1.lastElementChild.style.color = 'orangered';
+//
+//// Upwards
+//console.log('parentNode:', h1.parentNode);
+//console.log('parentElement:', h1.parentElement);
+//console.log('closest: ', h1.closest('.header'));
+//
+//// Sideways
+//console.log('previousElementSibling:', h1.previousElementSibling);
+//console.log('nextElementSibling:', h1.nextElementSibling);
+//
+//console.log('previousSibling:', h1.previousSibling);
+//console.log('nextSibling:', h1.nextSibling);
+//
+//console.log('parentElement.children:', h1.parentElement.children);
+//Array.from(h1.parentElement.children).forEach(child => {
+//  if (child !== h1) child.style.transform = 'scale(0.5)';
+//});
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
 ///////////////////////////////////////
 // Modal window
 
@@ -8,7 +120,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +131,10 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+//for (let i = 0; i < btnsOpenModal.length; i++)
+//  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -27,5 +142,233 @@ overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
+  }
+});
+
+///////////////////////////////////////
+// Smooth Scrolling
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  e.preventDefault();
+  // const s1Coords = section1.getBoundingClientRect();
+
+  // console.log(s1Coords);
+  // console.log('Current scroll X Y', window.scrollX, window.scrollY);
+  // console.log(
+  //   'height/width viewport',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
+
+  // scrolling
+  section1.scrollIntoView({ behavior: 'smooth' });
+  //  window.scrollTo({
+  //    left: s1Coords.left + window.scrollX,
+  //    top: s1Coords.top + window.scrollY,
+  //    behavior: 'smooth',
+  //  });
+});
+
+///////////////////////////////////////
+// Nav Link Scrolling
+
+const nav = document.querySelector('.nav');
+const navLinksContainer = document.querySelector('.nav__links');
+const navLinks = document.querySelectorAll('.nav__link');
+
+//navLinks.forEach(link => {
+//  link.addEventListener('click', function (e) {
+//    e.preventDefault();
+//    const id = this.getAttribute('href');
+//    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//  });
+//});
+
+navLinksContainer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const id = e.target?.getAttribute('href');
+  if (id) document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+});
+///////////////////////////////////////
+// Nav link focus
+const handleHover = function (event) {
+  if (event.target.classList.contains('nav__link')) {
+    const link = event.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+///////////////////////////////////////
+// Operations tabed compnent
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const clickedBtn = e.target.closest('.operations__tab');
+  // Guard Clause
+  if (!clickedBtn) return;
+
+  // Activate tab
+  console.log(this.children);
+  [...this.children].forEach(tab =>
+    tab.classList.remove('operations__tab--active')
+  );
+  clickedBtn.classList.add('operations__tab--active');
+
+  // Display content
+  const tabContent = document.querySelector(
+    `.operations__content--${clickedBtn.dataset.tab}`
+  );
+  document
+    .querySelector('.operations__content--active')
+    .classList.remove('operations__content--active');
+  tabContent.classList.add('operations__content--active');
+});
+
+///////////////////////////////////////
+// ticky nav bar
+
+//window.addEventListener('scroll', function (e) {
+//  console.log(this.window.scrollY);
+//  const initialCoords = section1.getBoundingClientRect();
+//  if (this.window.scrollY > initialCoords.top) nav.classList.add('sticky');
+//});
+
+//const obsCallback = function (entries, observer) {
+//  entries.forEach(entry => {
+//    console.log(entry);
+//  });
+//};
+//const observerOptions = {
+//  root: null,
+//  threshold: [0, 0.2],
+//};
+//const observer = new IntersectionObserver(obsCallback, observerOptions);
+//observer.observe(section1);
+
+const navheight = nav.getBoundingClientRect().height;
+
+const headerObserverOptions = {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navheight}px`,
+};
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+const headerObserver = new IntersectionObserver(
+  stickyNav,
+  headerObserverOptions
+);
+
+headerObserver.observe(header);
+
+///////////////////////////////////////
+// Reveal sections on scroll
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (entry.isIntersecting) {
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  }
+};
+
+const sectionObserverOptions = {
+  root: null,
+  threshold: 0.15,
+  rootMargin: '',
+};
+
+const sectionObserver = new IntersectionObserver(
+  revealSection,
+  sectionObserverOptions
+);
+
+allSections.forEach(section => {
+  sectionObserver.observe(section);
+  //section.classList.add('section--hidden');
+});
+
+///////////////////////////////////////
+// Lazy loading images
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (entry.isIntersecting) {
+    entry.target.src = entry.target.dataset.src;
+    entry.target.addEventListener('load', () => {
+      entry.target.classList.remove('lazy-img');
+    });
+    observer.unobserve(entry.target);
+  }
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0.4,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => {
+  imgObserver.observe(img);
+});
+
+///////////////////////////////////////
+// Slider Component
+
+const slides = document.querySelectorAll('.slide');
+const btnSliderLeft = document.querySelector('.slider__btn--left');
+const btnSliderRight = document.querySelector('.slider__btn--right');
+let currentSlide = 0;
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${(i - slide) * 100}%)`)
+  );
+};
+
+// init slides
+goToSlide(0);
+
+btnSliderRight.addEventListener('click', function (e) {
+  e.preventDefault();
+  currentSlide++;
+  if (currentSlide < slides.length) {
+    goToSlide(currentSlide);
+  } else {
+    currentSlide = 0;
+    goToSlide(currentSlide);
+  }
+});
+
+btnSliderLeft.addEventListener('click', function (e) {
+  e.preventDefault();
+  currentSlide--;
+  if (currentSlide >= 0) {
+    goToSlide(currentSlide);
+  } else {
+    currentSlide = slides.length - 1;
+    goToSlide(currentSlide);
   }
 });
