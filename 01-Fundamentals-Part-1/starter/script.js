0ca1508fee4f71732d866c3194a125edc4e365ee
burhan1997 <burhanelaldi97@gmail.com>
2024-02-23 21:13:53 +0100

    let, const and var

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -25,7 +25,7 @@ let job1 = 'programmer';
 let job2 = 'teacher';
 
 console.log(myFirstJob);
-*/
+
 
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
@@ -46,3 +46,19 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+
+*/
+
+let age= 30;
+age = 31;
+
+const birthYear = 1991;
+//birthYear = 1990;
+
+const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Schmedtmann';
+console.log(lastName);
