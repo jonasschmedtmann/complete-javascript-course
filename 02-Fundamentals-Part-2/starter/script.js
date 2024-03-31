5bc90c825149fa07f32927f8570fed2803ad74b2
Daniel Bekenstein <dbekenstein@gmail.com>
2024-03-31 18:45:08 -0400

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,36 @@
-"user strict";
+"use strict";
+/*
+function logger(myName) {
+  console.log(`My name is ${myName}`);
+}
+
+logger("Dan");
+logger("Bob");
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+console.log(fruitProcessor(5, 1));
+
+const calcAge3 = (birthYear) => 2037 - birthYear;
+console.log(calcAge3(1994));
+*/
+const calcAverage = (scr1, scr2, scr3) => (scr1 + scr2 + scr3) / 3;
+
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+
+function checkWinner(avgDolphins, avgKoalas) {
+  if (avgKoalas > avgDolphins * 2) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+  } else if (avgDolphins > avgKoalas * 2) {
+    console.log(`Koalas win (${avgDolphins} vs. ${avgKoalas})`);
+  } else {
+    console.log(`No team wins...`);
+  }
+}
+
+checkWinner(scoreDolphins, scoreKoalas);
