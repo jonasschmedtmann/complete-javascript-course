7ffecb4e8846e3022c417cae0d139ca546f24c68
Daniel Bekenstein <dbekenstein@gmail.com>
2024-04-03 21:49:10 -0400

    Modal finished

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -15,9 +15,14 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
+for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
 
-  btnCloseModal.addEventListener('click', closeModal);
-  overlay.addEventListener('click', closeModal);
-}
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
