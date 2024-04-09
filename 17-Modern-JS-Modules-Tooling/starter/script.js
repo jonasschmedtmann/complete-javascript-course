0c5a03b41df65efc0b67d3aea5865265ab2dbd55
jack-hendry <jack.r.hendry.p@gmail.com>
2024-04-09 08:25:50 -0400

    276. CommonJS Modules

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -50,31 +50,41 @@
 
 
 
-const ShoppingCart2 = (function() {
-    const cart = [];
-    const shippingCost = 10;
-    const totalPrice = 237;
-    const totalQuantity = 23;
-    const addToCart = function(product, quantity){
-    cart.push({product, quantity});
-    console.log(`${quantity} ${product} added to cart `);
-    };
+// const ShoppingCart2 = (function() {
+//     const cart = [];
+//     const shippingCost = 10;
+//     const totalPrice = 237;
+//     const totalQuantity = 23;
+//     const addToCart = function(product, quantity){
+//     cart.push({product, quantity});
+//     console.log(`${quantity} ${product} added to cart `);
+//     };
 
-    const orderStock = function(product, quantity){
+//     const orderStock = function(product, quantity){
+//     cart.push({product, quantity});
+//     console.log(`${quantity} ${product} ordered from supplier `);
+//     };
+
+//     return {
+//         addToCart,
+//         cart,
+//         totalPrice,
+//         totalQuantity
+//     }
+// })();
+
+// ShoppingCart2.addToCart('apple', 4);
+// ShoppingCart2.addToCart('pizza', 2);
+// console.log(ShoppingCart2);
+// console.log(ShoppingCart2.shippingCost);
+
+
+// Export 
+export.addToCart = function(product, quantity){
     cart.push({product, quantity});
     console.log(`${quantity} ${product} ordered from supplier `);
     };
 
-    return {
-        addToCart,
-        cart,
-        totalPrice,
-        totalQuantity
-    }
-})();
 
-ShoppingCart2.addToCart('apple', 4);
-ShoppingCart2.addToCart('pizza', 2);
-console.log(ShoppingCart2);
-console.log(ShoppingCart2.shippingCost);
-
+// Import 
+const { addToCart } = require('./shoppingCart.js');
