0973f1a4e40458c6acb2d2727414da1c8a9ebc78
Phillip <philmography2010@gmail.com>
2024-07-27 10:44:23 -0500

    work

---
+++ 01-Fundamentals-Part-1/starter/assignmentFolder/scriptAssignment.js
@@ -0,0 +1,34 @@
+let js = 'amazing';
+console.log(40+8+23-10);
+
+console.log("Jonas");
+console.log('23');
+
+
+let firstName = "Jonas";
+let firstNamePerson;
+let first_name_person;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let threeyears = 3;
+console.log(threeyears);
+
+console.log(Math.PI);
+
+let myFIrstJob = "Programmer";
+let myCurrentJob ='Teacher';
+
+console.log(myCurrentJob);
+console.log('BREAK\n',10)
+
+
+let country = 'USA';
+let continent = 'NA';
+let population = 100;
+
+console.log(country);
+console.log(continent);
+console.log(population);
