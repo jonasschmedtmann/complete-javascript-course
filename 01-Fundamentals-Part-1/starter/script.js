5ac5f8beaa5198a62305eb40a32c83a57a5e6c57
BGDNbgdn <bogdan.dmytrenko15@gmail.com>
2024-07-30 23:11:09 +0200

    UPDT

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,4 @@
+const age = 23;
+const drink = age >= 18 ? "wine" : "water";
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const calcage2 = (birthyear) => 2037 - birthyear;
+const age = calcage2(1991);
+console.log(age);
