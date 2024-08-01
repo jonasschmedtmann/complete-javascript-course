b602977563c4634901b5a03f8f56875166220984
Phillip <philmography2010@gmail.com>
2024-08-01 18:31:26 -0500

    work

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+if (x === 23) {
+  console.log(23);
+}
+
+const caclAge = birthYear => 2037 - birthYear;
+console.log();
+console.log(`I am  `);
+console.log();
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 3;
+document.querySelector('.score').textContent = '#';
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
