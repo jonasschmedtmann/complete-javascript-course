f7c57a228acdf9fb0be70a2f82b10cfc54ce6809
Phillip <philmography2010@gmail.com>
2024-08-01 18:54:01 -0500

    update

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,10 +1,23 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number!';
+/* console.log(document.querySelector('.message').textContent);
+;
 
 document.querySelector('.number').textContent = 3;
 document.querySelector('.score').textContent = '#';
 
 document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+console.log(document.querySelector('.guess').value); */
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Bad Guess;Invalid!!!';
+  } else if (guess === 1) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else {
+    document.querySelector('.message').textContent = 'Try again!';
+  }
+});
