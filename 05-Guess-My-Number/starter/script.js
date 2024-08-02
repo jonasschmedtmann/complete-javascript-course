ea2dd4aba9df4eb230ed9d26be9ef8cac1b95fae
Phillip <philmography2010@gmail.com>
2024-08-01 22:02:46 -0500

    update

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -9,15 +9,33 @@ document.querySelector('.score').textContent = '#';
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value); */
 
+/* const secretNumber = function () {
+  let arr = [];
+  for (let i = 1; i <= 20; i++) {
+    arr.push(i);
+  }
+  let num = Math.floor(Math.random() * (arr.length + 1));
+  if (num === 0) {
+    return secretNumber();
+  } else {
+    return num;
+  }
+}; */
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
-  if (!guess) {
+  if (!guess || guess < 0 || guess > 20) {
     document.querySelector('.message').textContent = 'Bad Guess;Invalid!!!';
-  } else if (guess === 1) {
+  } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
-  } else {
-    document.querySelector('.message').textContent = 'Try again!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!';
   }
 });
