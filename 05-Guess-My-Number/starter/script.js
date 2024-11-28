f15793bb37a36596595f39a56ccd81ce0ac9520e
Mohamed Altahan <maltahanml@gmail.com>
2024-11-28 13:39:04 +0200

    add score variable and logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+const number = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+document.querySelector('.number').textContent = number;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === number) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > number) {
+    document.querySelector('.message').textContent = '📈  high!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < number) {
+    document.querySelector('.message').textContent = '📉  low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
+
+
