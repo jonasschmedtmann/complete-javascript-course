ef82c3e87a6b2f94adfd650e19084a6b54e7c0dc
Mohamed Altahan <maltahanml@gmail.com>
2024-12-01 23:47:15 +0200

    more iterations

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let index = 0; index < btnsOpenModal.length; index++) {
+  console.log(btnsOpenModal[index].textContent);
+}
