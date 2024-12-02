6b6812f70a9277a59b276d48d39ebdb8501e49ce
Mohamed Altahan <maltahanml@gmail.com>
2024-12-02 11:04:28 +0200

    create modals

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,6 +4,22 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let index = 0; index < btnsOpenModal.length; index++) {
-  console.log(btnsOpenModal[index].textContent);
-}
+for (let index = 0; index < btnsOpenModal.length; index++)
+  btnsOpenModal[index].addEventListener('click', e => {
+    // code
+    // Open the modal when the corresponding button is clicked
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+btnCloseModal.addEventListener('click', e => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+  // code
+});
+
+overlay.addEventListener('click', e => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+  // code
+});
